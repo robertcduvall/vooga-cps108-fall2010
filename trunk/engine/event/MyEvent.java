cc25963b2d7613445ba946dcaa7382f6f3e4d0cd
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-09-28 03:36:33 +0000

    add example to javadoc

--- trunk/engine/event/MyEvent.java
+++ trunk/engine/event/MyEvent.java
@@ -1,12 +1,36 @@
 package engine.event;
 /**
  * A object of a class which implements this interface will be passed to the eventlistener
+ * The class which implements this interface can be customized to create more methods.
  * 
+ * Example:
+ * public class PacmanMoveEvent implements MyEvent {
+ *	private Object obj;
+ *	private String sName;
+ *	private int x;
+ *	private int y;
+ *
+ *
+ *	public PacmanMoveEvent(Object source, String sName, int x, int y) {
+ *		obj = source;
+ *		this.sName = sName;
+ *		this.x = x;
+ *		this.y = y;
+ *	}
+ *   @Override
+ *   public Object getSource() {
+ *	    return obj;
+ *   }
+ *   @Override
+ *   public String getName() {
+ *		return sName;
+ *	}
+ *	
  * @author Hao He
  * @author Meng Li
  * @author Cody Kolodziejzyk
  * @Version: September 26th
- *gog
+ *
  */
 public interface MyEvent{
 	/**
