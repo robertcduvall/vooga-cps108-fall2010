e1edc6d250b1d64ab89bb08c2aa3e9f91db9fa13
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-09-28 03:36:41 +0000

    add example to javadoc

--- trunk/engine/event/MyEventListener.java
+++ trunk/engine/event/MyEventListener.java
@@ -3,7 +3,29 @@ package engine.event;
 
 /**
  * A object of a class which wants to addMyEventListener or a class which wants to listen to an event 
- * must implement this interface
+ * must implement this interface. If the event has been fired, the actionPerformed in the interface
+ * will invoked.
+ * 
+ * Example:
+ * public class Ghost extends AbstractEventManager implements PacmanMoveListener {
+	private int x,y;
+
+	Ghost(int x, int y) {
+		this. x = x;
+		this. y = y;
+	}
+
+	@Override
+	public void actionPerformed(MyEvent e) {
+		action((PacmanMoveEvent)e);
+	}
+
+	@Override
+	public void action(PacmanMoveEvent e) {
+		//do something
+		setX(e.getX());
+		setY(e.getY());
+	}
  * 
  * @author Hao He
  * @author Meng Li
@@ -12,9 +34,9 @@ package engine.event;
  *
  */
 public interface MyEventListener {
-   /**
-    * When the event is triggered, this method will be called.
-    * @param e:MyEvent interface
-    */
-   public void actionPerformed(MyEvent e);
+	/**
+	 * When the event is triggered, this method will be called.
+	 * @param e:MyEvent interface
+	 */
+	public void actionPerformed(MyEvent e);
 }
