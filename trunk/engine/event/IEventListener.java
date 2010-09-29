76ce94c75eb08eecbf5e7689a32a0bd48e91bb69
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-09-29 19:58:18 +0000

    refactored the name of the classes and methods in the class

---
+++ trunk/engine/event/IEventListener.java
@@ -0,0 +1,42 @@
+package engine.event;
+
+
+/**
+ * A object of a class which wants to addEventListener or a class which wants to listen to an event 
+ * must implement this interface. If the event has been fired, the actionPerformed in the interface
+ * will invoked.
+ * 
+ * Example:
+ * public class Ghost extends AbstractEventManager implements PacmanMoveListener {
+	private int x,y;
+
+	Ghost(int x, int y) {
+		this. x = x;
+		this. y = y;
+	}
+
+	@Override
+	public void actionPerformed(IGameEvent e) {
+		action((PacmanMoveEvent)e);
+	}
+
+	@Override
+	public void action(PacmanMoveEvent e) {
+		//do something
+		setX(e.getX());
+		setY(e.getY());
+	}
+ * 
+ * @author Hao He
+ * @author Meng Li
+ * @author Cody Kolodziejzyk
+ * @Version: September 26th
+ *
+ */
+public interface IEventListener {
+   /**
+    * When the event is triggered, this method will be called.
+    * @param e:IGameEvent interface
+    */
+   public void actionPerformed(IGameEvent e);
+}
