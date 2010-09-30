19c3c7b9d43898573658df732330427913624ecc
robertcduvall <robertcduvall@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-09-30 00:39:11 +0000

    more cleaning up

--- trunk/engine/core/Game.java
+++ trunk/engine/core/Game.java
@@ -1,11 +1,25 @@
 package engine.core;
 
+import java.awt.*;
+import com.golden.gamedev.*;
+
+
 /**
  * Placeholder until we decide whose Sprite to use.
  *
  * @author rcd
  */
-public class Game
-{
+public class Game extends com.golden.gamedev.Game {
 
+   public void initResources() {
+      // initialize game packages
+   }
+
+   public void update(long elapsedTime) {
+      // call game packages before user gets to update
+   }
+
+   public void render(Graphics2D g) {
+      // render the game to the screen
+   }
 }
