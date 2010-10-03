e7a4a86b9e008fcc6932bb54cf1f137d548d5db3
sfeldsott <sfeldsott@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-03 15:26:09 +0000

    Porting to Golden T.

--- trunk/src/vooga/engine/overlay/OverlayPanel.java
+++ trunk/src/vooga/engine/overlay/OverlayPanel.java
@@ -1,8 +1,11 @@
 package vooga.engine.overlay;
 
-
+import com.golden.gamedev.*;
+import com.golden.gamedev.engine.*;
+import com.golden.gamedev.object.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collection;
 
 /**
  * The OverlayPanel provides a convenient way for users
@@ -18,7 +21,7 @@ import java.util.List;
  */
 public class OverlayPanel extends OverlayManager{
 
-	private List<Overlay> myOvrlys;
+//	private Collection<Overlay> myOverlays;
 	private boolean isOnTop;
 	
 	/**
@@ -27,11 +30,11 @@ public class OverlayPanel extends OverlayManager{
 	 * @param thisWorld
 	 * @param topOrBottom
 	 */
-	public OverlayPanel(World thisWorld, boolean topOrBottom)
+	public OverlayPanel(Game thisGame, boolean topOrBottom)
 	{
-		super(thisWorld,0,0);
+		super(thisGame,0,0);
 		isOnTop = topOrBottom;
-		myOvrlys = new ArrayList<Overlay>();
+		myOverlays = new ArrayList<Overlay>();
 	}
 	
 	/**
@@ -39,7 +42,7 @@ public class OverlayPanel extends OverlayManager{
 	 * @param overlay
 	 */
 	public void addOverlay(Overlay overlay){
-        myOvrlys.add(overlay);
+        myOverlays.add(overlay);
 	}
 	
 	/**
@@ -49,19 +52,17 @@ public class OverlayPanel extends OverlayManager{
 	 */
 	public void initialize()
 	{
-		int numberOfOverlays = myOvrlys.size();
+		int numberOfOverlays = myOverlays.size();
 		int[] xCoordinates = setXCoordinates();
-		int distanceFromEdge = getWorld().getHeight()/50;
+		int distanceFromEdge = getGame().getHeight()/50;
 		int yCoordinate = distanceFromEdge;
 		int yOffset = 0;	//The offsets are for in case the Overlay would run
 		int xOffset = 0;	//off the screen.
 		if(!isOnTop)
-			yCoordinate = getWorld().getHeight() - distanceFromEdge;
-		Overlay overlay;
-		for(int i=0; i < numberOfOverlays; i++)
+			yCoordinate = getGame().getHeight() - distanceFromEdge;
+		for(Overlay overlay: myOverlays)
 		{
-			overlay = myOvrlys.get(i);
-			getWorld().addObject(overlay, 0, yCoordinate);
+			getGame().addObject(overlay, 0, yCoordinate);
 			
 			if(overlay.getHeight()/2 > distanceFromEdge)
 				yOffset = overlay.getHeight()/2 - distanceFromEdge + 1;
