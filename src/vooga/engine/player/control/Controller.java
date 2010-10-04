621b87d1a56a867d676fb64dda003bd559012427
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-04 03:39:18 +0000

    Added Comments

--- src/vooga/engine/player/control/Controller.java
+++ src/vooga/engine/player/control/Controller.java
@@ -1,6 +1,9 @@
 package vooga.engine.player.control;
 
-/*
+/**
+ * Interface that can be used by classes that want to extend the Control class. The methods
+ * in this class are required to create a new Control scheme.
+ * 
  * @author Choi, Cue, Hawthorne
  * @version 1.0
  */
@@ -8,11 +11,29 @@ package vooga.engine.player.control;
 public interface Controller {
 
 	public void initializeMappings();
-	
+	/**
+     * Create keyset to map input to method. Can be overwritten to create new control scheme  
+     * 
+     * @param listen Use a String version of what to listen to (eg. "a"
+     * for "KEYBOARD" or "1" for "MOUSE")
+     * 
+     * @param method Name of method to map to (do not include brackets)
+     * 
+     * @param classname Name of class that wants to use this (eg.
+     * "Player" or "Game")
+     * 
+     * @param paramVals Value of the parameters that the method has
+     */
 	public void addInput(String listen, String method, String classname, Object... paramVals);
-	
+    /**
+     * Sets the parameter types that need to be used for the next method
+     * 
+     * @param parameterTypes The types of parameters that need to be used. Need to be implemented in the form setParams(new Class[]{Class cls})
+     */
     public void setParams(Class<?>... parameterTypes);
-	
+    /**
+     * Invoke methods here. Call method each time through game loop
+     */
 	public void update();
 	
 }
--- src/vooga/engine/player/control/MouseControl.java
+++ src/vooga/engine/player/control/MouseControl.java
@@ -6,7 +6,9 @@ import com.golden.gamedev.Game;
 import com.golden.gamedev.engine.BaseInput;
 import vooga.engine.player.control.*;
 
-/*
+/**
+ * Built-in example of how to extend Control class properly. Also a usable mouse
+ * Controller creator.
  * @author Choi, Cue, Hawthorne
  * @version 1.0
  */
@@ -41,6 +43,19 @@ public class MouseControl extends Control{
 		mouseParamMap = new HashMap<Integer, Object[]>();
 	}
 	
+	/**
+     * Create keyset to map input to method. Can be overwritten to create new control scheme  
+     * 
+     * @param listen Use a String version of what to listen to (eg. "a"
+     * for "KEYBOARD" or "1" for "MOUSE")
+     * 
+     * @param method Name of method to map to (do not include brackets)
+     * 
+     * @param classname Name of class that wants to use this (eg.
+     * "Player" or "Game")
+     * 
+     * @param paramVals Value of the parameters that the method has
+     */
 	public void addInput(String listen, String method,
             String classname, Object... paramVals) {
         try{
