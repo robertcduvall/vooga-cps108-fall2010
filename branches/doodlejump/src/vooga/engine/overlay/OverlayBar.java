a9d2666c4f8306eb3a8d2fddcce3bc6acc461534
acue@mac.com <acue@mac.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-05 21:54:22 +0000

    Doodle jump branch

---
+++ branches/doodlejump/src/vooga/engine/overlay/OverlayBar.java
@@ -0,0 +1,215 @@
+package vooga.engine.overlay;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
+/**
+ * The OverlayBar class displays a bar that grows or shrinks based on an int and
+ * a maximum indicated by the user
+ * 
+ * This example creates an OverlayBar that displays the score of a game (as represented by myStat). The
+ * OverlayBar is placed at point (10,10);
+ * 
+ * Stat<Integer> myStat;
+ * myStat = new Stat(Integer 0);
+ * OverlayBar scorebar = new OverlayBar(myStat,100);
+ * addObject(scorebar,10,10); 
+ * 
+ * @author Andrew Bown
+ * 
+ */
+public class OverlayBar extends Overlay {
+
+	private static final int DEFAULT_MAXLENGTH = 300;
+	private static final int DEFAULT_HEIGHT = 20;
+	private static final Color DEFAULT_FILLCOLOR = Color.BLACK;
+	private static final Color DEFAULT_BACKGROUNDCOLOR = Color.RED;
+
+	private Stat<Integer> myStat;
+	private int myMaxLength = DEFAULT_MAXLENGTH;
+	private int myMaxScore;
+	private Color myColor = DEFAULT_FILLCOLOR;
+	private Color myBackgroundColor = DEFAULT_BACKGROUNDCOLOR;
+	private int myHeight = DEFAULT_HEIGHT;
+	
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * Constructs an OverlayBar with the given Stat<Integer> and maxScore.
+	 * 
+	 * @param stat
+	 *            The statistic that the OverlayBar should represent.
+	 * @param maxScore
+	 *            The maximum value the statistic can reach.
+	 */
+	public OverlayBar(Stat<Integer> stat, int maxScore) {
+		myStat = stat;
+		myMaxScore = maxScore;
+	}
+
+	/**
+	 * Prints the bar on the screen by calling drawBar().
+	 */
+	@Override
+	public void update(long t) {
+		drawBar();
+	}
+
+	/**
+	 * This method draws the OverlayBar on the screen according to the
+	 * specifications set by the user.
+	 */
+	private void drawBar(){
+		BufferedImage bufferedImage = new BufferedImage(myMaxLength, myHeight,BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2d = bufferedImage.createGraphics();
+		g2d.setColor(myBackgroundColor);
+		g2d.fillRect(0, 0, myMaxLength, myHeight);
+		
+		float ratio = (float) myStat.getStat() / (float) myMaxScore;
+
+		// only draw the bar fill if the score is greater than 0
+		if (ratio > 0) {
+			int width = Math.round(ratio * myMaxLength);
+			g2d.setColor(myColor);
+			g2d.fillRect(0, 0, width, myHeight);
+		}
+		
+		g2d.dispose();
+		setImage(bufferedImage);
+	}
+
+	/**
+	 * This method returns the color of the OverlayBar's fill. This is the color
+	 * of the rectangle that grows and shrinks based on the score.
+	 * 
+	 * @return The color of the rectangle.
+	 */
+	public Color getColor() {
+		return myColor;
+	}
+
+	/**
+	 * This method sets the color of the OverlayBar's fill. This sets the color
+	 * of the rectangle that grows and shrinks based on the score.
+	 * 
+	 * @param color
+	 *            The new color for the rectangle
+	 */
+	public void setColor(Color color) {
+		if(color != null){
+			myColor = color;
+		}
+	}
+
+	/**
+	 * Returns the maximum length of the OverlayBar.
+	 * 
+	 * @return The maximum length of the OverlayBar.
+	 */
+	public int getMaxLength() {
+		return myMaxLength;
+	}
+
+	/**
+	 * Sets the maximum length of the OverlayBar. It must be greater than 0.
+	 * 
+	 * @param maxLength
+	 *            The length the maximum should be set to.
+	 */
+	public void setMaxLength(int maxLength) {
+		if(maxLength > 0){
+			myMaxLength = maxLength;
+		}
+	}
+
+	/**
+	 * Returns the Stat that the OverlayBar represents.
+	 * 
+	 * @return The Stat the OverlayBar represents.
+	 */
+	public Stat<Integer> getStat() {
+		return myStat;
+	}
+
+	/**
+	 * Sets the Stat the OverlayBar represents.
+	 * 
+	 * @param stat
+	 *            The stat the OverlayBar should represent.
+	 */
+	public void setStat(Stat<Integer> stat) {
+		if(stat != null){
+			myStat = stat;
+		}
+	}
+
+	/**
+	 * Returns the maximum score for the OverlayBar, which determines the length
+	 * of the OverlayBar relative to the current score.
+	 * 
+	 * @return The maximum score for the OverlayBar.
+	 */
+	public int getMaxScore() {
+		return myMaxScore;
+	}
+
+	/**
+	 * Sets the maximum score for the OverlayBar, which determines the length of
+	 * the OverlayBar relative to the current score. Must be greater than 0.
+	 * 
+	 * @param maxScore 
+	 * 			The score the maximum should be set to.
+	 */
+	public void setMaxScore(int maxScore) {
+		if(maxScore > 0){
+			myMaxScore = maxScore;
+		}
+	}
+
+	/**
+	 * Returns the background color of the OverlayBar.
+	 * 
+	 * @return The background color of the OverlayBar.
+	 */
+	public Color getBackgroundColor() {
+		return myBackgroundColor;
+	}
+
+	/**
+	 * Sets the backgroundColor of the OverlayBar.
+	 * 
+	 * @param backgroundColor
+	 *            The color the background color should be set to.
+	 */
+	public void setBackgroundColor(Color backgroundColor) {
+		if(backgroundColor != null){
+			myBackgroundColor = backgroundColor;
+		}
+	}
+
+	/**
+	 * Returns the height of the OverlayBar.
+	 * 
+	 * @return The height of the OverlayBar.
+	 */
+	@Override
+	public int getHeight() {
+		return myHeight;
+	}
+
+	/**
+	 * Sets the height of the OverlayBar. Must be greater than 0.
+	 * 
+	 * @param height
+	 *            The height the OverlayBar should be set to.
+	 */
+	public void setHeight(int height) {
+		if(height > 0){
+			myHeight = height;
+		}
+	}
+
+}
---
+++ branches/doodlejump/src/vooga/engine/overlay/OverlayStat.java
@@ -0,0 +1,68 @@
+package vooga.engine.overlay;
+
+
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+
+/**
+ * The Overlay stat class to display a String and something the user defines
+ * @author Justin Goldsmith
+ * 
+ * Stat<Integer> stat = new Stat<Integer>(5);
+ * OverlayStat overlay = new OverlayStat("score: ", stat);
+ */
+
+
+public class OverlayStat extends OverlayString {
+	
+	private StatInt myIntTracker;
+	private Stat<?> myGenTracker;
+	//private Stat myGenTracker;
+	
+	/**
+	 * Creates a OverlyStat Class
+	 * @param label String to display
+	 * @param tracker Int specific stat
+	 */
+	public OverlayStat(String label, StatInt tracker){
+		super(label);
+		myIntTracker = tracker;
+	}
+	
+	/**
+	 * Creates a OverlyStat Class
+	 * @param label String to display
+	 * @param tracker Can be a Stat of any type.  Will call the toString of the type to display.
+	 */
+	public OverlayStat(String label, Stat<?> tracker){
+		super(label);
+		myGenTracker = tracker;
+	}	
+
+
+	
+	public void render(Graphics2D g){
+		String track = setStat();
+		print(getString() + " " + track, g);
+	}
+
+	private String setStat() {
+		if(myIntTracker != null){
+			return Integer.toString(myIntTracker.getStat());
+		}else if(myGenTracker != null){
+			return myGenTracker.getStat().toString();
+		}else {
+			return "";
+		}
+		
+	}
+	
+
+}
