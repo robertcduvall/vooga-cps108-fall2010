b8111638fb9550c6e1629642995e5a5e82f658b4
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-05 03:57:30 +0000

    Branch for developing a TowerDefense game. Split necessary for hacking into other people's APIs in order to get game to run.
    In vooga.engine.player.control.GameEntitySprite, setToCurrentSprite(Sprite nextSprite) has a null reference exception. Offending code has been commented out and replaced 
    with just zeros.
    
    Derek Zhou (dzz@duke.edu)
    Justin Goldsmith (jcg26@duke.edu)
    Daniel Koverman (djk14@duke.edu)

---
+++ branches/https:/vooga-cps108-fall2010.googlecode.com/svn/branches/towerdefense/src/vooga/engine/event/IEventListener.java
@@ -0,0 +1,22 @@
+package vooga.engine.event;
+
+/**
+ * The listener interface for receiving game events. When the game event occurs,
+ * that object's actionPerformed method is invoked.
+ * 
+ * @author Hao He
+ * @author Meng Li
+ * @author Cody Kolodziejzyk
+ * @see examples/event
+ * @Version: September 26th
+ * 
+ */
+public interface IEventListener {
+	/**
+	 * When the event is triggered, this method will be called.
+	 * 
+	 * @param e
+	 *            The occured event
+	 */
+	public void actionPerformed(IEvent e);
+}
---
+++ branches/https:/vooga-cps108-fall2010.googlecode.com/svn/branches/towerdefense/src/vooga/engine/level/reader/Level.java
@@ -0,0 +1,72 @@
+package vooga.engine.level.reader;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+import vooga.engine.core.Sprite;
+import vooga.engine.resource.ResourceHandler;
+
+
+/**
+ *@author Bhawana, Cameron, Derek
+ *
+ * Level class describes a static level, ie, a non-moving game space.
+ * This class would be used by the LevelManager class to load the Sprites for a new static level.
+ * This class is dependent on the ResourceManager to provide the image required to instantiate
+ * the Sprites for any given Level.
+ * 
+ * Example: The following piece of code would return a Collection of Sprites
+ * for the level specified by the File fileName.
+ * 
+ * @code
+ * Level level = new Level(new Scanner(fileName));
+ * Collection<Sprite> spritesList = level.getSpritesList();
+ * 
+ * 
+ */
+
+public class Level {
+	private Collection<Sprite> mySpritesList; 
+	
+	public Level(Scanner fileToBeRead)
+	{
+		loadSprites(fileToBeRead);
+		mySpritesList = new ArrayList<Sprite>();
+	}
+	
+	/**
+	 * Initializes all the Sprites for a particular level.
+	 * The scanner passed in should be
+	 * initialized with the text file for that level.
+	 * 
+	 * @param Scanner which scans from the level initialization file.
+	 */
+	
+	public void loadSprites(Scanner fileToBeRead) {
+		//ImageHandler resourceManager = new ImageHandler();
+		
+        while (fileToBeRead.hasNextLine()) {
+            String spriteDetails = fileToBeRead.nextLine();
+            Scanner details = new Scanner(spriteDetails);
+            details.useDelimiter(", *");
+            String imageName = details.next();
+            BufferedImage image = (BufferedImage)ResourceHandler.getMapping(imageName);
+            
+            double xPosition = details.nextDouble();
+            double yPosition = details.nextDouble();
+            Sprite sprite = new Sprite(image, xPosition, yPosition);
+            mySpritesList.add(sprite);
+        }
+	}
+	
+	/**
+	 * @return A Collection of Sprites to be used by the Game.
+	 */
+	
+	public Collection<Sprite> getSpritesList()
+	{
+		return mySpritesList;
+	}
+
+}
