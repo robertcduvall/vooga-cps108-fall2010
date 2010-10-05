a231f698ab43d4564d66139428d52c420eb43e1b
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-05 20:20:39 +0000

    Updated Game, began implementing control methods

--- src/vooga/games/zombieland/Zombieland.java
+++ src/vooga/games/zombieland/Zombieland.java
@@ -1,7 +1,7 @@
 package vooga.games.zombieland;
 
-import java.awt.Dimension;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 import com.golden.gamedev.GameLoader;
 import com.golden.gamedev.object.*;
@@ -9,6 +9,7 @@ import com.golden.gamedev.object.background.*;
 
 import vooga.engine.core.Game;
 import vooga.games.jumper.Jumper;
+import vooga.engine.player.control.KeyboardControl;
 import vooga.engine.player.control.PlayerSprite;
 
 public class Zombieland extends Game{
@@ -16,15 +17,32 @@ public class Zombieland extends Game{
     private static final int GAME_WIDTH = 640;
 	private static final int GAME_HEIGHT = 480;
 
-	private Sprite hero;
+	private Sprite shooter;
 	private Background background;
+	private SpriteGroup zombies;
+	private Shooter hero;
+	private PlayField playfield;
+	private KeyboardControl control;
 	
 	public void initResources(){
+		shooter = new AnimatedSprite();
+		zombies = new SpriteGroup("Zombies");
+		hero = new Shooter("Hero", "Idle", shooter, 100, 0);
+		playfield = new PlayField();
+		control = new KeyboardControl(hero, this);
+		
+		playfield.add(hero);
 	}
 	
 	public void update(long elapsedTime) {
     }
 
+
+	public void setListeners(){
+		control.addInput(KeyEvent.VK_LEFT, "goLeft", "Shooter", null);
+	}
+	
+	
     public void render(Graphics2D g) {
     }
     
