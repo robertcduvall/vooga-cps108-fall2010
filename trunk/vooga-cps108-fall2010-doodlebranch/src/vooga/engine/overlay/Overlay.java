27cff01d3f4a53378b2149b23e41a182c58ae389
acue@mac.com <acue@mac.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-05 21:51:07 +0000

    Doodle jump branch

---
+++ trunk/vooga-cps108-fall2010-doodlebranch/src/vooga/engine/overlay/Overlay.java
@@ -0,0 +1,18 @@
+package vooga.engine.overlay;
+
+import java.awt.image.BufferedImage;
+
+import com.golden.gamedev.object.Sprite;
+
+
+
+/**
+ * Generic Overlay class to be extended by any Overlay class that
+ * uses a Stat object to keep track of it's current value.
+ * @author Se-Gil Feldsott
+ */
+public abstract class Overlay extends Sprite {
+
+	private Stat myStat;
+	
+}
