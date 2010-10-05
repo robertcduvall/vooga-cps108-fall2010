8474dda5c9ff8646dcff20999060cea5e04b162d
acue@mac.com <acue@mac.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-05 23:01:30 +0000

    Trying to merge

--- branches/doodlejump/src/vooga/games/doodlejump/DoodleGame.java
+++ branches/doodlejump/src/vooga/games/doodlejump/DoodleGame.java
@@ -1,5 +1,6 @@
 package vooga.games.doodlejump;
 
+import vooga.engine.player.control.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
@@ -9,11 +10,14 @@ import vooga.engine.player.control.PlayerSprite;
 
 import com.golden.gamedev.*;
 import com.golden.gamedev.object.*;
+import com.golden.gamedev.object.background.*;
 
 
 public class DoodleGame extends Game {
-	private Sprite piece;
+	private Sprite green_platform;
+	private Sprite light_blue_platform;
 	private PlayerSprite doodle;
+	Background background;
 	private PlayerSprite blue_flying_monster;
 	private PlayerSprite blue_monster;
 	private PlayerSprite green_flying_monster;
@@ -23,51 +27,36 @@ public class DoodleGame extends Game {
 
 	
 	public void initResources() {
-		
-		blue_flying_monster = new PlayerSprite("blue_flying_monster", "bfm", new Sprite(getImage("blue_flying_monster.png")));
-		
-		blue_monster = new PlayerSprite("blue_monster", "bm", new Sprite(getImage("blue_monster.png")));
-
-		green_flying_monster = new PlayerSprite("green_flying_monster", "gfm", new Sprite(getImage("green_flying_monster.png")));
-		
-		green_monster = new PlayerSprite("green_monster", "gm", new Sprite(getImage("green_monster.png")));
-		
-		purple_monster = new PlayerSprite("purple_monster", "pm", new Sprite(getImage("purple_monster.png")));
-		
-		red_monster = new PlayerSprite("red_monster", "rm", new Sprite(getImage("red_monster.png")));
-		
-		
-        setFPS(100);
+        BufferedImage image = getImage("green_platform.png");
+        double x = 100;
+        double y = 200;
+        green_platform = new Sprite(image, x, y);
+        BufferedImage image1 = getImage("light_blue_platform.png");
+        double x1 = 200;
+        double y1 = 100;
+        light_blue_platform = new Sprite(image1, x1, y1);
+        background = new ImageBackground(getImage("background.png"));
+        doodle = new PlayerSprite("doodle", "normal", new Sprite(getImage("doodle.png")));
+        doodle.setLocation(340, 411);
     }
 
 
     public void update(long elapsedTime) {
-        // Input Engine
-        //if (keyPressed(KeyEvent.VK_SPACE)) {
-
-            // Sound Engine
-          //  playSound("sound1.wav");
-        //}
+        
     }
 
 
     public void render(Graphics2D g) {
         // Graphics Engine
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, getWidth(), getHeight());
-        //piece.render(g);
-        blue_flying_monster.render(g);
-        blue_monster.render(g);
-        green_flying_monster.render(g);
-        green_monster.render(g);
-        purple_monster.render(g);
-        red_monster.render(g);
-        
+        background.render(g);
+        green_platform.render(g);
+        light_blue_platform.render(g);
+        doodle.render(g);
     }
 
     public static void main(String[] args) {
         GameLoader game = new GameLoader();
-        game.setup(new DoodleGame(), new Dimension(640,480), false);
+        game.setup(new DoodleGame(), new Dimension(695,822), false);
         game.start();
     }
 
