cdc66fa76e5649972949191dd50fe01ad3fc81ab
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-06 19:55:31 +0000

    Made GoldenTCollisionManager's detectCollisions class public

--- src/vooga/engine/collision/GoldenTCollisionManager.java
+++ src/vooga/engine/collision/GoldenTCollisionManager.java
@@ -13,7 +13,7 @@ public class GoldenTCollisionManager extends CollisionManager {
 	public GoldenTCollisionManager() {
 	}
 
-	boolean detectCollision(Collidable object1, Collidable object2) {
+	public boolean detectCollision(Collidable object1, Collidable object2) {
 		if(object1 instanceof CollisionShape && object2 instanceof CollisionShape){
 			return ((CollisionShape)object1).intersects((CollisionShape)object2);
 		}
