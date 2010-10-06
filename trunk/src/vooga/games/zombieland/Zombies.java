f3e11aa26d31eedefc2ea86739f9f2bc5503ca05
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-06 16:36:30 +0000

    Added Zombies class, there is now one Zombie chasing the player. No interactions yet.

---
+++ trunk/src/vooga/games/zombieland/Zombies.java
@@ -0,0 +1,61 @@
+package vooga.games.zombieland;
+
+import com.golden.gamedev.object.Sprite;
+
+import vooga.engine.player.control.PlayerSprite;
+
+public class Zombies extends PlayerSprite{
+	
+	private Shooter target;
+	private double xTarget;
+	private double yTarget;
+	private String targetDirection;
+	private double speed;
+	
+	public Zombies(String name, String stateName, Sprite s, Shooter hero) {
+		super(name, stateName, s);
+		target = hero;
+		targetDirection = "X";
+		speed = -0.5;
+	}
+	
+	public double getDirection(){
+		xTarget = target.getX();
+		yTarget = target.getY();
+		
+		if (Math.abs(getX()-xTarget) >= Math.abs(getY()-yTarget)){
+			targetDirection = "X";
+			return xTarget-getX();
+			
+		}
+		else{
+			targetDirection = "Y";
+			return yTarget-getY();
+		}
+	}
+	
+	public void update(){
+		double direction = getDirection();
+		if (targetDirection.equals("X")){
+			if (direction < 0){
+				moveX(speed);
+			}
+			else{
+				moveX(Math.abs(speed));
+			}
+		}
+		else{
+			if (direction < 0){
+				moveY(speed);
+			}
+			else{
+				moveY(Math.abs(speed));
+			}
+		}
+		
+	}
+	
+	
+	
+
+}
