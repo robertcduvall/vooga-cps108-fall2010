691ae062a269f34a7da742ee3fec04e235161c7d
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-06 06:21:44 +0000

    Made a movable player

--- src/vooga/games/zombieland/Shooter.java
+++ src/vooga/games/zombieland/Shooter.java
@@ -1,19 +1,40 @@
 package vooga.games.zombieland;
 
-import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.*;
+import com.golden.gamedev.Game.*;
 
 import vooga.engine.player.control.KeyboardControl;
 import vooga.engine.player.control.PlayerSprite;
 
 public class Shooter extends PlayerSprite{
-
+	
+	private Sprite currentSprite;
+	
 	public Shooter(String name, String stateName, Sprite s,int playerHealth, int playerRank) {
 		super(name, stateName, s, playerHealth, playerRank);
+		currentSprite = s;
 	}
 
+	
+	
 	public void goLeft()
 	{
-		
+		moveX(-10);
+	}
+	
+	public void goRight()
+	{
+		moveX(10);
+	}
+	
+	public void goUp()
+	{
+		moveY(-10);
+	}
+	
+	public void goDown()
+	{
+		moveY(10);
 	}
 	
 }
--- src/vooga/games/zombieland/Zombieland.java
+++ src/vooga/games/zombieland/Zombieland.java
@@ -9,41 +9,48 @@ import com.golden.gamedev.object.background.*;
 
 import vooga.engine.core.Game;
 import vooga.games.jumper.Jumper;
-import vooga.engine.player.control.KeyboardControl;
-import vooga.engine.player.control.PlayerSprite;
+import vooga.engine.player.control.*;
 
 public class Zombieland extends Game{
 	
-    private static final int GAME_WIDTH = 640;
-	private static final int GAME_HEIGHT = 480;
+    private static final String PLAYER_CLASS = "vooga.games.zombieland.Shooter";
+	private static final int GAME_WIDTH = 700;
+	private static final int GAME_HEIGHT = 500;
 
 	private Sprite shooter;
 	private Background background;
 	private SpriteGroup zombies;
-	private Shooter hero;
+	private Shooter player;
 	private PlayField playfield;
 	private KeyboardControl control;
 	
 	public void initResources(){
-		shooter = new AnimatedSprite();
+		shooter = new Sprite(getImage("resources/Down1.png"), 350, 250);
 		zombies = new SpriteGroup("Zombies");
-		hero = new Shooter("Hero", "Idle", shooter, 100, 0);
+		player = new Shooter("Hero", "Idle", shooter, 100, 0);
 		playfield = new PlayField();
-		control = new KeyboardControl(hero, this);
+		control = new KeyboardControl(player, this);
 		
-		playfield.add(hero);
+		playfield.add(player);
+		setListeners();
 	}
 	
 	public void update(long elapsedTime) {
+		playfield.update(elapsedTime);
+		control.update();
     }
 
 
 	public void setListeners(){
-		control.addInput(KeyEvent.VK_LEFT, "goLeft", "Shooter", null);
+		control.addInput(KeyEvent.VK_LEFT, "goLeft", PLAYER_CLASS, null);
+		control.addInput(KeyEvent.VK_RIGHT, "goRight", PLAYER_CLASS, null);
+		control.addInput(KeyEvent.VK_UP, "goUp", PLAYER_CLASS, null);
+		control.addInput(KeyEvent.VK_DOWN, "goDown", PLAYER_CLASS, null);
 	}
 	
 	
     public void render(Graphics2D g) {
+    	playfield.render(g);
     }
     
     public static void main(String[] args) {
