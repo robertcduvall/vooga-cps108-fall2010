ee1191643b313777d0ec4299a1dec4a63ca93c33
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-06 05:34:05 +0000

    Added marioclone tile and map loading

---
+++ src/vooga/games/marioclone/map/tiles/IndestructibleTile.java
@@ -0,0 +1,25 @@
+package vooga.games.marioclone.map.tiles;
+
+import java.awt.image.BufferedImage;
+
+import vooga.engine.collision.Collidable;
+
+public class IndestructibleTile extends Tile {
+	private BufferedImage image;
+	
+	public IndestructibleTile(double x, double y, BufferedImage image) {
+		super(x,y);
+		this.image = image;
+	}
+	
+	@Override
+	public void actOnCollision(Collidable object) {
+		// do nothing
+	}
+
+	@Override
+	public BufferedImage getImage() {
+		return image;
+	}
+
+}
