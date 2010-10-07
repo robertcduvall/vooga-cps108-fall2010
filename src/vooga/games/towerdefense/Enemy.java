14d8c7b8755e83fac8edb4ac04e09ffc8869bbae
justin.goldsmith613 <justin.goldsmith613@gmail.com>
2010-10-07 06:50:59 +0000

    Enemy that moves along path at different speeds defined by game creator, and also a tool to help us build levels by tracing the path.

---
+++ src/vooga/games/towerdefense/Enemy.java
@@ -0,0 +1,130 @@
+package vooga.games.towerdefense;
+
+import java.util.ArrayList;
+
+import com.golden.gamedev.object.Sprite;
+
+import vooga.engine.player.control.PlayerSprite;
+
+public class Enemy extends PlayerSprite {
+	
+	private ArrayList<PathPoint> myPath;
+	ArrayList<PathPoint> myCurrentPath;
+	private int mySpeed;
+	private int myLoc;
+	private long myTotalTime;
+	private int myFreq;
+	private int myTempLoc;
+	private boolean restart;
+
+	public Enemy(String name, String stateName, Sprite s, ArrayList<PathPoint> path, int speed) {
+		super(name, stateName, s);
+		myPath = path; 
+		mySpeed = speed;
+		myLoc = 0;
+		myTotalTime = 0;
+		myFreq = 0;
+		myTempLoc = 0;
+		restart = true;
+	}
+	
+	@Override
+	public void update(long elapsedTime) {
+		super.update(elapsedTime);
+		if(restart){
+			int[] dist = getDistance();
+			//System.out.println(dist[0]);
+			createPath(dist[0], (int) ((((double)dist[0])/mySpeed) * 50) , myLoc, dist[1]);
+			//System.out.println(myCurrentPath.size());
+			//System.out.println(myTempLoc);
+			PathPoint point = myCurrentPath.get(myTempLoc);
+			myFreq = 20;
+			setLocation(point.getX(), point.getY());
+			//System.out.println(dist[1]);
+			//System.out.println(myPath.size());
+			myTempLoc++;
+			myTotalTime = 0;
+			restart = false;
+			//System.out.println(myLoc + " : " +  myPath.size());
+			if(myLoc >= myPath.size() - 1){
+				setActive(false);
+			}
+			myLoc = dist[1];
+			//System.out.println(myLoc + " : " +  myPath.size() + "a");
+		}else{
+			myTotalTime += elapsedTime;
+			if(myTotalTime >= myFreq * myTempLoc){
+				//System.out.println(myLoc + " : " + myPath.size());
+				PathPoint point = myCurrentPath.get(myTempLoc);
+				setLocation(point.getX(), point.getY());
+				if(myTempLoc == myCurrentPath.size()-1){
+					restart = true;
+					myTempLoc = 0;
+				}else{
+					myTempLoc++;
+				}
+			}
+			
+		}
+	}
+	
+	
+	private int[] getDistance(){
+		PathPoint current = myPath.get(myLoc);
+		PathPoint end;
+		double dist = 0;
+		for(int k = myLoc + 1; k< myPath.size(); k++){
+			end = myPath.get(k);
+			dist += getDistance(current, end);
+			if(dist>=mySpeed){
+				return new int[]{(int) dist, k};
+			}
+			current = end;
+		}
+		return new int[]{(int) dist, myPath.size() - 1};
+		
+	}
+	
+	private double getDistance(PathPoint beg, PathPoint end){
+		double changeXsq = Math.pow(beg.getX() - end.getX(), 2);
+		double changeYsq = Math.pow(beg.getY() - end.getY(), 2);
+		
+		return Math.sqrt((changeXsq + changeYsq));
+		
+	}
+	
+	private void createPath(double totalDistance, int segments, int start, int end){
+		int current = start + 1;
+		//System.out.println(segments + "seg");
+		myCurrentPath = new ArrayList<PathPoint>();
+		myCurrentPath.add(myPath.get(start));
+		double diff = totalDistance/segments;
+		//System.out.println(start + " : " + end);
+		while(start < end){
+			//System.out.println("a");
+			while(true){
+				if(current >= end){
+					start = current;
+					current++;
+					break;
+				}
+				double dist = getDistance(myPath.get(start), myPath.get(current));
+				//System.out.println(current + " : " + start);
+				if(dist>=diff){
+					myCurrentPath.add(myPath.get(current));
+					start = current;
+					current++;
+					break;
+				}else{
+					current++;
+				}
+			}
+			
+		}
+		//System.out.println(myCurrentPath.size() + "act");
+		
+	}
+
+	
+
+}
---
+++ src/vooga/games/towerdefense/PathTracer.java
@@ -0,0 +1,138 @@
+package vooga.games.towerdefense;
+
+import java.util.*;
+import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.*;
+import com.golden.gamedev.Game;
+import com.golden.gamedev.engine.BaseInput;
+import vooga.engine.player.control.*;
+
+/**
+ * Built-in example of how to extend Control class properly. Also a usable mouse
+ * Controller creator.
+ * @author Choi, Cue, Hawthorne
+ * @version 1.0
+ */
+
+public class PathTracer extends Control implements Controller{
+        private Map<Integer, Method> mouseMethodMap;
+        private Map<Integer, Object[]> mouseParamMap;
+        private Object[] parametervalues;
+        FileWriter fstream;
+    BufferedWriter out;
+    // Code only used for level creation
+    long myTime;
+    long myStartTime;
+    int myX;
+    int myY;
+       
+        public PathTracer(PlayerSprite initialPlayer, Game game){
+                super(initialPlayer, game);
+                initializeMappings();
+                 // Code only used for level creation
+                File file = new File("src/vooga/games/towerdefense/resources/levels/level1.txt");
+                try {
+                        fstream = new FileWriter(file);
+                } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                out = new BufferedWriter(fstream);
+                myStartTime = System.currentTimeMillis();
+        }
+       
+        public void initializeMappings(){
+                mouseMethodMap = new HashMap<Integer, Method>();
+                mouseParamMap = new HashMap<Integer, Object[]>();
+        }
+       
+        /**
+     * Create keyset to map input to method. Can be overwritten to create new control scheme  
+     *
+     * @param listen Use a String version of what to listen to (eg. "a"
+     * for "KEYBOARD" or "1" for "MOUSE")
+     *
+     * @param method Name of method to map to (do not include brackets)
+     *
+     * @param classname Name of class that wants to use this (eg.
+     * "Player" or "Game")
+     *
+     * @param paramVals Value of the parameters that the method has
+     */
+        public void addInput(String listen, String method,
+            String classname, Object... paramVals) {
+        try{
+                Class myClass = Class.forName(classname);
+            Method perform = myClass.getMethod(method,paramTypes);
+                int key = Integer.parseInt(listen);
+            mouseMethodMap.put(key, perform);
+            mouseParamMap.put(key, paramVals);
+            paramTypes = null;
+        } catch (Throwable e) {
+            System.err.println(e);
+        }
+    }
+       
+        public void update(){
+                for (int i = 0; i < players.size(); i++)
+        {
+             try {
+                                moveToCursor(players.get(i));
+                        } catch (IOException e) {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace();
+                        }
+        }
+                int key = myGame.bsInput.getMousePressed();
+        if (mouseMethodMap.containsKey(key))
+        {
+            try{
+                for (int i = 0; i < players.size(); i++)
+                {
+                     Method perform = mouseMethodMap.get(key);
+                     Object[] paramVals = mouseParamMap.get(key);
+                     perform.invoke(players.get(i), paramVals);
+                }
+            }
+            catch (Throwable e){
+                System.err.println(e);
+            }
+        }
+        // Code only used for level creation
+       myTime = System.currentTimeMillis() - myStartTime;
+       
+       
+        }
+       
+        private void moveToCursor(PlayerSprite player) throws IOException{
+                player.forceX(myGame.bsInput.getMouseX());
+        player.forceY(myGame.bsInput.getMouseY());
+       
+        // Code only used for level creation
+       if(myTime < 40000){
+                if(myGame.bsInput.isMouseDown(MouseEvent.BUTTON1)){
+                                int x = myGame.bsInput.getMouseX();
+                                int y = myGame.bsInput.getMouseY();
+                                //if((x != myX) && (y != myY)){
+                                        myX = x;
+                                        myY = y;
+                                        out.write(myX+ " ");
+                                        out.write(myY+ " ");
+                                        System.out.println(myX + " : " + myY);
+                               // }
+                        }
+        }else{
+                System.out.println("stop");
+                        out.close();
+        }
+        
+        }
+       
+       
+}
+
+
