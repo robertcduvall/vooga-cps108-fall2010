ab158f7b8dee5b4ad20169e64a84e6faeba73bf1
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-07 08:11:35 +0000

    Added bullets

--- trunk/src/vooga/games/zombieland/Shooter.java
+++ trunk/src/vooga/games/zombieland/Shooter.java
@@ -1,39 +1,91 @@
 package vooga.games.zombieland;
 
 import com.golden.gamedev.object.*;
+
 import vooga.engine.player.control.PlayerSprite;
 
 public class Shooter extends PlayerSprite {
 
 	private int speed;
-
+	private double angle;
+	private Zombieland game;
+	private int weapon;
 	public Shooter(String name, String stateName, AnimatedSprite s,
-			int playerHealth, int playerRank) {
+			int playerHealth, int playerRank, Zombieland zombieland) {
 		super(name, stateName, s, playerHealth, playerRank);
+		game=zombieland;
 		speed = -1;
+		angle=90;
+		weapon=1;
 	}
 
 	public void goLeft() {
+		angle=180;
 		setToCurrentSprite("Left");
 		moveX(speed);
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goRight() {
+		angle=0;
 		setToCurrentSprite("Right");
 		moveX(Math.abs(speed));
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goUp() {
+		angle=270;
 		setToCurrentSprite("Up");
 		moveY(speed);
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goDown() {
+		angle=90;
 		setToCurrentSprite("Down");
 		moveY(Math.abs(speed));
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
+	public void shoot() {
+		setToCurrentSprite("Shoot");
+		fire();
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
+	}
+
+	private void fire() {
+		switch(weapon) {
+		case 1:
+			spawnBullets(weapon);
+			break;
+		default:
+			spawnBullets(weapon);
+			break;
+		}
+	}
+
+	private void spawnBullets(int weaponChoice) {
+		switch(weaponChoice) {
+		case 1:
+			makeBullet(angle);
+			makeBullet(angle+10);
+			makeBullet(angle-10);
+			break;
+		default:
+			makeBullet(angle);
+			break;
+		}
+	}
+	
+	private void makeBullet(double theta) {
+		Bullet b=new Bullet(this, theta);
+		game.addBullet(b,theta);
+	}
 	public void update(long elapsedTime) {
+		AnimatedSprite sprite=(AnimatedSprite) getCurrentSprite();
 		super.update(elapsedTime);
+
+		sprite.setAnimate(false);
+		//		sprite.setFrame(0);
 	}
 }
