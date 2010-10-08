ba118b5c784bbe2e218961904b4c5bc094049986
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-08 02:29:30 +0000

    updates

--- src/vooga/games/zombieland/Weapon.java
+++ src/vooga/games/zombieland/Weapon.java
@@ -1,36 +1,83 @@
 package vooga.games.zombieland;
 
 public abstract class Weapon {
-	
+
 	private Shooter player;
 	private int ammoCount;
-	
-	public Weapon(Shooter s, int ammo){
-		player=s;
-		ammoCount=ammo;
+	private double damage;
+
+	public Weapon(Shooter s, int ammo) {
+		this(s, ammo, 0);
 	}
-	
-	public int getAmmo(){
+
+	public Weapon(Shooter s, int ammo, double damage) {
+		player = s;
+		ammoCount = ammo;
+		this.damage = damage;
+	}
+
+	/**
+	 * Add ammo. Used for bonuses
+	 * 
+	 * @param ammo
+	 *            additional ammo
+	 */
+	public void addAmmo(int ammo) {
+		ammoCount += ammo;
+	}
+
+	/**
+	 * Get the remaining ammo. Used for display
+	 * 
+	 * @return number of remaining ammo
+	 */
+	public int getAmmo() {
 		return ammoCount;
 	}
-	
-	public void fire(){
-		if(ammoCount>0)
+
+	/**
+	 * Get the orientation of the player/weapon
+	 * 
+	 * @return orientation of the player/weapon
+	 */
+	public double getOrientation() {
+		return player.getOrientation();
+	}
+
+	/**
+	 * Get the weapon damage. Used for damage calculations in the game. Returns
+	 * a random damage within 10% of the weapon damage
+	 * 
+	 * @return weapon damage
+	 */
+	public double getDamage() {
+		return (1 + (Math.random() * 2 - 1) * 0.1) * damage;
+	}
+
+	/**
+	 * Checks if there's ammo left. If so, fire weapon and subtract from
+	 * ammoCount
+	 */
+	public void fire() {
+		if (ammoCount > 0)
 			fireBullets();
 		ammoCount--;
 	}
-	
-	public double getOrientation(){
-		return player.getOrientation();
-	}
+
+	/**
+	 * Specifies how and how many bullets are created every time the weapon
+	 * fires. To be overwritten in subclasses
+	 */
 	protected abstract void fireBullets();
-	
+
 	/**
 	 * Creates a bullet whose orientation is specified by angle
-	 * @param angle orientation of the bullet to be created
+	 * 
+	 * @param angle
+	 *            orientation of the bullet to be created
 	 */
-	
 	protected void makeBullet(double angle) {
-		player.addBulletToGame(new Bullet(player.getX(),player.getY(),angle),angle);
+		player.addBulletToGame(new Bullet(player.getX(), player.getY(), angle),
+				angle);
 	}
 }
