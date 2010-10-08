8353015bc554c933066f44c59eda141add5d09ec
acue <acue@mac.com>
2010-10-08 05:28:05 +0000

    Fixed moving platforms and added several animating monsters

---
+++ src/vooga/games/doodlejump/MovingMonster.java
@@ -0,0 +1,39 @@
+package vooga.games.doodlejump;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.golden.gamedev.object.Sprite;
+
+public class MovingMonster extends Sprite{
+	
+	public MovingMonster(BufferedImage image, double x, double y){
+		super(image, x, y);
+		setHorizontalSpeed(-0.2);
+	}
+	
+	@Override
+	public void update(long elapsedTime){
+		super.update(elapsedTime);
+		if(getX() < 0 || getX() > 532 - getWidth()){
+			if(getX() < 0){
+				try {
+					setImage(ImageIO.read(new File("src/vooga/games/doodlejump/images/blue_monster_right.png")));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+				try {
+					setImage(ImageIO.read(new File("src/vooga/games/doodlejump/images/blue_monster_left.png")));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			setHorizontalSpeed(getHorizontalSpeed() * -1);
+		}
+	}
+}
