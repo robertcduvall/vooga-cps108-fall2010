470f2f7ce94c944c3264f24d3985b3ad5e456528
justin.goldsmith613@gmail.com <justin.goldsmith613@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-08 05:48:48 +0000

    updates to how game reacts with mouse

--- trunk/src/vooga/games/towerdefense/PlayerCursor.java
+++ trunk/src/vooga/games/towerdefense/PlayerCursor.java
@@ -8,23 +8,26 @@ import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
 import com.golden.gamedev.util.ImageUtil;
 
+import vooga.engine.core.Game;
 import vooga.engine.player.control.PlayerSprite;
 
 public class PlayerCursor extends PlayerSprite{
 	
 	private static final long serialVersionUID = -8174656868252384431L;
 	SpriteGroup towerGroup;
+	Game myGame;
 
-	public PlayerCursor(String name, String stateName, Sprite s, SpriteGroup towerGroup) {
+	public PlayerCursor(String name, String stateName, Sprite s, SpriteGroup towerGroup, Game game) {
 		super(name, stateName, s);
 		this.towerGroup = towerGroup;
+		myGame = game;
 	}
 	
 	public void buildTower() throws MalformedURLException{
 		try {
 			File file = new File("src/vooga/games/towerdefense/resources/images/tower.png");
 			URL imageURL = file.toURI().toURL();
-			Sprite image = new Sprite(ImageUtil.getImage(imageURL), getX(), getY());
+			Sprite image = new Sprite(ImageUtil.getImage(imageURL), myGame.getMouseX(), myGame.getMouseY());
 			towerGroup.add(image);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
