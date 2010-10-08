7566e4372299ea6a501f87ce17c0c6d6d6a304f2
acue@mac.com <acue@mac.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-08 06:18:18 +0000

    Finished all monster animations

--- trunk/src/vooga/games/doodlejump/JigglingFlyingMonster.java
+++ trunk/src/vooga/games/doodlejump/JigglingFlyingMonster.java
@@ -4,18 +4,13 @@ import java.awt.image.BufferedImage;
 
 import com.golden.gamedev.object.AnimatedSprite;
 
-/**
- * The FlyingMonster class extends AnimatedSprite and defines a FlyingMonster 
- * 
- * @author Adam Cue, Marcus Molchany, Nick Straub
- *
- */
-public class FlyingMonster extends AnimatedSprite{
+public class JigglingFlyingMonster extends AnimatedSprite{
+	private int jiggleTimer;
 	
-	public FlyingMonster(BufferedImage[] images, double x, double y){
+	public JigglingFlyingMonster(BufferedImage[] images, double x, double y){
 		super(images, x, y);
+		jiggleTimer = 20;
 		setHorizontalSpeed(-0.2);
-		setVerticalSpeed(-0.1);
 		setAnimationFrame(0, 4);
 		setLoopAnim(true);
 		setAnimate(true);
@@ -24,8 +19,10 @@ public class FlyingMonster extends AnimatedSprite{
 	@Override
 	public void update(long elapsedTime){
 		super.update(elapsedTime);
-		if(getX() < 0 || getX() > 532 - getWidth()){
+		jiggleTimer--;
+		if(jiggleTimer < 0){
 			setHorizontalSpeed(getHorizontalSpeed() * -1);
+			jiggleTimer = 20;
 		}
 	}
 }
