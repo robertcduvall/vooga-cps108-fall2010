15070e6a4f80b9b13f1b2046be614b4d81c4d398
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-08 02:46:16 +0000

    additional functionality

--- src/vooga/games/zombieland/Shooter.java
+++ src/vooga/games/zombieland/Shooter.java
@@ -13,104 +13,114 @@ public class Shooter extends PlayerSprite {
 	private int weaponChoice;
 	private Weapon[] weapons;
 	private Stat<Integer> health;
-	
-	
+
 	public Shooter(String name, String stateName, AnimatedSprite s,
 			int playerHealth, int playerRank, Zombieland zombieland) {
 		super(name, stateName, s, playerHealth, playerRank);
-		game=zombieland;
-		weapons=new Weapon[3];
-		
-		//DEFAULT values
+		game = zombieland;
+		weapons = new Weapon[3];
+
+		// DEFAULT values
 		speed = -1;
-		orientation=90;
-		weaponChoice=2;
-		weapons[0]=new Pistol(this,99999);
-		weapons[1]=new AssaultRifle(this, 100);
-		weapons[2]=new ShotGun(this, 40);
+		orientation = 90;
+		weaponChoice = 0;
+		weapons[0] = new Pistol(this, 99999);
+		weapons[1] = new AssaultRifle(this, 100);
+		weapons[2] = new ShotGun(this, 40);
 
 		health = new Stat<Integer>(playerHealth);
 	}
 
-	
 	public void goLeft() {
-		orientation=180;
-		
-		if(healthIsZero()) return;
+		orientation = 180;
+
+		if (healthIsZero())
+			return;
 		setToCurrentSprite("Left");
 		moveX(speed);
 		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goRight() {
-		orientation=0;
-		if(healthIsZero()) return;
+		orientation = 0;
+		if (healthIsZero())
+			return;
 		setToCurrentSprite("Right");
 		moveX(Math.abs(speed));
 		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goUp() {
-		orientation=270;
-		if(healthIsZero()) return;
+		orientation = 270;
+		if (healthIsZero())
+			return;
 		setToCurrentSprite("Up");
 		moveY(speed);
 		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
 
 	public void goDown() {
-		if(healthIsZero()) return;
-		orientation=90;
+		if (healthIsZero())
+			return;
+		orientation = 90;
 		setToCurrentSprite("Down");
 		moveY(Math.abs(speed));
 		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 	}
-	
+
+	public void switchWeapons(int choice){
+		if(choice<=3)
+			weaponChoice=choice;
+	}
 	/**
-	 * Initiate the shooting animation and fire bullets according to weapon choice
+	 * Initiate the shooting animation and fire bullets according to weapon
+	 * choice
 	 */
 	public void shoot() {
-		
-		if(healthIsZero()) return;
-		
+
+		if (healthIsZero())
+			return;
+
 		setToCurrentSprite("Shoot");
 		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
 		fireBullets();
 	}
-	
-	public Stat<Integer> getHealth(int placeholder){
+
+	public Stat<Integer> getHealth(int placeholder) {
 		return health;
 	}
-	
+
 	private void fireBullets() {
 		weapons[weaponChoice].fire();
 	}
-	
-	public void addBulletToGame(Bullet bullet, double angle){
-		game.addBullet(bullet,angle);
-	}
-	
-	public double getOrientation(){
-		return orientation;
-	}
-	
-	public void setHealth(int i) {
-        super.setHealth(i);
-        health.setStat(getHealth());
-	}
-	
-	public void update(long elapsedTime) {
-		AnimatedSprite sprite=(AnimatedSprite) getCurrentSprite();
-		super.update(elapsedTime);
 
-		sprite.setAnimate(false);
-		//		sprite.setFrame(0);
+	public void addBulletToGame(Bullet bullet, double angle) {
+		game.addBullet(bullet, angle);
 	}
-	
-	public boolean healthIsZero()
-	{
+
+	public void setHealth(int i) {
+		super.setHealth(i);
+		health.setStat(getHealth());
+	}
+
+	public boolean healthIsZero() {
 		return (getHealth() <= 0);
 	}
 
-	
+	public double getOrientation() {
+		return orientation;
+	}
+
+	public int getAmmo() {
+		return weapons[weaponChoice].getAmmo();
+	}
+
+	public void update(long elapsedTime) {
+		AnimatedSprite sprite = (AnimatedSprite) getCurrentSprite();
+		super.update(elapsedTime);
+
+		sprite.setAnimate(false);
+		// sprite.setFrame(0);
+	}
+
 }
