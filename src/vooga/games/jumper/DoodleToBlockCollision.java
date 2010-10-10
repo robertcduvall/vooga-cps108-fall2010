c305777ca5358937d0ebec24a629abe5c012ba33
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-10 20:00:36 +0000

    added a spring block that if you step on bounces you up in the air.  it's sick.

--- src/vooga/games/jumper/DoodleToBlockCollision.java
+++ src/vooga/games/jumper/DoodleToBlockCollision.java
@@ -10,13 +10,17 @@ import com.golden.gamedev.object.collision.AdvanceCollisionGroup;
  *
  */
 public class DoodleToBlockCollision extends AdvanceCollisionGroup {
-
+	private double springVelocityMultiplier = 15.0;
+	
 	/**
 	 * Create new Collision constructor
 	 */
+	private String myAction;
 	public DoodleToBlockCollision(){
 		pixelPerfectCollision = true;
+		String myAction = new String("stand");
 	}
+	
 
 	/**
 	 * React if there is a collision between player and block
@@ -26,12 +30,18 @@ public class DoodleToBlockCollision extends AdvanceCollisionGroup {
 	@Override
 	public void collided(Sprite doodle, Sprite block) {
 
-		System.out.println(super.getCollisionSide());
-		if(super.getCollisionSide() == 8){
-		doodle.setVerticalSpeed(block.getVerticalSpeed());
+		if(super.getCollisionSide() == 8){ //if collision = doodle feet to block...
+			
+			if (block.getID()==1){ //if collision with normal block....
+				doodle.setVerticalSpeed(block.getVerticalSpeed()); //stand on block
+			} else if(block.getID()==2){ //if collision with springBlock...
+				doodle.setVerticalSpeed(block.getVerticalSpeed()*springVelocityMultiplier); //bounce
+			} /*else if(block.getID()==3){ //if collision with breakingBlock...
+				doodle.setVerticalSpeed(block.getVerticalSpeed()); //doodlespeed = speed of block
+			}*/
 		}
 		//How to disallow horizontally walking through blocks?  Btw, is there a more elegant way to
-		//allow different actions based on different vals of getCollisionSide()?
+		//allow different actions based on different vals of getCollisionSide()? --devon
 		/* else if(super.getCollisionSide() == 2){
 			doodle.setHorizontalSpeed(1);
 		} else if(super.getCollisionSide() == 4){
@@ -39,6 +49,7 @@ public class DoodleToBlockCollision extends AdvanceCollisionGroup {
 		}
 		*/
 	}
+	
 	
 	
 }
