73d9c4f4083060e18c1c92381fe9a9573230a071
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-19 01:12:02 +0000

    Used GameState package to add gameStates, so that game can now pause.

---
+++ trunk/src/vooga/games/zombieland/ZombieGameStates.java
@@ -0,0 +1,14 @@
+package vooga.games.zombieland;
+
+import com.golden.gamedev.object.SpriteGroup;
+
+import vooga.engine.state.GameState;
+
+public class ZombieGameStates extends GameState{
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}	
+}
