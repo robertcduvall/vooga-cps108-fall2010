36e7bc900f93f856e18058e0b22dcc6ee64ba10c
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-10-19 04:05:16 +0000

    Add interface constants

---
+++ trunk/src/vooga/games/zombieland/IZombielandConstants.java
@@ -0,0 +1,12 @@
+package vooga.games.zombieland;
+
+import java.util.ResourceBundle;
+
+public interface IZombielandConstants {
+	
+	static final String PLAYER_CLASS = "vooga.games.zombieland.Shooter";
+	static final String MAIN_RESOURCES_PATH = "vooga.games.zombieland.MainResources";
+	static final ResourceBundle bundle = ResourceBundle.getBundle(MAIN_RESOURCES_PATH);
+	
+	static final String DELIM = bundle.getString("delim");
+}
