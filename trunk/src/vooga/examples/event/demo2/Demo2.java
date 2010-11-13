c82a85ba5812415fb6c75dee4c5175da6b1df4ea
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-13 19:43:30 +0000

    add more comments to help developer to understand how to use event system

--- trunk/src/vooga/examples/event/demo2/Demo2.java
+++ trunk/src/vooga/examples/event/demo2/Demo2.java
@@ -12,13 +12,20 @@ public class Demo2 {
 	}
 	
 	public void gameLoop(){
+		// how to setup the event before the gameloop
 		EventPool eventPool = EventPool.getInstance();
 		PacmanEvent pacmanEvent = new PacmanEvent(pacman,ghost);
 		eventPool.addEvent(pacmanEvent);
+		//
 		int pacmanSpeed = 1;
 		int ghostSpeed = 2;
 		while(true){
+			//this will check all the registered events for the game, user don't need to write any if
+			// and else statement, also user doesn't need check when to fire event. The eventPool will fire
+			//the event for you as long as the condition in the PacmanEvent has been met .
 			eventPool.checkEvents();
+			//user has flexibility to remove a certain event
+			//eventPool.removeEvent(pacmanEvent);
 			pacman.setX(pacman.getX()+pacmanSpeed);
 			ghost.setX(ghost.getX()+ghostSpeed);
 			System.out.println("Pacman position: " +pacman.getX()+";"+ "Ghost position: "+ghost.getX());
