b42cba318a789404e05669901f2a4b56344e0633
bhawana686 <bhawana686@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-13 21:19:00 +0000

    New class VoogaPlayfield added to the core package. VoogaPlayfield extends GoldenT Playfield to include OverlayTracker.

---
+++ trunk/src/vooga/engine/core/VoogaPlayfield.java
@@ -0,0 +1,53 @@
+package vooga.engine.core;
+
+import java.util.ArrayList;
+
+import vooga.engine.overlay.OverlayTracker;
+
+import com.golden.gamedev.object.Background;
+import com.golden.gamedev.object.PlayField;
+import com.golden.gamedev.object.SpriteGroup;
+
+/**
+ * Extension of the Golden T Playfield to allow addition of OverlayTracker to the Playfield.
+ * Example:
+ * <pre>
+ * <code>
+ * VoogaPlayfield playfield = new VoogaPlayfield();
+ * OverlayTracker track = OverlayCreator.createOverlays(String xmlFileLocation);
+ * playfield.addOverlayTracker(track);
+ * </pre>
+ * </code>
+ * @author Bhawana , Cameron , Derek
+ *
+ */
+public class VoogaPlayfield extends PlayField {
+	
+	public VoogaPlayfield(Background background){
+		super(background);
+	}
+	
+	public VoogaPlayfield() {
+		super();
+	}
+	
+	/**
+	 * Adds OverlayTracker to the playfield.
+	 * @param overlayTracker
+	 */
+	public void addOverlayTracker(OverlayTracker overlayTracker){
+		addOverlayGroups(overlayTracker.getOverlayGroups());
+	}
+
+
+	/**
+	 * Adds every SpriteGroup from the OverlayTracker to the playfield.
+	 * @param overlayGroups
+	 */
+	private void addOverlayGroups(ArrayList<SpriteGroup> overlayGroups) {
+		for(SpriteGroup overlay : overlayGroups){
+			this.addGroup(overlay);
+		}
+	}
+
+}
