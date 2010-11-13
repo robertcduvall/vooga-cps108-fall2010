b0ab2d6c970667021a5b331a21034744a32b6e6a
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-13 19:38:32 +0000

    Second edition event demo, help developer and other API team to know how to use our event system

---
+++ src/vooga/examples/event/demo2/Ghost.java
@@ -0,0 +1,17 @@
+package vooga.examples.event.demo2;
+
+public class Ghost {
+	private int x;
+
+	Ghost(int x) {
+		this.x = x;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+}
