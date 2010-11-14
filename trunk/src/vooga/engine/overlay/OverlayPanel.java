f0de5421780fbe3fc850283b240eba24caaef51b
sfeldsott <sfeldsott@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-14 20:49:13 +0000

    Refactored out some constants and methods; updated the comments a bit.

--- trunk/src/vooga/engine/overlay/OverlayPanel.java
+++ trunk/src/vooga/engine/overlay/OverlayPanel.java
@@ -6,10 +6,16 @@ import com.golden.gamedev.object.SpriteGroup;
 
 /**
  * The OverlayPanel provides a convenient way for users
- * to evenly place up to 5 overlays across the top or
- * bottom of the screen.
+ * to evenly place overlays across the top or
+ * bottom of the screen.  About 3 overlays or less is
+ * recommended since OverlayPanel does not take measures
+ * to prevent overlapping.
  * 
  * <pre>
+ * OverlayBar	overlay1 = new OverlayBar(stat1,100);
+ * OverlayIcon	overlay2 = new OverlayIcon(stat2, image, "Lives: ");
+ * OverlayStat	overlay3 = new OverlayStat("Score: ", stat3);
+ * 
  * OverlayPanel panel = new Overlay("Overlays", someGame, true);
  * panel.add(overlay1);
  * panel.add(overlay2);
@@ -21,6 +27,10 @@ import com.golden.gamedev.object.SpriteGroup;
  */
 public class OverlayPanel extends SpriteGroup{
 
+	private static final double OVERLAY_OFFSET = 1.7;
+	private static final int NEGATIVE = -1;
+	private static final int EXTRA_PIXEL = 1;
+	private static final int MIDDLE = 2;
 	private boolean isOnTop;
 	private int screenHeight;
 	private int screenWidth;
@@ -40,16 +50,16 @@ public class OverlayPanel extends SpriteGroup{
 	}
 	
 	/**
-	 * This calculates important values in the OverlayPanel
-	 * regarding spacing of the Overlays and it also places
-	 * the OverlayPanel into the world.  It also builds the
-	 * Overlay SpriteGroup.
+	 * This sets up the panel by calculating important values
+	 * in the OverlayPanel regarding spacing of the Overlays
+	 * and it also places the OverlayPanel into the world.
+	 * It also builds the Overlay SpriteGroup.
 	 */
 	public void initialize()
 	{
 		int numberOfOverlays = this.getSize();
 		int[] xCoordinates = setXCoordinates(numberOfOverlays);
-		int distanceFromEdge = screenHeight/50;
+		int distanceFromEdge = twoPercentOfScreenHeight();
 		int yCoordinate = distanceFromEdge;
 		int yOffset = 0;	//The offsets are for in case the Overlay would run
 		int xOffset = 0;	//off the screen.
@@ -64,16 +74,17 @@ public class OverlayPanel extends SpriteGroup{
 				this.remove(overlay);
 				continue;
 			}
-			xOffset = (-1)*overlay.getWidth()/2;
+			xOffset = NEGATIVE*getHalf(overlay.getWidth());
 			
-			if(overlay.getHeight()/2 > distanceFromEdge)
-				yOffset = overlay.getHeight()/2 - distanceFromEdge + 1;
+			if(getHalf(overlay.getHeight()) > distanceFromEdge)
+				yOffset = getHalf(overlay.getHeight()) - distanceFromEdge + EXTRA_PIXEL;
 			if(!isOnTop)
-				yOffset*=(-1);
-			if(i==0 && numberOfOverlays > 1)
+				yOffset*=NEGATIVE;
+			if(i==0 && moreThanOneOverlay(numberOfOverlays))
 				xOffset = 0;
-			else if(i==numberOfOverlays-1 && numberOfOverlays > 1)
-				xOffset -= overlay.getWidth()*1.7;
+			else if(notLastOverlay(numberOfOverlays, i) &&
+					moreThanOneOverlay(numberOfOverlays))
+				xOffset -= overlay.getWidth()*OVERLAY_OFFSET;
 			overlay.setLocation(xCoordinates[i]+xOffset, yCoordinate+yOffset);
 			xOffset = 0;
 			yOffset = 0;
@@ -81,12 +92,30 @@ public class OverlayPanel extends SpriteGroup{
 			i++;
 		}
 	}
+
+	private boolean moreThanOneOverlay(int numberOfOverlays) {
+		return numberOfOverlays > 1;
+	}
+
+	private boolean notLastOverlay(int numberOfOverlays, int i) {
+		return i==numberOfOverlays-1;
+	}
+
+	private int twoPercentOfScreenHeight() {
+		return screenHeight/50;
+	}
 	
+	/**
+	 * Calculates the x-coordinates for the horizontal
+	 * spacing of the overlays in OverlayPanel.
+	 * @param numberOfOverlays
+	 * @return xCoordinates
+	 */
 	private int[] setXCoordinates(int numberOfOverlays)
 	{
 		int[] xCoordinates = new int[numberOfOverlays];
 		if(numberOfOverlays == 1)
-			xCoordinates[0] = screenWidth/2;
+			xCoordinates[0] = screenWidth/MIDDLE;
 		else
 		{
 			int xIncrement = screenWidth / (numberOfOverlays-1);
@@ -98,4 +127,13 @@ public class OverlayPanel extends SpriteGroup{
 		return xCoordinates;
 	}
 	
+	/**
+	 * Returns half of the passed value.
+	 * @param number
+	 * @return number / 2
+	 */
+	private int getHalf(int number)
+	{
+		return number / 2;
+	}
 }
