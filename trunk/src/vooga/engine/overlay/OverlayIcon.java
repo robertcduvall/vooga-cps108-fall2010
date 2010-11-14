a797949dc9155d936008c1bd4015c3d3bf07337b
sfeldsott <sfeldsott@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-14 20:58:04 +0000

    Made OverlayStatImage part of Icon;  Added and fixed some javadoc comments.

--- trunk/src/vooga/engine/overlay/OverlayIcon.java
+++ trunk/src/vooga/engine/overlay/OverlayIcon.java
@@ -13,21 +13,20 @@ import vooga.engine.resource.Resources;
 import com.golden.gamedev.object.GameFont;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.util.ImageUtil;
 
 /**
- * This Overlay class displays a finite number of icons, with the image provided by the user
- * @author Se-Gil Feldsott and Justin Goldsmith
+ * This Overlay class displays a finite number of icons, with an image provided by the user.
  * 
- * <xmp>
+ * <pre>
  * <p>Stat<Integer> stat = new Stat<Integer>(5);</p>
  * <p>GreenfootImage image = new GreenfootImage("...");</p>
  * <p>OverlayIcon overlay = new OverlayIcon(stat, image, "Lives: ");</p>
- * </xmp>
+ * </pre>
  * 
- *  <p>All overlays must be updated and rendered, This is the responsibility of the game creator</p>
+ * <p>All overlays must be updated and rendered; This is the responsibility of the game creator.</p>
  * 
- * 
- *
+ * @author Se-Gil Feldsott and Justin Goldsmith
  */
 public class OverlayIcon extends Overlay {
 
@@ -37,10 +36,10 @@ public class OverlayIcon extends Overlay {
 	private Stat<Integer> myStatKeeperGen;
 	private OverlayString myText;
 	private SpriteGroup myIconGroup;
-		
+
 	/**
 	 * 
-	 * Creats Overlay Icon class that is Integer specific
+	 * Creates Overlay Icon class that is Integer specific
 	 * @param stat Stat object that contains an Integer to keep track of
 	 * @param icon Icon to display
 	 * @param label String to label the icons
@@ -50,31 +49,28 @@ public class OverlayIcon extends Overlay {
 		myText = new OverlayString(label);
 		myStatKeeperGen = stat;
 		myIcon = new OverlayStatImage(icon);
-		myNumOfIcons = stat.getStat().intValue();
-		myPreviousNumOfIcons = 0;
-		myIconGroup = new SpriteGroup("Icons");
-		updateIcon();
-	}
-	
-	public void scaleIcon(int width, int height){
-		myIcon.scale(width, height);
-	}
-	
-	
-	public OverlayIcon(Map<String, String> attributes, OverlayTrackerTemp tracker){
-		myText = new OverlayString(attributes, tracker);
-		myIcon = new OverlayStatImage(attributes, tracker);
-		setLocation(attributes);
-		String statName = attributes.get("stat");
-		myStatKeeperGen = tracker.getStat(statName, new Integer(1));
 		myNumOfIcons = myStatKeeperGen.getStat().intValue();
 		myPreviousNumOfIcons = 0;
 		myIconGroup = new SpriteGroup("Icons");
 		updateIcon();
-		
 	}
-	
-		
+
+	public OverlayIcon(Map<String, String> attributes, OverlayTrackerTemp tracker){
+		this(tracker.getStat(attributes.get("stat"), new Integer(1)), null, null);
+		myText = new OverlayString(attributes, tracker);
+		myIcon = new OverlayStatImage(attributes, tracker);
+		setLocation(attributes);
+	}
+
+	/**
+	 * Adjusts the icon to be the given width, height.
+	 * @param width
+	 * @param height
+	 */
+	public void scaleIcon(int width, int height){
+		myIcon.scale(width, height);
+	}
+
 	/**
 	 * Used to render to the screen.
 	 * @param g Graphic to render image to.
@@ -85,10 +81,10 @@ public class OverlayIcon extends Overlay {
 		myText.print(myText.getString(), g);
 		myIconGroup.render(g);
 	}
-	
-	
-	
-	
+
+
+
+
 	/**
 	 * Updates the number of images showing
 	 */
@@ -103,7 +99,7 @@ public class OverlayIcon extends Overlay {
 		updateIcon();
 		myIconGroup.update(t);
 	}
-	
+
 	private void updateIcon()
 	{
 		//Loops until the previous number of icons equals the new number of icons
@@ -113,7 +109,7 @@ public class OverlayIcon extends Overlay {
 				myIconGroup.add(myIcon.clone());
 				myPreviousNumOfIcons++;
 			}
-				
+
 			else if(myNumOfIcons < myPreviousNumOfIcons)
 			{
 				myIconGroup.remove(myIconGroup.getSize() - 1);
@@ -136,10 +132,10 @@ public class OverlayIcon extends Overlay {
 				}
 			}
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * @return width of the Overlay Icon including String and all icons
 	 */
@@ -150,26 +146,65 @@ public class OverlayIcon extends Overlay {
 		int locOfLastIcon = (int)myIconGroup.getSprites()[(myIconGroup.getSize() - 1)].getX();
 		int endLocOfLastIcon = locOfLastIcon + myIconGroup.getActiveSprite().getImage().getWidth();
 		return endLocOfLastIcon - begLocOfString;
-		
+
 	}
-	
-	
+
+
 	public void setFont(Font font){
 		myText.setFont(font);
 	}
-	
+
 	public void setFont(GameFont font){
 		myText.setFont(font);
 	}
-	
+
 	public void setColor(Color color){
 		myText.setColor(color);
 	}
-	
+
 	protected OverlayString getOverlayString(){
 		return myText;
 	}
 
+	/**
+	 * The OverlayStatImage class is essentially a Sprite
+	 * wrapper used by the OverlayIcon class to display
+	 * the icons.  It has a cloning method which allows
+	 * the OverlayIcon to easily display multiples of the
+	 * icon image.
+	 * @author Se-Gil Feldsott and Justin Goldsmith
+	 */
+	private class OverlayStatImage extends Overlay{
+
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Creates a Image to be placed in game.
+		 * @param image image to display
+		 */
+		public OverlayStatImage(BufferedImage image){
+			setImage(image);
+		}
+
+		public OverlayStatImage(Map<String, String> attributes, OverlayTrackerTemp tracker){
+			this(Resources.getImage(attributes.get("image")));
+			String width = attributes.get("width");
+			String height = attributes.get("height");
+			if(width!=null && height!=null){
+				setImage(ImageUtil.resize(this.getImage(), Integer.valueOf(width), Integer.valueOf(height)));
+			}
+			setLocation(attributes);
+		}
+
+		protected void scale(int width, int height){
+			setImage(ImageUtil.resize(this.getImage(), width, height));
+		}
+
+		protected OverlayStatImage clone(){
+			return new OverlayStatImage(getImage());
+		}
+
+	}
 
 }
 
