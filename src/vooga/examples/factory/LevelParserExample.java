9f66f29e21276d7efd32d17e09e1966234f0f0f9
sailing.thru.gravity <sailing.thru.gravity@gmail.com>
2010-11-15 03:45:07 +0000

    Added random spawn to example

--- src/vooga/examples/factory/LevelParserExample.java
+++ src/vooga/examples/factory/LevelParserExample.java
@@ -7,6 +7,11 @@ import vooga.engine.factory.LevelParser;
 This class provides full implementation for LevelParser.
  * An example of how this could be used in a game is as follows:
  * To understand the formatting of the XML file look at tutorialXML
+ * 
+ * This ultimate purpose of the class is to turn an .xml file into a VoogaPlayField,
+* which will contain all resources that constitute a level. This level, in the form of a
+* VoogaPlayField, can be used to construct a GameState for the given level, which should
+* handle rendering and updating of the level.
  * @author Derek
 *
 */
