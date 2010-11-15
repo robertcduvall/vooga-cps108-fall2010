bde746c4316d40a5fdad5e16b91cb7376fb5d8ab
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-15 01:32:38 +0000

    worked on RuleCondition

--- src/vooga/engine/level/RuleCondition.java
+++ src/vooga/engine/level/RuleCondition.java
@@ -1,5 +1,35 @@
 package vooga.engine.level;
 
-public class RuleCondition {
+import java.util.List;
+import vooga.engine.core.*;
 
+public abstract class RuleCondition {
+
+	/**
+	 * This class provides a framework to add conditions to rules
+	 */
+	List<GameEntitySprite> spritesAffected;
+	
+	
+	
+	/**
+	 * constructor
+	 */
+	public RuleCondition () {
+		
+	}
+	
+	/**
+	 * adds a single GameEntitySprite to the List spritesAffected by the rule.
+	 * @param ges
+	 */
+	public void addSpriteAffected (GameEntitySprite ges) {
+		spritesAffected.add(ges);
+	}
+	
+	
+	
+	
+	
+	
 }
