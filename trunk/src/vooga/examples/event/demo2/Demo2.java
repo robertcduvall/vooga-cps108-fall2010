179cce9c43443b809b7993d945835eb5493d4bfa
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-15 05:06:54 +0000

    Demo2 javadoc

--- trunk/src/vooga/examples/event/demo2/Demo2.java
+++ trunk/src/vooga/examples/event/demo2/Demo2.java
@@ -1,16 +1,28 @@
 package vooga.examples.event.demo2;
 
 import vooga.engine.event.EventPool;
-
+/**
+ * This demo <code>Demo2</code> shows how to use {@link #EventPool EventPool} and {@link #IEventHandler IEventHandler} to create 
+ * events in game.
+ * <p>
+ * Taking Pacman game for example, ghosts, enemies of pacman, are trying to catch pacman in one line and end the game. The
+ * speed of ghost is faster than pacman. Here is the fire event condition: when the ghost catches 
+ * up with pacman which indicated by their x position, the pacman will be killed the killevent will 
+ * be triggered. In this case, we can write a event class such as PacmanKilledEvent.Add it in the
+ * EventLoop class. And the eventLoop will check the event for you. 
+ * </p>
+ * @author Meng Li
+ *
+ */
 public class Demo2 {
 	private Pacman pacman;
 	private Ghost ghost; 
-	
+
 	public Demo2(){
 		pacman = new Pacman(10);
 		ghost = new Ghost(2);
 	}
-	
+
 	public void gameLoop(){
 		// how to setup the event before the gameloop
 		EventPool eventPool = EventPool.getInstance();
@@ -31,7 +43,7 @@ public class Demo2 {
 			System.out.println("Pacman position: " +pacman.getX()+";"+ "Ghost position: "+ghost.getX());
 		}
 	}
-	
+
 	public static void main(String[] args){
 		Demo2 demo = new Demo2();
 		demo.gameLoop();
