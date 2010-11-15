d1bc24d2397b8bc32c5354ffbce9fbc8be8ac28f
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-15 05:11:37 +0000

    Updates to DBHandler, profile resource example

--- src/vooga/engine/resource/DBHandler.java
+++ src/vooga/engine/resource/DBHandler.java
@@ -1,47 +1,179 @@
 package vooga.engine.resource;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import com.almworks.sqlite4java.SQLiteConnection;
 import com.almworks.sqlite4java.SQLiteException;
 import com.almworks.sqlite4java.SQLiteStatement;
 
 /**
- * DBHandler is a class that allows static access to a database for reading and writing data.
+ * DBHandler is a class that allows static access to a database for reading and
+ * writing data.
  * 
  * @author David Herzka
- *
+ * 
  */
 public class DBHandler {
 	private static SQLiteConnection dbConnection_;
 	private static String table_;
-	
+
 	public static boolean open(String dbFileName) {
-		dbConnection_=new SQLiteConnection(new File(dbFileName));
+		dbConnection_ = new SQLiteConnection(new File(dbFileName));
 		try {
 			dbConnection_.open(true);
 		} catch (SQLiteException e) {
-			return false;
+			throw new RuntimeException(e);
 		}
 		return true;
 	}
-	
-	public static boolean selectTable(String tableName,Column... columns) {
-		table_=tableName;
-		StringBuilder query = new StringBuilder("CREATE TABLE " + tableName + " (");
-		for(Column column : columns) {
-			query.append(column.getDeclaration()+",");
+
+	public static boolean createTable(String tableName, Column... columns) {
+		StringBuilder query = new StringBuilder("CREATE TABLE " + tableName
+				+ " (");
+		for (Column column : columns) {
+			query.append(column.getDeclaration() + ",");
 		}
-		query.deleteCharAt(query.length()-1);
+		query.deleteCharAt(query.length() - 1);
 		query.append(")");
 		try {
-			SQLiteStatement statement = null;
-			statement = dbConnection_.prepare(query.toString());
+			SQLiteStatement statement = dbConnection_.prepare(query.toString());
 			statement.step();
 		} catch (SQLiteException e) {
 			return false;
 		}
 		return true;
 	}
-	
+
+	public static boolean createTable(String tableName, String... columnNames) {
+		Column[] columns = new Column[columnNames.length];
+		for (int i = 0; i < columnNames.length; i++) {
+			columns[i] = new Column(columnNames[i], Column.NONE);
+		}
+		return createTable(tableName, columns);
+	}
+
+	public static boolean setTable(String tableName) {
+		String query = "SELECT name FROM sqlite_master WHERE name = '"
+				+ tableName + "'";
+		SQLiteStatement statement = null;
+		try {
+			statement = dbConnection_.prepare(query);
+			statement.step();
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		if (!statement.hasRow())
+			return false;
+		table_ = tableName;
+		return true;
+	}
+
+	public static String[] getColumnStrings(String columnName) {
+		String query = "SELECT " + columnName + " FROM " + table_;
+		SQLiteStatement statement = null;
+		Collection<String> column = new ArrayList<String>();
+		try {
+			statement = dbConnection_.prepare(query);
+			statement.step();
+			while (statement.hasRow()) {
+				column.add(statement.columnString(0));
+				statement.step();
+			}
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return column.toArray(new String[0]);
+	}
+
+	public static Integer[] getColumnInts(String columnName) {
+		String query = "SELECT " + columnName + " FROM " + table_;
+		SQLiteStatement statement = null;
+		Collection<Integer> column = new ArrayList<Integer>();
+		try {
+			statement = dbConnection_.prepare(query);
+			statement.step();
+			while (statement.hasRow()) {
+				column.add(statement.columnInt(0));
+				statement.step();
+			}
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return column.toArray(new Integer[0]);
+	}
+
+	public static Double[] getColumnDoubles(String columnName) {
+		String query = "SELECT " + columnName + " FROM " + table_;
+		SQLiteStatement statement = null;
+		Collection<Double> column = new ArrayList<Double>();
+		try {
+			statement = dbConnection_.prepare(query);
+			statement.step();
+			while (statement.hasRow()) {
+				column.add(statement.columnDouble(0));
+				statement.step();
+			}
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return column.toArray(new Double[0]);
+	}
+
+	public static Boolean[] getColumnBools(String columnName) {
+		String query = "SELECT " + columnName + " FROM " + table_;
+		SQLiteStatement statement = null;
+		Collection<Boolean> column = new ArrayList<Boolean>();
+		try {
+			statement = dbConnection_.prepare(query);
+
+			while (statement.hasRow()) {
+				statement.step();
+				column.add(statement.columnInt(0) != 0);
+			}
+
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return column.toArray(new Boolean[0]);
+	}
+
+	public static boolean addRow(Entry<?>... entries) {
+		StringBuilder columnNames = new StringBuilder("");
+		StringBuilder columnValues = new StringBuilder("");
+		for (Entry<?> e : entries) {
+			columnNames.append(e.getColumnName() + ",");
+			columnValues.append("'" + e.getData().toString() + "',");
+		}
+		columnNames.deleteCharAt(columnNames.length() - 1);
+		columnValues.deleteCharAt(columnValues.length() - 1);
+
+		String query = "INSERT INTO " + table_ + "(" + columnNames.toString()
+				+ ") values(" + columnValues + ")";
+		try {
+			dbConnection_.prepare(query).step();
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return true;
+	}
+
+	public static boolean deleteRow(Entry<?>... entries) {
+		StringBuilder conditions = new StringBuilder("");
+		for (Entry<?> e : entries) {
+			conditions.append(e.getColumnName() + " LIKE '"
+					+ e.getData().toString() + "' AND");
+		}
+		conditions.delete(conditions.length() - 4, conditions.length());
+		String query = "DELETE FROM " + table_ + " WHERE "
+				+ conditions.toString();
+		try {
+			dbConnection_.prepare(query).step();
+		} catch (SQLiteException e) {
+			throw new RuntimeException(e);
+		}
+		return true;
+	}
+
 }
