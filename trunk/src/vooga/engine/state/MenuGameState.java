c0b7d8d6eb55932c6c15dbccd0d8d6759fe8f953
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-16 03:40:40 +0000

    javadoc updates

--- trunk/src/vooga/engine/state/MenuGameState.java
+++ trunk/src/vooga/engine/state/MenuGameState.java
@@ -3,10 +3,23 @@ package vooga.engine.state;
 import vooga.engine.core.PlayField;
 import vooga.widget.Button;
 
+/**
+ * MenuGameState is a reusable component extension of GameState which contains a series of Buttons
+ * @author Brent
+ * @author with a splash of Brian (comments)
+ *
+ */
 public class MenuGameState extends GameState {
 
+	/**
+	 * menuPlayfield is the desired new instance of PlayField
+	 */
 	private PlayField menuPlayfield = new PlayField();
 
+	/**
+	 * Creates a new instance of MenuGameState with a specified collection of buttons
+	 * @param buttons iterable collection of buttons
+	 */
 	public MenuGameState(Iterable<Button> buttons) {
 
 		for (Button button : buttons) {
@@ -15,11 +28,18 @@ public class MenuGameState extends GameState {
 
 	}
 
+	/**
+	 * Initializes MenuGameState
+	 */
 	@Override
 	public void initialize() {
 
 	}
 
+	/**
+	 * Adds a specified Button to the MenuGameState
+	 * @param button
+	 */
 	public void addButton(Button button) {
 
 		menuPlayfield.add(button);
