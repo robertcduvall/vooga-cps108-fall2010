1c11e226e8e59c6d1f77b46695f5bc3bf2c56452
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-16 05:01:46 +0000

    Game example updated

--- src/vooga/examples/core/Example.java
+++ src/vooga/examples/core/Example.java
@@ -1,10 +1,12 @@
 package vooga.examples.core;
 
+import java.awt.Dimension;
+
+import com.golden.gamedev.GameLoader;
+
 import vooga.engine.core.Game;
 /**
- * Copied resources files from the resource group.
  * @author Yang
- *
  */
 public class Example extends Game {
 
@@ -14,6 +16,13 @@ public class Example extends Game {
 
 		getGameStateManager().addGameState(new ExampleState1(),
 				new ExampleState2());
+	}
+	
+	public static void main(String[] args){
+		launch(new Example());
+//		GameLoader game = new GameLoader();
+//		game.setup(new Example(), new Dimension(600, 400), false);
+//		game.start();
 	}
 
 }
--- src/vooga/examples/core/ExampleState1.java
+++ src/vooga/examples/core/ExampleState1.java
@@ -1,13 +1,17 @@
 package vooga.examples.core;
 
+import com.golden.gamedev.object.SpriteGroup;
+
+import vooga.engine.core.Sprite;
+import vooga.engine.resource.Resources;
 import vooga.engine.state.GameState;
 
 public class ExampleState1 extends GameState {
-
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-
+		SpriteGroup bg=new SpriteGroup("background");
+		bg.add(new Sprite(Resources.getImage("BG")));
+		addGroup(bg);
 	}
 
 }
