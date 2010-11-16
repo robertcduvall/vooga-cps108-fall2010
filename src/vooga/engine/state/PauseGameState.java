ed754947bcd24e3bdc1345fdc20dfb8fb695733a
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-16 03:40:40 +0000

    javadoc updates

--- src/vooga/engine/state/PauseGameState.java
+++ src/vooga/engine/state/PauseGameState.java
@@ -4,13 +4,33 @@ import java.awt.Color;
 
 import vooga.engine.core.PlayField;
 import vooga.engine.overlay.OverlayString;
-
+/**
+ * PauseGameState is a reusable extension of GameState that displays a basic pause message.
+ * @author Brent
+ * @author Brian (comments)
+ *
+ */
 public class PauseGameState extends GameState {
 
+	/**
+	 * PauseOverlay is the OverlayString that will be displayed on the screen
+	 */
 	private OverlayString pauseOverlay;
+	
+	/**
+	 * Default Message is the default message to be displayed
+	 */
 	private final String DEFAULT_MESSAGE = "Paused";
+	
+	/**
+	 * BasicTextPlayfield is the instance of playfield for this class
+	 */
 	private PlayField basicTextPlayfield = new PlayField();
 
+	/**
+	 * Creates an instance of PauseGameState with a default message and color
+	 * @param previousGameState
+	 */
 	public PauseGameState(GameState previousGameState) {
 
 		this.addRenderState(previousGameState);
@@ -22,6 +42,11 @@ public class PauseGameState extends GameState {
 		addPlayField(basicTextPlayfield);
 	}
 
+	/**
+	 * Creates an instance of PauseGameState with a defaultcolor
+	 * @param previousGameState 
+	 * @param pauseMessage message to be displayed on pause
+	 */
 	public PauseGameState(GameState previousGameState, String pauseMessage) {
 
 		this.addRenderState(previousGameState);
@@ -33,6 +58,12 @@ public class PauseGameState extends GameState {
 		addPlayField(basicTextPlayfield);
 	}
 
+	/**
+	 * Creates an instance of PauseGameState
+	 * @param previousGameState 
+	 * @param pauseMessage message to be displayed on pause
+	 * @param color represents the desired color of the pause message
+	 */
 	public PauseGameState(GameState previousGameState, String pauseMessage,
 			Color color) {
 
@@ -45,6 +76,13 @@ public class PauseGameState extends GameState {
 		addPlayField(basicTextPlayfield);
 	}
 
+	/**
+	 * Creates an instance of PauseGameState with a specific location for the pause message
+	 * @param previousGameState 
+	 * @param pauseMessage message to be displayed on pause
+	 * @param x x-coordinate of the pause message
+	 * @param y y-coordinate of the pause message
+	 */
 	public PauseGameState(GameState previousGameState, String pauseMessage,
 			int x, int y) {
 
@@ -54,6 +92,14 @@ public class PauseGameState extends GameState {
 		addPlayField(basicTextPlayfield);
 	}
 
+	/**
+	 * Creates an instance of PauseGameState with a specific location for the pause message, color, and message
+	 * @param previousGameState 
+	 * @param pauseMessage message to be displayed on pause
+	 * @param color represents the desired color of the pause message
+	 * @param x x-coordinate of the pause message
+	 * @param y y-coordinate of the pause message
+	 */
 	public PauseGameState(GameState previousGameState, String pauseMessage,
 			Color color, int x, int y) {
 
@@ -63,6 +109,11 @@ public class PauseGameState extends GameState {
 		addPlayField(basicTextPlayfield);
 	}
 
+	/**
+	 * Changes the desired location of the pause message
+	 * @param x x-coordinate of the pause message
+	 * @param y y-coordinate of the pause message
+	 */
 	public void setMessagePosition(int x, int y) {
 
 		pauseOverlay.setX(x);
@@ -70,6 +121,9 @@ public class PauseGameState extends GameState {
 
 	}
 
+	/**
+	 * Initializes the PauseGameState
+	 */
 	@Override
 	public void initialize() {
 
