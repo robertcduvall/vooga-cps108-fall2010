5136cbcf0960e895ebf787e44f24310420311a4f
robertcduvall <robertcduvall@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 20:11:14 +0000

    Starting Asteroids framework

---
+++ trunk/src/vooga/games/asteroids/BlahThis.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package vooga.games.asteroids;
+
+import vooga.engine.core.Game;
+
+/**
+ * @author student
+ *
+ */
+public class BlahThis extends Game {
+
+	/**
+	 * 
+	 */
+	public BlahThis() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
