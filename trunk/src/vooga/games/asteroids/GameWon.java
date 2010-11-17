66e44c8527e4b5d3f105c0acf8150907c5e5602d
kate.l.yang <kate.l.yang@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 21:03:02 +0000

    added methods to Conditions

--- trunk/src/vooga/games/asteroids/GameWon.java
+++ trunk/src/vooga/games/asteroids/GameWon.java
@@ -1,15 +1,22 @@
 package vooga.games.asteroids;
 
+import com.golden.gamedev.object.SpriteGroup;
+
 import vooga.engine.level.GameWonConditions;
 
 public class GameWon extends GameWonConditions{
 	
+	private SpriteGroup myAsteroids;
+	
 	@Override
 	public boolean GameWon() {
-		if (getAsteroids() == 0)
+		if (myAsteroids == null)
 			return true;
 		return false;
 	}
 
+	public void getSpriteGroup(SpriteGroup asteroids){
+		myAsteroids = asteroids;
+	}
 	
 }
