4d48b08096d8c2ae0bfdaf47a8c60a78b4d83be8
bhawana686 <bhawana686@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 03:48:35 +0000

    Updated Enemy to extend core.Sprite

--- trunk/src/vooga/games/grandius/enemy/common/Enemy.java
+++ trunk/src/vooga/games/grandius/enemy/common/Enemy.java
@@ -2,29 +2,60 @@ package vooga.games.grandius.enemy.common;
 
 import java.awt.image.BufferedImage;
 
-import com.golden.gamedev.object.*;
+import vooga.engine.core.Sprite;
+import vooga.engine.overlay.Stat;
 
 /**
  * An Enemy is any adversary of the player.
  */
-public abstract class Enemy extends AnimatedSprite {
-	
+public abstract class Enemy extends Sprite {
+	//TODO - what is serialVersionUID for?
 	private static final long serialVersionUID = 1L;
-	protected int scoreValue;
-	protected int cashValue;
+	private static final String SCORE = "Score";
+	private static final String CASH = "Cash";
 	
-	public Enemy(BufferedImage[] images, double x, double y)
-	{
-		super(images, x, y);
+	public Enemy(BufferedImage[] images, double x, double y){
+		super(images);
+		this.setLocation(x, y);
 	}
 	
-	public int getScore()
-	{
-		return scoreValue;
+	public void setScore(int score){
+		Stat<Integer> scoreValue = new Stat<Integer>(score);
+		setStat(SCORE, scoreValue);
 	}
 	
-	public int getCashValue()
-	{
-		return cashValue;
+	public int getScore(){
+		return (Integer) this.getStat(SCORE).getStat();
+	}
+	
+	public void setCash(int cash){
+		Stat<Integer> cashValue = new Stat<Integer>(cash);
+		setStat(CASH, cashValue);
+	}
+	
+	public int getCash(){
+		return (Integer) this.getStat(CASH).getStat();
 	}
 }
+
+//public abstract class Enemy extends AnimatedSprite {
+//	
+//	private static final long serialVersionUID = 1L;
+//	protected int scoreValue;
+//	protected int cashValue;
+//	
+//	public Enemy(BufferedImage[] images, double x, double y)
+//	{
+//		super(images, x, y);
+//	}
+//	
+//	public int getScore()
+//	{
+//		return scoreValue;
+//	}
+//	
+//	public int getCashValue()
+//	{
+//		return cashValue;
+//	}
+//}
