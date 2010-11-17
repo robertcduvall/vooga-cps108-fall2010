50aa5b9795336d31cc34f85bacc6801f57aa7afa
laz3638 <laz3638@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 20:37:16 +0000

    added a constructor to Ship

--- src/vooga/games/asteroids/sprites/Ship.java
+++ src/vooga/games/asteroids/sprites/Ship.java
@@ -3,12 +3,25 @@ package vooga.games.asteroids.sprites;
 import vooga.engine.core.Sprite;
 
 public class Ship extends Sprite {
+	
+	private static int ROTATION_AMOUNT;
+	private static int VELOCITY_REDUCTION;
+	
+	private int myVelocity;
+	private int myOrientation;
+	
+	public Ship()
+	{
+		myVelocity = 0;
+		myOrientation = 0;
+	}
+	
 	public void rotateLeft(){
-		
+		myOrientation -= ROTATION_AMOUNT;
 	}
 	
 	public void rotateRight(){
-		
+		myOrientation += ROTATION_AMOUNT;
 	}
 	
 	public void thrust(){
