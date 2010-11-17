31908a3b515612219b3cacb652e3b09d982ed7dd
cody.ko <cody.ko@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 20:31:08 +0000

    added the ship

---
+++ src/vooga/games/asteroids/sprites/Ship.java
@@ -0,0 +1,21 @@
+package vooga.games.asteroids.sprites;
+
+import vooga.engine.core.Sprite;
+
+public class Ship extends Sprite {
+	public void rotateLeft(){
+		
+	}
+	
+	public void rotateRight(){
+		
+	}
+	
+	public void thrust(){
+		
+	}
+	
+	public void shoot(){
+		
+	}
+}
