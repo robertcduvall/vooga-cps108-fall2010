38a86af285a62afb3ef97bf50003b0a8da5f9be1
cody.ko <cody.ko@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 20:46:57 +0000

    updated ship

--- trunk/src/vooga/games/asteroids/sprites/Ship.java
+++ trunk/src/vooga/games/asteroids/sprites/Ship.java
@@ -4,8 +4,8 @@ import vooga.engine.core.Sprite;
 
 public class Ship extends Sprite {
 	
-	private static int ROTATION_AMOUNT;
-	private static int VELOCITY_REDUCTION;
+	private static double ROTATION_AMOUNT;
+	private static double VELOCITY_REDUCTION;
 	
 	private double myVelocity;
 	private double myOrientation;
@@ -14,6 +14,8 @@ public class Ship extends Sprite {
 	{
 		myVelocity = 0;
 		myOrientation = 0;
+		ROTATION_AMOUNT = 1;
+		VELOCITY_REDUCTION = .1;
 	}
 	
 	public void rotateLeft(){
@@ -25,10 +27,17 @@ public class Ship extends Sprite {
 	}
 	
 	public void thrust(){
-		this.setMovement(myVelocity, myOrientation);
+		myVelocity++;
 	}
 	
 	public void fire(){
 	
+	}
+	
+	@Override
+	public void update(long elapsedTime){
+		super.update(elapsedTime);
+		myVelocity -= VELOCITY_REDUCTION;
+		setMovement(myVelocity, myOrientation);
 	}
 }
