30bef208f05c3885787bb0daa986dd4d842ae3f2
kate.l.yang <kate.l.yang@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-17 21:03:02 +0000

    added methods to Conditions

--- src/vooga/games/asteroids/GameLost.java
+++ src/vooga/games/asteroids/GameLost.java
@@ -1,14 +1,21 @@
 package vooga.games.asteroids;
 
 import vooga.engine.level.GameLostConditions;
+import vooga.engine.overlay.Stat;
 
 
 public class GameLost extends GameLostConditions{
 	
+	private Stat<Integer> myStat;
+	
 	@Override
 	public boolean GameLost() {
-		if (getLives() == 0)
+		if (myStat.getStat() == 0)
 			return true;
 		return false;
+	}
+	
+	public void addStat(Stat<Integer> stat){
+		myStat = stat;
 	}
 }
