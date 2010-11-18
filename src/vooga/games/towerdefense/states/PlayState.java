21686a962759522c5911e3ed0097a0817fd7f054
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-18 06:00:19 +0000

    Now PlayField initializes myControls with constructor.

--- src/vooga/games/towerdefense/states/PlayState.java
+++ src/vooga/games/towerdefense/states/PlayState.java
@@ -1,7 +1,11 @@
 package vooga.games.towerdefense.states;
 
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import com.golden.gamedev.object.Background;
 import com.golden.gamedev.object.background.*;
+import com.golden.gamedev.util.ImageUtil;
 
 
 import vooga.engine.control.*;
@@ -24,9 +28,9 @@ public class PlayState extends GameState{
 		addPlayField(initPlayField());
 	}
 	
-	public PlayField initPlayField(){
+	private PlayField initPlayField(){
 		PlayField playField = new PlayField();
-		playField.setBackground(new ImageBackground(Resources.getImage("background"), Resources.getInt("gameWidth"), Resources.getInt("gameHeight")));
+		playField.setBackground(initBackground());
 		Sprite player = initPlayer();
 		playField.add(player);
 		playField.addControl(initControl(player));
@@ -34,7 +38,13 @@ public class PlayState extends GameState{
 		return playField;
 	}
 	
-	public Sprite initPlayer(){
+	private Background initBackground(){
+		BufferedImage backgroundImage = Resources.getImage("background");
+		backgroundImage = ImageUtil.resize(backgroundImage, Resources.getInt("gameWidth"), Resources.getInt("gameHeight"));
+		return new ImageBackground(backgroundImage);
+	}
+	
+	private Sprite initPlayer(){
 		Sprite player = new Player(Resources.getImage("towerPreview"), 0 , 0);		
 		return player;
 	}
