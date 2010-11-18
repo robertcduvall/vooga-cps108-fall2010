ec872e8586dab759b1a174f1ce1e20fa202f04e4
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-18 22:52:14 +0000

    add player and boundaries collision handler class

---
+++ src/vooga/games/tronupdate/collisions/PlayerAndBoundariesCollision.java
@@ -0,0 +1,32 @@
+package vooga.games.tronupdate.collisions;
+/**
+ * This class handles the collision between the boundary
+ * @author Meng Li,Brent Sodman,JiaQi Yan
+ */
+import vooga.engine.state.GameState;
+import vooga.engine.state.GameStateManager;
+
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.collision.CollisionBounds;
+
+public class PlayerAndBoundariesCollision extends CollisionBounds{
+	
+	private GameState gameOverState;
+	private GameStateManager gm;
+	
+	public PlayerAndBoundariesCollision(int x, int y, int width,
+			int height,GameState gameOverState,GameStateManager gm) {
+		super(x, y, width, height);
+		this.gameOverState=gameOverState;
+		this.gm=gm;
+	}
+	/**
+	 * determines what happens after the collision
+	 */
+	@Override
+	public void collided(Sprite s) {
+		//s.setSpeed(-0.2, -0.2);
+		//s.setActive(false); //this will prevent collided method being called many times after collision
+		//gm.switchTo(gameOverState);
+	}
+}
