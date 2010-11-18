12216ea0e5f793d21953f16e9a2c98a68ad846e8
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-18 22:09:38 +0000

    adding controls to doodlesprite

--- trunk/src/vooga/games/jumper/NormalGameState.java
+++ trunk/src/vooga/games/jumper/NormalGameState.java
@@ -5,15 +5,19 @@ import java.awt.event.KeyEvent;
 import vooga.engine.control.Control;
 import vooga.engine.control.KeyboardControl;
 import vooga.engine.core.Game;
+import vooga.engine.core.PlayField;
 import vooga.engine.core.Sprite;
 import vooga.engine.state.GameState;
 
 public class NormalGameState extends GameState{
 	
 	private Game myGame;
+	private PlayField myField;
 	
-	public NormalGameState(Game game){
+	public NormalGameState(Game game, PlayField field){
+		super(field);
 		myGame = game;
+		myField = field;
 	}
 	
 	@Override
@@ -27,7 +31,7 @@ public class NormalGameState extends GameState{
 	}
 
 	private void initControls(Sprite player) {
-		Control playerControl = new KeyboardControl(player, game);
+		Control playerControl = new KeyboardControl(player, myGame);
 		playerControl.addInput(KeyEvent.VK_LEFT, "moveLeft", "vooga.games.jumper.sprites.DoodleSprite");
 		playerControl.addInput(KeyEvent.VK_RIGHT, "moveRight", "vooga.games.jumper.sprites.DoodleSprite");
 		playerControl.addInput(KeyEvent.VK_UP, "moveUp", "vooga.games.jumper.sprites.DoodleSprite");
