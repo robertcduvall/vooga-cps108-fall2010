b91935ae1de42beef2ac11e26a79ea5b9f7ee3c5
jtkline1205 <jtkline1205@gmail.com>
2010-11-19 02:31:43 +0000

    grandius - made new event class

---
+++ src/vooga/games/grandius/events/FireBlackHoleEvent.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package vooga.games.grandius.events;
+
+import java.awt.event.KeyEvent;
+
+import vooga.engine.event.IEventHandler;
+import vooga.engine.resource.Resources;
+import vooga.games.grandius.DropThis;
+import vooga.games.grandius.Player;
+import vooga.games.grandius.states.PlayState;
+import vooga.games.grandius.weapons.BlackHole;
+
+/**
+ * @author bhawana
+ *
+ */
+public class FireBlackHoleEvent  implements IEventHandler{
+	private DropThis grandius; 
+	private Player player;
+	private PlayState playState;
+	
+	public FireBlackHoleEvent(DropThis grandius, Player player, PlayState playState){
+		this.grandius = grandius;
+		this.player = player;
+		this.playState = playState;
+	}
+	
+	@Override
+	public boolean isTriggered() {
+		return grandius.keyPressed(KeyEvent.VK_ALT);
+	}
+
+	@Override
+	public void actionPerformed() {
+		BlackHole blackhole = new BlackHole(Resources.getImage("BlackHole"),player.getX()+player.getWidth(),player.getY());
+		blackhole.setHorizontalSpeed(Resources.getDouble("ProjectileSpeed"));
+		playState.getGroup("BlackHole").add(blackhole);
+		grandius.playSound(Resources.getSound("MissileSound"));	
+	}
+
+}
