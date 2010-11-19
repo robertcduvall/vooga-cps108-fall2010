69e810da25acccc5dc0283c9e18ebb9930fd4c66
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-19 00:26:13 +0000

    changed BorderRule to fit new Rule methods

--- trunk/src/vooga/games/asteroids/BorderRule.java
+++ trunk/src/vooga/games/asteroids/BorderRule.java
@@ -4,23 +4,35 @@ import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
 import vooga.engine.level.Rule;
 
-public class BorderRule implements Rule{
-
-	private SpriteGroup group;
+public class BorderRule implements Rule {
+	//TODO remove hard-coded resolution
 	
 	@Override
-	public void enforce() {
-		for (Sprite sprite : group.getSprites()){
-			if (sprite.getX()>=800)
-				sprite.setX(0);
-			else if (sprite.getX()<=0)
-				sprite.setX(800);
-			else if (sprite.getY()>=600)
-				sprite.setY(0);
-			else if (sprite.getY()<=0)
-				sprite.setY(600);
+	public void enforce(SpriteGroup... groups) {
+		for (SpriteGroup g : groups) {
+			for (Sprite sprite : g.getSprites()) {
+				if (sprite.getX() >= 800)
+					sprite.setX(0);
+				else if (sprite.getX() <= 0)
+					sprite.setX(800);
+				else if (sprite.getY() >= 600)
+					sprite.setY(0);
+				else if (sprite.getY() <= 0)
+					sprite.setY(600);
+			}
 		}
 	}
-	
+
+	@Override
+	public boolean isSatisfied(SpriteGroup... groups) {
+		for (SpriteGroup g : groups) {
+			for (Sprite sprite : g.getSprites()) {
+				if (sprite.getX() >= 800 || sprite.getX() <= 0
+						|| sprite.getY() >= 600 || sprite.getY() <= 0)
+					return false;
+			}
+		}
+		return true;
+	}
 
 }
