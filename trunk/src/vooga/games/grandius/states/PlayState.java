063d4acc37ca2144a3337b486ea0a7a9a7b03e61
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-19 02:38:54 +0000

    grandius - moved several methods from DropThis to PlayState

--- trunk/src/vooga/games/grandius/states/PlayState.java
+++ trunk/src/vooga/games/grandius/states/PlayState.java
@@ -1,17 +1,24 @@
 package vooga.games.grandius.states;
 
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import vooga.engine.core.PlayField;
 import vooga.engine.core.BetterSprite;
+import vooga.engine.core.PlayField;
 import vooga.engine.resource.Resources;
 import vooga.engine.state.GameState;
 import vooga.games.grandius.Player;
 import vooga.games.grandius.collisions.BasicCollision;
+import vooga.games.grandius.enemy.boss.reacher.Reacher;
+import vooga.games.grandius.enemy.boss.reacher.ReacherEye;
+import vooga.games.grandius.enemy.common.Zipster;
+import vooga.games.grandius.weapons.BlackHole;
 
+import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
 
 public class PlayState extends GameState {
@@ -30,6 +37,12 @@ public class PlayState extends GameState {
 	private SpriteGroup backgroundGroup; 
 	private Player player;
 	
+	private boolean reacherShieldsDepleted;
+	
+	// Cheat options
+	private boolean isInvincible = false;
+	private boolean skipLevel = false;
+
 	@Override
 	public void initialize() {
 		//myPlayField = new PlayField();
@@ -51,8 +64,10 @@ public class PlayState extends GameState {
 
 	@Override
 	public void update(long elapsedTime){
-		
 		super.update(elapsedTime);
+//		updateScreenSprites(); //TODO is this method needed?
+		updateEntities();
+		checkBossParts();
 	}
 
 	//TODO create PlayField for levels using Game.initLevel()
@@ -117,4 +132,205 @@ public class PlayState extends GameState {
 		this.addUpdatePlayField(newField);
 		this.addRenderPlayField(newField);
 	}
+	
+//	/**
+//	 * Updates the various enemies that are on screen.
+//	 */
+//	private void updateScreenSprites() {
+//		ArrayList<ArrayList<BetterSprite>> currentSprites = levelManager
+//				.currentLevel();
+//		updateSpriteGroup(enemyGroup, currentSprites, 0);
+//		updateSpriteGroup(bossPartGroup, currentSprites, 1);
+//		updateSpriteGroup(bossGroup, currentSprites, 2);
+//	}
+
+//	/**
+//	 * Utility method used in updateScreenSprites().
+//	 */
+//	private void updateSpriteGroup(SpriteGroup spriteGroup,
+//			ArrayList<ArrayList<BetterSprite>> currentSprites, int index) {
+//		spriteGroup.clear();
+//		Collection<BetterSprite> screenSprites = currentSprites.get(index);
+//		for (BetterSprite s : screenSprites) {
+//			if (s == null)
+//				break;
+//			spriteGroup.add(s);
+//		}
+//	}
+	
+	private void updateEntities() {
+//		updateScreenSprites();
+		player.setVerticalSpeed(0);
+		for (Sprite as : enemyGroup.getSprites()) {
+			if (as == null)
+				break;
+			if (as instanceof Zipster) {
+				if (((Zipster) (as)).willFire(player)) {
+					enemyProjectileGroup.add(((Zipster) (as)).fireLaser());
+//					playSound(Resources.getSound("ZipsterLaserSound"));
+				}
+				as.setHorizontalSpeed(-((Zipster) (as)).getSpeed());
+				((Zipster) as).setImages(new BufferedImage[] { Resources
+						.getAnimation("SpinningZipster")[((Zipster) as)
+						.getSpin()] });
+				if (!((Zipster) as).isProximateToBlackHole())
+					((Zipster) as).setSpin(0);
+				((Zipster) as).setProximateToBlackHole(false);
+			}
+		}
+		for (Sprite bp : bossPartGroup.getSprites()) {
+			if (bp == null)
+				break;
+			if (bp instanceof ReacherEye) {
+				if (((ReacherEye) (bp)).willFire(player)) {
+					enemyProjectileGroup.add(((ReacherEye) (bp)).fireBeam());
+//					playSound(Resources.getSound("ReacherEyeBeamSound"));
+				}
+				bp.setHorizontalSpeed(-((ReacherEye) (bp)).getSpeed());
+			}
+		}
+		for (Sprite b : bossGroup.getSprites()) {
+			if (b == null)
+				break;
+			if (b instanceof Reacher) {
+				if (((Reacher) (b)).topBeamWillFire(player)) {
+					enemyProjectileGroup.add(((Reacher) (b)).fireTopBeam());
+//					playSound(Resources.getSound("ReacherBeamSound"));
+				}
+				if (((Reacher) (b)).bottomBeamWillFire(player)) {
+					enemyProjectileGroup.add(((Reacher) (b)).fireBottomBeam());
+//					playSound(Resources.getSound("ReacherBeamSound"));
+				}
+				if (((Reacher) (b)).redRayWillFire(player)) {
+					enemyProjectileGroup.add(((Reacher) (b)).fireRedRay());
+//					playSound(Resources.getSound("ReacherRedRaySound"));
+				}
+				b.setHorizontalSpeed(-((Reacher) (b)).getSpeed());
+			}
+		}
+
+		double bulletSpeed = Resources.getDouble("bulletSpeed");
+		resetSpriteSpeed(projectileGroup, bulletSpeed);
+		resetSpriteSpeed(enemyProjectileGroup, -1 * bulletSpeed);
+		resetSpriteSpeed(missileGroup, bulletSpeed);
+
+		for (Sprite h : blackHoleGroup.getSprites()) {
+			if (h == null)
+				break;
+			if (h.isActive()) {
+				((BlackHole) h).suckEnemies(enemyGroup);
+				((BlackHole) h).setPlayerCompensationSpeed(0);
+			}
+		}
+
+		//TODO this code needs to be moved
+//		if (keyDown(KeyEvent.VK_LEFT)) {
+//			for (SpriteGroup sg : spriteGroupSpeedMap.keySet()) {
+//				moveSpriteGroup(sg, "right", spriteGroupSpeedMap.get(sg));
+//			}
+//			for (BetterSprite h : blackHoleGroup.getSprites()) {
+//				if (h == null)
+//					break;
+//				((BlackHole) h).setPlayerCompensationSpeed(1 * PLAYER_SPEED);
+//			}
+//		}
+//		if (keyDown(KeyEvent.VK_RIGHT)) {
+//			for (SpriteGroup sg : spriteGroupSpeedMap.keySet()) {
+//				moveSpriteGroup(sg, "left", spriteGroupSpeedMap.get(sg));
+//			}
+//			for (BetterSprite h : blackHoleGroup.getSprites()) {
+//				if (h == null)
+//					break;
+//				((BlackHole) h).setPlayerCompensationSpeed(-1 * PLAYER_SPEED);
+//			}
+//		}
+//		if (keyDown(KeyEvent.VK_DOWN)) {
+//			playerSprite.setVerticalSpeed(PLAYER_SPEED);
+//		}
+//		if (keyDown(KeyEvent.VK_UP)) {
+//			playerSprite.setVerticalSpeed(-1 * PLAYER_SPEED);
+//		}
+	}
+	
+	private void resetSpriteSpeed(SpriteGroup spriteGroup, double newSpeed) {
+		for (Sprite s : spriteGroup.getSprites()) {
+			if (s == null)
+				break;
+			s.setHorizontalSpeed(newSpeed);
+		}
+	}
+
+	/**
+	 * Utility method used in updateEntities().
+	 */
+	private void moveSpriteGroup(SpriteGroup spriteGroup, String direction,
+			double offset) {
+		if (direction.equals("right")) {
+			for (Sprite s : spriteGroup.getSprites()) {
+				if (s == null)
+					break;
+				s.setHorizontalSpeed(1 * Resources.getDouble("playerSpeed") + offset);
+			}
+		} else if (direction.equals("left")) {
+			for (Sprite s : spriteGroup.getSprites()) {
+				if (s == null)
+					break;
+				s.setHorizontalSpeed(-1 * Resources.getDouble("playerSpeed") + offset);
+			}
+		}
+	}
+	
+	private void checkBossParts() {
+		if (bossGroup.isActive()) {
+			// Change this method to accomodate more than one level
+			Sprite reacherSprite = bossGroup.getSprites()[0];
+			int reacherEyesDestroyed = 0;
+			for (Sprite bp : bossPartGroup.getSprites()) {
+				if (bp == null)
+					break;
+				if (bp instanceof ReacherEye && !bp.isActive())
+					reacherEyesDestroyed++;
+			}
+			if (reacherEyesDestroyed == 1) {
+				((Reacher) (reacherSprite))
+						.setImage(new BufferedImage[] { Resources
+								.getAnimation("Reacher")[1] });
+			} else if (reacherEyesDestroyed == 3) {
+				((Reacher) (reacherSprite))
+						.setImage(new BufferedImage[] { Resources
+								.getAnimation("Reacher")[2] });
+			} else if (reacherEyesDestroyed == 5 && !reacherShieldsDepleted) {
+				// Deplete shields, but do not reset to the "depleted shields"
+				// image more than once (the next
+				// images should be of the Reacher's status becoming yellow,
+				// then red)
+				((Reacher) (reacherSprite))
+						.setImage(new BufferedImage[] { Resources
+								.getAnimation("Reacher")[3] });
+				reacherShieldsDepleted = true;
+				((Reacher) (reacherSprite)).setVulnerable(true);
+			}
+		}
+	}
+	
+	/**
+	 * Checks to see if a level has been completed (all the enemies have been
+	 * cleared, or the user has used the skip level cheat code.
+	 */
+	private boolean checkLevelComplete() {
+		if (skipLevel) {
+			skipLevel = false;
+			return true;
+		}
+		for (int i = 0; i < 3; i++) {
+			for (BetterSprite s : levelManager.currentLevel().get(i)) {
+				if (s.isActive()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	
 }
