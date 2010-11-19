1f02d6562c68ca9a7d7441789e50f4c44e258410
sailing.thru.gravity <sailing.thru.gravity@gmail.com>
2010-11-19 04:10:17 +0000

    added score and money

--- src/vooga/games/towerdefense/events/ShootEvent.java
+++ src/vooga/games/towerdefense/events/ShootEvent.java
@@ -3,11 +3,19 @@ package vooga.games.towerdefense.events;
 import vooga.engine.event.IEventHandler;
 import vooga.engine.resource.Resources;
 import vooga.games.towerdefense.DropThis;
+import vooga.games.towerdefense.actors.Player;
 import vooga.games.towerdefense.actors.TowerShot;
+import vooga.games.towerdefense.actors.enemies.Enemy;
 import vooga.games.towerdefense.actors.towers.ShootingTower;
 
 public class ShootEvent implements IEventHandler{
 	ShootingTower tower;
+	Player player;
+	
+	public ShootEvent(Player p)
+	{
+		player = p;
+	}
 	
 	public void setTower(ShootingTower t){
 		tower = t;
@@ -22,8 +30,11 @@ public class ShootEvent implements IEventHandler{
 	@Override
 	public void actionPerformed() {
 		if(tower.canShoot()){
-			tower.getTarget().gotHit();
+			Enemy target = tower.getTarget();
+			target.gotHit();
 			tower.resetShot();
+			player.addMoney(target.getMoneyPerHit());
+			player.addScore(target.getScorePerHit());
 			//TowerShot shot = new TowerShot(Resources.getImage("towerShot"),getX(), getY(), target.getX(), target.getY(), shotSpeed);
 		}
 	}
