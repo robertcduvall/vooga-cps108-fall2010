a3c5208b420ae137bc5219811e04543c9ff258f6
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-19 15:06:02 +0000

    grandius updates - game menu loads, when addOverlays() is commented out - still a bug in creation of Overlays. clicking buttons on menu does not yield results of actionPerformed()

--- trunk/src/vooga/games/grandius/buttons/BuyBlackHoleButton.java
+++ trunk/src/vooga/games/grandius/buttons/BuyBlackHoleButton.java
@@ -1,14 +1,19 @@
 package vooga.games.grandius.buttons;
 
+import java.awt.image.BufferedImage;
+
 import vooga.engine.core.Game;
 import vooga.engine.resource.Resources;
 import vooga.widget.Button;
 
 public class BuyBlackHoleButton extends Button {
 
+	private static final int START_X = 220;
+	private static final int START_Y = 200;
+	private static final BufferedImage myImage = Resources.getImage("BuyBlackHoleButton");
+	
 	public BuyBlackHoleButton(Game game) {
-		super(game);
-		this.setImage(Resources.getImage("BuyBlackHoleButton"));
+		super(game, myImage, START_X, START_Y);
 	}
 
 	@Override
--- trunk/src/vooga/games/grandius/buttons/NextLevelButton.java
+++ trunk/src/vooga/games/grandius/buttons/NextLevelButton.java
@@ -1,14 +1,19 @@
 package vooga.games.grandius.buttons;
 
+import java.awt.image.BufferedImage;
+
 import vooga.engine.core.Game;
 import vooga.engine.resource.Resources;
 import vooga.widget.Button;
 
 public class NextLevelButton extends Button {
 
+	private static final int START_X = 220;
+	private static final int START_Y = 400;
+	private static final BufferedImage myImage = Resources.getImage("NextLevelButton");
+	
 	public NextLevelButton(Game game) {
-		super(game);
-		this.setImage(Resources.getImage("NextLevelButton"));
+		super(game, myImage, START_X, START_Y);
 	}
 
 	@Override
