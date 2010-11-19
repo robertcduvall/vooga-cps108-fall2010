d021f5c8471185e7c227e4a23ff02e969c0c3e26
jtkline1205 <jtkline1205@gmail.com>
2010-11-19 01:18:14 +0000

    grandius state, resource file, and button updates

--- src/vooga/games/grandius/states/LevelCompleteState.java
+++ src/vooga/games/grandius/states/LevelCompleteState.java
@@ -1,7 +1,29 @@
 package vooga.games.grandius.states;
 
+import vooga.engine.resource.Resources;
 import vooga.engine.state.BasicTextGameState;
-
+//TODO
+//This GameState should be implemented using a BasicTextGameState
+//private void buildLevelCompleteState() {
+//	levelCompleteState = new GameState();
+//	int displayX = Resources.getInt("levelCompleteX");
+//	int displayY = Resources.getInt("levelCompleteY");
+//
+//	OverlayString levelComplete1 = new OverlayString("LEVEL " + (levelManager.getCurrentLevel()+1) + " COMPLETE", font);
+//	levelComplete1.setLocation(displayX,displayY);
+//	OverlayString levelComplete2 = new OverlayString(Resources.getString("levelCompleteMessage"), font);
+//	levelComplete2.setLocation(displayX,2*displayY);
+//
+//	levelCompleteState.addGroup(backgroundGroup);
+//	levelCompleteGroup.add(levelComplete1);
+//	levelCompleteGroup.add(levelComplete2);
+//	levelCompleteState.addRenderGroup(levelCompleteGroup);
+//}
 public class LevelCompleteState extends BasicTextGameState {
 
+	private static String myLevelCompleteMessage = Resources.getString("LevelCompleteMessage");
+	public LevelCompleteState() {
+		super(myLevelCompleteMessage);
+	}
+	
 }
