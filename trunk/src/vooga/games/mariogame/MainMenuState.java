bfcf88422db3475793313c4ee7af2f5adf7bc1a9
uscfan1781@gmail.com <uscfan1781@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-19 00:12:00 +0000

    Changed MainMenuState to use Overlays.

--- trunk/src/vooga/games/mariogame/MainMenuState.java
+++ trunk/src/vooga/games/mariogame/MainMenuState.java
@@ -3,13 +3,13 @@ package vooga.games.mariogame;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-import vooga.engine.resource.Resources;
-import vooga.engine.state.GameState;
+import vooga.engine.overlay.OverlayCreator;
+import vooga.engine.overlay.OverlayTracker;
 import vooga.engine.state.MenuGameState;
 
-import com.golden.gamedev.object.GameFont;
 import com.golden.gamedev.object.GameFontManager;
 import com.golden.gamedev.object.background.ImageBackground;
+import com.golden.gamedev.object.SpriteGroup;
 
 /**
  * 
@@ -23,9 +23,7 @@ import com.golden.gamedev.object.background.ImageBackground;
 public class MainMenuState extends MenuGameState {
 
 	private ImageBackground myMainMenuBG;
-	private int myWidth;
-	private int myHeight;
-	private GameFont myMenuFont;
+	private SpriteGroup myOverlays;
 
 	/**
 	 * This constructs a MainMenuState with dimensions for the screen, as well
@@ -39,10 +37,10 @@ public class MainMenuState extends MenuGameState {
 	public MainMenuState(BufferedImage backgroundImage,
 			GameFontManager fontManager) {
 		super();
-		myMenuFont = fontManager.getFont("MENU");
 		myMainMenuBG = new ImageBackground(backgroundImage);
-		myWidth = Resources.getInt("Width");
-		myHeight = Resources.getInt("Height");
+		OverlayCreator overlayCreator = new OverlayCreator();
+		OverlayTracker overlayTracker = overlayCreator.createOverlays("src/vooga/games/mariogame/resources/MainMenuOverlays.xml");
+		overlayTracker.getOverlayGroup("MainMenuGroup");
 	}
 
 	/**
@@ -50,16 +48,8 @@ public class MainMenuState extends MenuGameState {
 	 */
 	public void render(Graphics2D g) {
 		super.render(g);
-
 		myMainMenuBG.render(g);
-		myMenuFont.drawString(g, "WELCOME TO MARIOCLONE!", myWidth / 4,
-				myHeight / 2 + 50);
-		myMenuFont.drawString(g, "PRESS A AND D TO MOVE LEFT AND RIGHT.",
-				(myWidth / 4), (myHeight / 2) + 125);
-		myMenuFont.drawString(g, "PRESS W TO JUMP AND S TO CROUCH.",
-				(myWidth / 4), (myHeight / 2) + 200);
-		myMenuFont.drawString(g, "PRESS SPACE TO PLAY AND P TO PAUSE!",
-				(myWidth / 4), (myHeight / 2) + 250);
+		myOverlays.render(g);
 	}
 
 	@Override
