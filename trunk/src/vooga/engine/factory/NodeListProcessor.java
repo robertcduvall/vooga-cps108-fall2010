64e24cffd9ebd60a9dd9ccd50562dc6c29c433f1
jimmymu123456789 <jimmymu123456789@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-20 21:05:03 +0000

    Updated the overlayTracker to work with stats

--- trunk/src/vooga/engine/factory/NodeListProcessor.java
+++ trunk/src/vooga/engine/factory/NodeListProcessor.java
@@ -1,11 +1,14 @@
 package vooga.engine.factory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import vooga.engine.core.PlayField;
 import vooga.engine.factory.xmltags.MapProcessor;
 import vooga.engine.factory.xmltags.SpriteGroupsProcessor;
+import vooga.engine.overlay.OverlayCreator;
+import vooga.engine.overlay.OverlayTracker;
 
 
 /**
@@ -18,14 +21,22 @@ import vooga.engine.factory.xmltags.SpriteGroupsProcessor;
 
 public abstract class NodeListProcessor implements NodeList{
 	
-	
 	protected PlayField myPlayField;
 	protected Document myXMLDocument;
 	protected NodeList myChildren;
+	protected OverlayTracker myTracker;
 	
 	public NodeListProcessor(Document xmlDocument, NodeList children) {
 		myXMLDocument = xmlDocument;
-		myChildren = children;
+		myChildren = children;	
+		createOverlayTracker(xmlDocument);
+	}
+
+	private void createOverlayTracker(Document xmlDocument) {
+		Node level = xmlDocument.getChildNodes().item(0);
+		Element levelElement = (Element) level;
+		String xmloverlaypath = levelElement.getAttribute("xmloverlaypath");
+		myTracker = OverlayCreator.createOverlays(xmloverlaypath);
 	}
 	
 	public abstract void process();
