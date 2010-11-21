2b88c4cb7bca5dbdece075ff49d95d3f313994c4
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-21 23:32:03 +0000

    GridSpace contains the information whether a grid is filled with true or not. Take the responsibility from Tronplayer,PlayState to GridSpace.

--- src/vooga/games/tronupdate/util/GridSpace.java
+++ src/vooga/games/tronupdate/util/GridSpace.java
@@ -1,14 +1,21 @@
 package vooga.games.tronupdate.util;
 
+import java.util.Arrays;
+
 
 
 public class GridSpace {
 	private int totalRow;
 	private int totalColumn;
+	private boolean[][] gridsTaken;
 
 	public GridSpace(int totalRow,int totalColumn){  //for example, for the screen size 480*480 and the image size is 5*5,it is better to set it (480/5) by (480/5)
 		this.totalRow=totalRow;      				 
 		this.totalColumn=totalColumn;
+		gridsTaken = new boolean[totalRow][totalColumn];
+		for(int i=0;i<totalRow;i++){
+			Arrays.fill(gridsTaken[i],false);
+		}
 	}	
 	/**
 	 * get the total number of rows in the grid
@@ -23,6 +30,22 @@ public class GridSpace {
 	 */
 	public int getTotalColumn(){
 		return totalColumn;
-	}	
+	}		
+	
+	public void fillGrid(int row,int col){
+		gridsTaken[row][col] = true;
+	}
+	
+	public boolean isGridTaken(int row,int col){
+		return gridsTaken[row][col];
+	}
+	
+	public boolean[][] gridsTaken(){
+		return gridsTaken;
+	}
+	
+	public void updateGridsTaken(boolean[][] grids){
+		gridsTaken = grids;
+	}
 	
 }
