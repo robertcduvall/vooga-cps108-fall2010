7356ad48f6a60ca85ad589cb165c43badf177c3d
jimmymu123456789 <jimmymu123456789@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-21 20:35:20 +0000

    Got rid of all error...making vooga bug free!

--- src/vooga/engine/factory/xmltags/LevelProcessor.java
+++ src/vooga/engine/factory/xmltags/LevelProcessor.java
@@ -1,38 +1,38 @@
-package vooga.engine.factory.xmltags;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import vooga.engine.factory.NodeListProcessor;
-
-public class LevelProcessor extends NodeListProcessor {
-	
-	public LevelProcessor(Document xmlDocument, NodeList nodeList) {
-		super(xmlDocument, nodeList);
-	}
-	
-	@Override
-	public int getLength() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Node item(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void process() {
-		SpriteGroupsProcessor spriteGroupsProcessor = 
-			new SpriteGroupsProcessor(myXMLDocument, 
-					myXMLDocument.getElementsByTagName("SpriteGroups").item(0).getChildNodes());
-		spriteGroupsProcessor.process();
-//		CollisionGroupsProcessor collisionGroupsProcessor = new CollisionGroupsProcessor(myXMLDocument, myChildren);
-//		collisionGroupsProcessor.process()
-	}
-
-}
+//package vooga.engine.factory.xmltags;
+//
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Node;
+//import org.w3c.dom.NodeList;
+//
+//import vooga.engine.factory.NodeListProcessor;
+//
+//public class LevelProcessor extends NodeListProcessor {
+//	
+//	public LevelProcessor(Document xmlDocument, NodeList nodeList) {
+//		super(xmlDocument, nodeList);
+//	}
+//	
+//	@Override
+//	public int getLength() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	@Override
+//	public Node item(int arg0) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public void process() {
+//		SpriteGroupsProcessor spriteGroupsProcessor = 
+//			new SpriteGroupsProcessor(myXMLDocument, 
+//					myXMLDocument.getElementsByTagName("SpriteGroups").item(0).getChildNodes());
+//		spriteGroupsProcessor.process();
+////		CollisionGroupsProcessor collisionGroupsProcessor = new CollisionGroupsProcessor(myXMLDocument, myChildren);
+////		collisionGroupsProcessor.process()
+//	}
+//
+//}
 
