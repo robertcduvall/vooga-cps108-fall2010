436ad031baf85d6d1d009dbc07f0e6cb7defe6ff
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 01:17:51 +0000

    two Game Event updates. PauseEvent takes care of pausing the game when space key is pressed while ResumeEvent takes care of resuming the game when c key is pressed

--- src/vooga/games/tronupdate/events/TronGameResumeEvent.java
+++ src/vooga/games/tronupdate/events/TronGameResumeEvent.java
@@ -2,36 +2,30 @@ package vooga.games.tronupdate.events;
 
 import java.awt.event.KeyEvent;
 
-import vooga.engine.core.BetterSprite;
 import vooga.engine.core.Game;
 import vooga.engine.event.IEventHandler;
 import vooga.engine.state.GameState;
 import vooga.engine.state.GameStateManager;
 
-public class TronGamePauseEvent implements IEventHandler {
-	
+public class TronGameResumeEvent implements IEventHandler {
+
 	private GameStateManager gm;
 	private Game game;
 
 
-	public TronGamePauseEvent(Game game,GameStateManager gm){
+	public TronGameResumeEvent(Game game,GameStateManager gm){
 		this.game=game;
 		this.gm=gm;
 	}
-	
 	@Override
 	public void actionPerformed() {
-
-		gm.switchTo(gm.getGameState(1));//index 1 means pauseState
-		System.out.println("Game is paused!");
-		
+		gm.switchTo(gm.getGameState(0));//0 index is playstate 
+		System.out.println("Game is resumed!");
 	}
 
 	@Override
-	public boolean isTriggered() 
-	{		
-		return game.keyPressed(KeyEvent.VK_SPACE);
-	}
-	
+	public boolean isTriggered() {
+		return game.keyPressed(KeyEvent.VK_C);
+    }
 
 }
