0471932a9f775ec6c1728e9f6456500231db3c57
scottwink <scottwink@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 08:58:47 +0000

    next level event

--- src/vooga/games/cyberion/events/LevelCompleteEvent.java
+++ src/vooga/games/cyberion/events/LevelCompleteEvent.java
@@ -1,18 +1,35 @@
 package vooga.games.cyberion.events;
 
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.SpriteGroup;
+
 import vooga.engine.event.IEventHandler;
+import vooga.games.cyberion.DropThis;
+import vooga.games.cyberion.states.PlayState;
 
 public class LevelCompleteEvent implements IEventHandler{
 
+	private DropThis myGame;
+	private PlayState playState;
+	private SpriteGroup spriteGroup;
+	
+	public LevelCompleteEvent(DropThis game, PlayState state, SpriteGroup group) {
+		myGame = game;
+		playState = state;
+		spriteGroup = group;
+	}
+
 	@Override
 	public boolean isTriggered() {
-		
+		for (Sprite tempSprite : spriteGroup.getSprites()){
+			if (tempSprite!=null && tempSprite.isActive())
+				return false;
+		}
 		return true;
 	}
 
 	@Override
 	public void actionPerformed() {
-		
-		
-	} 
+		playState.nextLevel();
+	}
 }
