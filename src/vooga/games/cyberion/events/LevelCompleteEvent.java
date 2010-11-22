fcba2b741264f23ae06e239cbca94226907f5e38
hehao87 <hehao87@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 22:48:06 +0000

    clean up

--- src/vooga/games/cyberion/events/LevelCompleteEvent.java
+++ src/vooga/games/cyberion/events/LevelCompleteEvent.java
@@ -10,12 +10,12 @@ import vooga.games.cyberion.DropThis;
 import vooga.games.cyberion.sprites.playership.PlayerShip;
 import vooga.games.cyberion.states.PlayState;
 
-public class LevelCompleteEvent implements IEventHandler{
+public class LevelCompleteEvent implements IEventHandler {
 
 	private DropThis myGame;
 	private PlayState playState;
 	private SpriteGroup spriteGroup;
-	
+
 	public LevelCompleteEvent(DropThis game, PlayState state, SpriteGroup group) {
 		myGame = game;
 		playState = state;
@@ -24,8 +24,8 @@ public class LevelCompleteEvent implements IEventHandler{
 
 	@Override
 	public boolean isTriggered() {
-		for (Sprite tempSprite : spriteGroup.getSprites()){
-			if (tempSprite!=null && tempSprite.isActive())
+		for (Sprite tempSprite : spriteGroup.getSprites()) {
+			if (tempSprite != null && tempSprite.isActive())
 				return false;
 		}
 		return true;
@@ -33,13 +33,7 @@ public class LevelCompleteEvent implements IEventHandler{
 
 	@Override
 	public void actionPerformed() {
-		//Stat<Integer> tempScore= (Stat<Integer>) playState.getPlayer().getStat("scoreStat");
-		//System.out.println(tempScore.getStat());
 		PlayState newState = playState.nextLevel();
-		//BetterSprite t = (BetterSprite) newState.getPlayField().getGroup("playerGroup").getSprites()[0];//.setStat("scoreStat", tempScore);
-		//t.setStat("scoreStat", tempScore);
-		//newState.getPlayer().setStat("scoreStat", tempScore);
-		//System.out.println((Integer) newState.getPlayer().getStat("scoreStat").getStat());
 		myGame.setAsPlayGameState(newState);
 		myGame.setPlayState();
 	}
