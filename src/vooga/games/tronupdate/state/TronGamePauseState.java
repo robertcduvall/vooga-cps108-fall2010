a7f3ebfd402d632ca7a22c4ffae50018450b0a7b
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 01:21:26 +0000

    GameState updates

--- src/vooga/games/tronupdate/state/TronGamePauseState.java
+++ src/vooga/games/tronupdate/state/TronGamePauseState.java
@@ -4,29 +4,39 @@ import java.awt.Graphics2D;
 
 import com.golden.gamedev.object.SpriteGroup;
 
+import vooga.engine.core.Game;
+import vooga.engine.event.EventPool;
 import vooga.engine.overlay.OverlayCreator;
 import vooga.engine.overlay.OverlayString;
 import vooga.engine.overlay.OverlayTracker;
 import vooga.engine.resource.Resources;
 import vooga.engine.state.GameState;
+import vooga.engine.state.GameStateManager;
+import vooga.games.tronupdate.events.TronGameResumeEvent;
 import vooga.games.zombieland.Blah;
 
 public class TronGamePauseState extends GameState{
-	public TronGamePauseState(){
-		
-	}
-	@Override
-	public void initialize() {
-		
-		// TODO Auto-generated method stub
-		
+	private GameStateManager gameStateManager;
+	private EventPool eventPool;
+	private Game game;
+	
+	public TronGamePauseState(Game game, GameStateManager gameStateManager){
+		this.gameStateManager = gameStateManager;
+		this.game=game;
 	}
 	
-	public void render(Graphics2D g) {
-		
+	@Override
+	public void initialize() {
+		eventPool = new EventPool();
+		TronGameResumeEvent tronGameResumeEvent = new TronGameResumeEvent(game,gameStateManager);
+		eventPool.addEvent(tronGameResumeEvent);
 	}
+	
+
 	public void update(long elapsedTime){
-		
+		super.update(elapsedTime);
+	//	playerControl.update();
+		eventPool.checkEvents();
 	}
 	
 }
