d6338dc18113fb1fa02a9e2bb1480938dd18138f
hehao87 <hehao87@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 22:48:06 +0000

    clean up

--- trunk/src/vooga/games/cyberion/collisions/EnemyCollidesWithShot.java
+++ trunk/src/vooga/games/cyberion/collisions/EnemyCollidesWithShot.java
@@ -2,14 +2,11 @@ package vooga.games.cyberion.collisions;
 
 import vooga.engine.core.Game;
 import vooga.engine.overlay.Stat;
-import vooga.engine.resource.Resources;
 import vooga.games.cyberion.sprites.enemyship.EnemyShip;
 import vooga.games.cyberion.sprites.playershot.PlayerShot;
 
-import com.golden.gamedev.engine.BaseAudio;
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.collision.BasicCollisionGroup;
-
 
 public class EnemyCollidesWithShot extends BasicCollisionGroup {
 
@@ -19,15 +16,16 @@ public class EnemyCollidesWithShot extends BasicCollisionGroup {
 
 	@Override
 	public void collided(Sprite playershot, Sprite enemy) {
-		collided((PlayerShot)playershot, (EnemyShip) enemy);
+		collided((PlayerShot) playershot, (EnemyShip) enemy);
 
 	}
-	
-//when a playershot collides with an enemy, enemy's life decreases, shot disappears, and a sound is played
+
+	// when a playershot collides with an enemy, enemy's life decreases, shot
+	// disappears, and a sound is played
 	public void collided(Sprite playershot, EnemyShip enemy) {
-		if (enemy.isOnScreen()){
-				Stat<Integer> score = (Stat<Integer>) enemy.getStat("scoreStat");
-				score.setStat(score.getStat()+10);
+		if (enemy.isOnScreen()) {
+			Stat<Integer> score = (Stat<Integer>) enemy.getStat("scoreStat");
+			score.setStat(score.getStat() + 10);
 		}
 		enemy.setLife(enemy.getLife() - 1);
 		playershot.setActive(false);
