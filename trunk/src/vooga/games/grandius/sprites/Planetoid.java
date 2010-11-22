faa3113313ec05362c9f8d0cd07b961e7834c928
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 04:57:21 +0000

    more grandius updates

---
+++ trunk/src/vooga/games/grandius/sprites/Planetoid.java
@@ -0,0 +1,15 @@
+package vooga.games.grandius.sprites;
+
+import vooga.engine.core.BetterSprite;
+
+@SuppressWarnings("serial")
+public class Planetoid extends BetterSprite {
+
+	public Planetoid() {
+		this(0,0);
+	}
+
+	public Planetoid(double x, double y) {
+		super(x, y);
+	}
+}
