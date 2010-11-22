16ded7b7d5f27df45f0ffd28708c7305f712a25a
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 06:17:42 +0000

    trivial updates

--- trunk/src/vooga/games/tronupdate/state/GameOverState.java
+++ trunk/src/vooga/games/tronupdate/state/GameOverState.java
@@ -1,15 +1,28 @@
 package vooga.games.tronupdate.state;
 
+import vooga.engine.core.Game;
 import vooga.engine.event.EventPool;
 import vooga.engine.state.GameState;
+import vooga.engine.state.GameStateManager;
+import vooga.games.tronupdate.events.GameOverEvent;
 import vooga.games.tronupdate.events.TronGameResumeEvent;
 
 public class GameOverState extends GameState{
 
+	private GameStateManager gameStateManager;
+	private EventPool eventPool;
+	private Game game;
+	
+	public GameOverState(Game game, GameStateManager gameStateManager){
+		this.gameStateManager = gameStateManager;
+		this.game=game;
+	}
+	
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-		
+		eventPool = new EventPool();
+		GameOverEvent gameOverEvent = new GameOverEvent(game,gameStateManager);
+		eventPool.addEvent(gameOverEvent);
 	}
 
 	
@@ -17,6 +30,6 @@ public class GameOverState extends GameState{
 	public void update(long elapsedTime){
 		super.update(elapsedTime);
 	//	playerControl.update();
-	//	eventPool.checkEvents();
+		eventPool.checkEvents();
 	}
 }
