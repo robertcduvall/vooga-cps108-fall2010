0829b55f025976c869b02db21dc2c27c2d9f4cf9
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 19:45:12 +0000

    made blockThroughWall.java almost independent

--- trunk/src/vooga/games/jumper/rules/BlockThroughWall.java
+++ trunk/src/vooga/games/jumper/rules/BlockThroughWall.java
@@ -6,11 +6,23 @@ import com.golden.gamedev.object.SpriteGroup;
 import vooga.engine.level.Rule;
 import vooga.engine.resource.Resources;
 
+
+/**
+ * Superclass for RightWall and LeftWall.  Contains all the functionality
+ * to send blocks to the other wall if they go through a side wall.
+ * RightWall and LeftWall should be phased out, they only contain two 
+ * fields.
+ * @author Devsno
+ *
+ */
 public class BlockThroughWall implements Rule{
 
-	protected int THIS_WALL_POSITION;
-	protected int OTHER_WALL_POSITION; 
 
+	protected int RIGHT_WALL_POSITION = Resources.getInt("gameWidth");
+	protected int LEFT_WALL_POSITION = 0; 
+
+	protected int THIS_WALL_POSITION = RIGHT_WALL_POSITION;
+	protected int OTHER_WALL_POSITION = 0; 
 	/** set type SpriteGroup to be enforced by this rule.
 	 * @param any number of type SpriteGroup
 	 */
@@ -27,6 +39,9 @@ public class BlockThroughWall implements Rule{
 	 * @param type SpriteGroup
 	 */
 
+	/*
+	 * Needs to be changed to work with both wall locations.
+	 */
 	public void reposition(SpriteGroup groups) {
 		for(Sprite sprite: groups.getSprites()){
 			if (sprite!=null)
@@ -39,14 +54,15 @@ public class BlockThroughWall implements Rule{
 	 * Check to see if the rule is satisfied.
 	 * 
 	 * @param any number of type SpriteGroup
-	 * @return true if sprite from sprites hit this wall
+	 * @return true if sprite from sprites hit EITHER wall.
 	 */
 
 	@Override
 	public boolean isSatisfied(SpriteGroup... groups) {
 		for (SpriteGroup group : groups) {
 			for (Sprite s : group.getSprites()) {
-				if (s!=null && (s.getX() + s.getWidth()) < THIS_WALL_POSITION) {
+				if (	s!=null && 
+						(((s.getX() + s.getWidth()) < LEFT_WALL_POSITION ) || (s.getX() > RIGHT_WALL_POSITION))) {
 					return true;
 				}
 			}
