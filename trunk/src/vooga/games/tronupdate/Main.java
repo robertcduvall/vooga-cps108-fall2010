620a5d56ec44bd961c1488819e42784157a29cc6
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 06:41:13 +0000

    MenuState added in and main updates

--- trunk/src/vooga/games/tronupdate/Main.java
+++ trunk/src/vooga/games/tronupdate/Main.java
@@ -11,6 +11,7 @@ import vooga.engine.resource.Resources;
 import vooga.engine.state.BasicTextGameState;
 import vooga.engine.state.GameState;
 import vooga.engine.state.GameStateManager;
+import vooga.games.tronupdate.state.GameOverState;
 import vooga.engine.state.PauseGameState;
 import vooga.games.tronupdate.state.MenuState;
 import vooga.games.tronupdate.state.TronGamePauseState;
@@ -19,8 +20,6 @@ import vooga.games.tronupdate.collisions.*;
 
 public class Main extends vooga.engine.core.Game{
 	
-	//private TronGamePlayState playState;
-	private EventPool eventPool;
 	
 	@Override
 	public void initResources() {
@@ -31,13 +30,15 @@ public class Main extends vooga.engine.core.Game{
 	
 	private void initStates(){
 		TronGamePauseState pauseState =new TronGamePauseState(this,stateManager);
+		GameOverState gameOverState =new GameOverState(this,stateManager);
 		TronGamePlayState playState = new TronGamePlayState(this,stateManager);
 		MenuState menuState = new MenuState(this,stateManager);
 		//playState.activate();
 		menuState.activate();
 		stateManager.addGameState(playState,0);
 		stateManager.addGameState(pauseState,1);
-		stateManager.addGameState(menuState,2);
+		stateManager.addGameState(gameOverState,2);
+		stateManager.addGameState(menuState,3);
 		
 		
 	}
