14276d0d5bc40db8f8c46f4cd7e4e913efe949af
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 16:08:42 +0000

    [doodlejump] Refactored monsters package

--- trunk/src/vooga/games/doodlejump/monsters/JigglingFlyingMonster.java
+++ trunk/src/vooga/games/doodlejump/monsters/JigglingFlyingMonster.java
@@ -13,6 +13,9 @@ public class JigglingFlyingMonster extends BetterSprite {
 	private static final String JIGGLING_FLYING_MONSTER_STRING = "jiggling_flying_monster";
 	private static final String JIGGLE_TIME_STRING = "jiggleTime";
 	private static final int MULTIPLY_HORIZONTAL_SPEED = -1;
+	private static final int ZERO_JIGGLE_TIMER = 0;
+	private static final int INITIAL_ANIMATION_FRAME = 0;
+	private static final int FINAL_ANIMATION_FRAME = 4;
 
 	private int jiggleTimer;
 
@@ -28,7 +31,7 @@ public class JigglingFlyingMonster extends BetterSprite {
 	public void firstRun() {
 		jiggleTimer = Resources.getInt(JIGGLE_TIME_STRING);
 		AnimatedSprite sprite = (AnimatedSprite) getCurrentSprite();
-		sprite.setAnimationFrame(0, 4);
+		sprite.setAnimationFrame(INITIAL_ANIMATION_FRAME, FINAL_ANIMATION_FRAME);
 		sprite.setLoopAnim(true);
 		sprite.setAnimate(true);
 	}
@@ -41,7 +44,7 @@ public class JigglingFlyingMonster extends BetterSprite {
 	}
 
 	private void makeMonsterJiggle() {
-		if (jiggleTimer < 0) {
+		if (jiggleTimer < ZERO_JIGGLE_TIMER) {
 			setHorizontalSpeed(getHorizontalSpeed() * MULTIPLY_HORIZONTAL_SPEED);
 			jiggleTimer = Resources.getInt(JIGGLE_TIME_STRING);
 		}
