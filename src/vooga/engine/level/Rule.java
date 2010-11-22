68d6528d5d597667fca0756dbc365230f0e77e31
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 03:47:58 +0000

    commented in the rule api

--- src/vooga/engine/level/Rule.java
+++ src/vooga/engine/level/Rule.java
@@ -2,8 +2,29 @@ package vooga.engine.level;
 
 import com.golden.gamedev.object.SpriteGroup;
 
+/**
+ * This interface allows Rules to be created for games.  The developer can add sprite groups
+ * to apply the rule to and check to see if the rule is satisfied for specific sprite groups.
+ * 
+ * The point of a rule is to call an action when certain criteria are met. The action called
+ * must be defined by the developer.
+ * @author Devon Townsend
+ *
+ */
 public interface Rule {
+	
+	/**
+	 * add SpriteGroups that this Rule applies to
+	 * @param groups
+	 */
 	public void enforce(SpriteGroup...groups);
 	
+	/**
+	 * 
+	 * @param SpriteGroup... groups
+	 * @return whether the rule is involved for the given SpriteGroups.  This
+	 * does not mean that the sprite groups are enforced by the rule, rather
+	 * that the rule is activated. 
+	 */
 	public boolean isSatisfied(SpriteGroup...groups);
 }
