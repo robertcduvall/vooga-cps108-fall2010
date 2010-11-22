45606f20ca317b1bee9442dcc50d1c2f52920035
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 06:39:48 +0000

    made jumper collisions look better

--- trunk/src/vooga/games/jumper/sprites/DoodleSprite.java
+++ trunk/src/vooga/games/jumper/sprites/DoodleSprite.java
@@ -14,40 +14,18 @@ import vooga.games.jumper.DropThis;
 
 public class DoodleSprite extends BetterSprite {
 
+	private double MAX_SPEED = Resources.getDouble("MAX_SPEED");
 
-	private double X_ACCELERATION = 5;
-	private double X_DECCELERATION = 1;
-	private double GRAVITY = 0.1;
-	private long ACCELERATION_TIME = 1;
-	private double MAX_SPEED = 2.0;
-	private double jetpackMultiplier = 3;
-
-	private int mySpriteWidth = getWidth();
-	private int mySpriteHeight = getHeight();
-
-	private BufferedImage doodleLeftImage = Resources.getImage("leftDoodle");
-	private BufferedImage doodleRightImage = Resources.getImage("rightDoodle");
-	private BufferedImage doodleLeftImageJetpack = Resources.getImage("leftJetpackDoodle");
-	private BufferedImage doodleRightImageJetpack = Resources.getImage("rightJetpackDoodle");
-
-	private int gameHeight = Resources.getInt("gameHeight");
-	private int gameWidth = Resources.getInt("gameWidth");
-	
 	/**
 	 * Create new DoodleSprite
 	 * @param image Sprite Image
 	 * @param location Point representing starting sprite location
 	 */
-
-	
-	
 	public DoodleSprite(BufferedImage image, Point location) {
 		super(image, location.x, location.y);  
 	}
 
-	public DoodleSprite(){
-		
-	}
+	public DoodleSprite(){}
 	
 	public void moveLeft(){	
 		setHorizontalSpeed(-1* MAX_SPEED);
@@ -55,9 +33,5 @@ public class DoodleSprite extends BetterSprite {
 	
 	public void moveRight(){
 		setHorizontalSpeed(MAX_SPEED);
-	}
-	
-	public void moveUp(){
-		System.out.println("up");
 	}
 }
