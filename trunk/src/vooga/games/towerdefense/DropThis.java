421172f0cfe55f23101b106a40bc39a5c74f37ea
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 17:35:50 +0000

    TowerDefense Ability to switch between levels using the main menu added.

--- trunk/src/vooga/games/towerdefense/DropThis.java
+++ trunk/src/vooga/games/towerdefense/DropThis.java
@@ -73,10 +73,9 @@ public class DropThis extends Game {
 		
 		//TODO uncomment once prolog error is found and fixed
 		GameState mainMenu = addGameState(new MainMenu());
-		GameState play = addGameState(new PlayState(tracker, levelManager));
+		PlayState play = (PlayState) addGameState(new PlayState(tracker));
 		GameState pause = addGameState(new PauseState(play, tracker));
-		//play.addPlayField(initLevel("/resources/level/level.xml"));
-		getGameStateManager().switchTo(play);
+		getGameStateManager().switchTo(mainMenu);
 	}
 
 private OverlayTracker initOverlays() {
