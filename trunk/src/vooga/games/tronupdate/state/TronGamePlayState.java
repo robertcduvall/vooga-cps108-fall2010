05c37647fff40f10ead2d47245ce89791a739c8e
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 23:09:56 +0000

    add a timer into the play state to make game more fun

--- trunk/src/vooga/games/tronupdate/state/TronGamePlayState.java
+++ trunk/src/vooga/games/tronupdate/state/TronGamePlayState.java
@@ -9,6 +9,8 @@ import java.util.Queue;
 
 import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.Timer;
+
 import vooga.engine.control.Control;
 import vooga.engine.control.KeyboardControl;
 import vooga.engine.core.Game;
@@ -37,7 +39,7 @@ import vooga.games.tronupdate.util.AI;
 
 
 public class TronGamePlayState extends GameState{
-	
+
 	private Game game;
 	private GameStateManager stateManager;
 	private PlayField playField;
@@ -56,27 +58,26 @@ public class TronGamePlayState extends GameState{
 	private int currentLevel=0;//0,1,2
 	private int totalLevel=3;
 	//private OverlayTracker tracker;
-	
+	public Timer timer = new Timer(50);
 	private boolean[][] levelBlocks;
 	private int totalRow,totalCol;
-	
+	private int i=0;
+	private int j=0;
 	public static final int GRID_WIDTH=Resources.getInt("width")/Resources.getInt("playerimagewidth");
 	public static final int GRID_HEIGHT=Resources.getInt("height")/Resources.getInt("playerimagewidth");
 	public static final String PLAYER_CLASS="vooga.games.tronupdate.items.TronPlayer";
 	public static final int PLAYER_IMAGE_WIDTH=Resources.getInt("playerimagewidth");
-	
-	private static int RANDOM_BLOCK_SIZE = 5;//15;
-	private static int MINIMUM_RANDOM_BLOCKS = 5;
-	private static int RANDOM_LEVEL_BLOCKS = 3;
-	
-	//int numBlocks,int minumumBlocks,int blockSize
-	
+
+	private static int numBlocks = 5;//15;
+	private static int minumumBlocks = 5;
+	private static int blockSize = 3;
+
 	public TronGamePlayState(Game game){
 		//super();
 		this(game,null);
-		
+
 	}
-	
+
 	public TronGamePlayState(Game game, GameStateManager stateManager){
 		//super();
 		this.game = game;
@@ -94,6 +95,7 @@ public class TronGamePlayState extends GameState{
 	}
 	@Override
 	public void initialize() {
+		
 		initializeSprites();
 		initializeControl();
 		initializeBlocks();
@@ -103,53 +105,55 @@ public class TronGamePlayState extends GameState{
 		initializeEvents();
 		game.playMusic(Resources.getSound("backgroundmusic"));
 	}
-	
+
 	public void initializeOverlay(){
 		OverlayCreator.setGame(game);
 		OverlayTracker tracker = OverlayCreator.createOverlays("src/vooga/games/tronupdate/resources/overlays.xml");
 		playField.addGroup(tracker.getOverlayGroup("second"));
 	}
-	
+
 	public void initializeCollision() {
 		PlayerAndEnemyCollision playerEnemyCollision=new PlayerAndEnemyCollision(game,stateManager);   
 		PlayerAndBoundariesCollision playerBoundariesCollision=new PlayerAndBoundariesCollision(0,0,Resources.getInt("width"),Resources.getInt("height"),game,stateManager);
 		PlayerAndBonusCollision playerBonusCollision=new PlayerAndBonusCollision(game);		
-		
+
 		playField.addCollisionGroup(playField.getGroup("player"), playField.getGroup("block"), playerEnemyCollision);
 		playField.addCollisionGroup(playField.getGroup("player"), playField.getGroup("player"), playerBoundariesCollision);
 		playField.addCollisionGroup(playField.getGroup("player"), playField.getGroup("bonus"), playerBonusCollision);
 	}
-	
+
 	public void initializeEvents() {
 		eventPool = new EventPool();
-	    TronGamePauseEvent tronGamePauseEvent = new TronGamePauseEvent(game,stateManager); 
-	    
-	    eventPool.addEvent(tronGamePauseEvent);
+		TronGamePauseEvent tronGamePauseEvent = new TronGamePauseEvent(game,stateManager); 
+
+		eventPool.addEvent(tronGamePauseEvent);
 	}
 
 	public void initializeSprites() {
-	    gridSpace=new GridSpace(GRID_WIDTH,GRID_HEIGHT);
+		gridSpace=new GridSpace(GRID_WIDTH,GRID_HEIGHT);
 		firstPlayer = new TronPlayer(Resources.getImage("redlazer") , initialFirstPlayerXPosition(), initialFirstPlayerYPosition() ,PLAYER_IMAGE_WIDTH, Direction.right);
 		secondPlayer = new TronPlayer(Resources.getImage("bluelazer") , initialSecondPlayerXPosition(), initialSecondPlayerYPosition() ,PLAYER_IMAGE_WIDTH, Direction.left);
 	}
-	
+
 	public int initialFirstPlayerXPosition(){
 		return gridSpace.getTotalRow() / 10;
 	}
-	
+
 	public int initialFirstPlayerYPosition(){
 		return gridSpace.getTotalColumn() / 2;
 	}
-	
+
 	public int initialSecondPlayerXPosition(){
 		return gridSpace.getTotalRow()*9 / 10;
 	}
-	
+
 	public int initialSecondPlayerYPosition(){
 		return gridSpace.getTotalColumn() / 2;
 	}
-	
+
 	public void initializeEnvironment() {
+		i=0;
+		j=0;
 		playField = new PlayField();
 		playField.addColorBackground(Color.WHITE);
 		playField.setBackground(0);
@@ -163,14 +167,14 @@ public class TronGamePlayState extends GameState{
 		}
 		playField.getGroup(spritegroups[0]).add(firstPlayer);
 		playField.getGroup(spritegroups[0]).add(secondPlayer);
-		
+
 		for(int i=0;i<firstPlayerBlocks.length;i++){
 			for(int j=0;j<firstPlayerBlocks[0].length;j++)
-		playField.getGroup(spritegroups[1]).add(firstPlayerBlocks[i][j]);
+				playField.getGroup(spritegroups[1]).add(firstPlayerBlocks[i][j]);
 		}
 		for(int i=0;i<secondPlayerBlocks.length;i++){
 			for(int j=0;j<secondPlayerBlocks[0].length;j++)
-		playField.getGroup(spritegroups[1]).add(secondPlayerBlocks[i][j]);
+				playField.getGroup(spritegroups[1]).add(secondPlayerBlocks[i][j]);
 		}	
 		createRandomBonus();
 		for(SpeedUpBonus bonus:bonusList){
@@ -178,21 +182,23 @@ public class TronGamePlayState extends GameState{
 			playField.getGroup(spritegroups[2]).add(bonus);
 		}
 		if(currentLevel==0){
-		createRandomLevelBlocks(RANDOM_BLOCK_SIZE,MINIMUM_RANDOM_BLOCKS,RANDOM_LEVEL_BLOCKS);
+			createLevelBlocks(numBlocks,minumumBlocks,blockSize);
+			numBlocks=15;
 		}
 		else if (currentLevel==1){
-			
+			createLevelBlocks(numBlocks,minumumBlocks,blockSize);
+			numBlocks=10;
 		}
 		else if (currentLevel==2){
-			
+
 		}
 	}
-	
+
 	/**
 	 * create random obstacle blocks for the level
 	 */
-	public void createRandomLevelBlocks(int numBlocks,int minumumBlocks,int blockSize){	
-		
+	public void createLevelBlocks(int numBlocks,int minumumBlocks,int blockSize){	
+
 		int randomNumberOfBlocks = (int)Math.ceil(Math.random() * numBlocks) + minumumBlocks;
 
 		for (int i = 0; i < randomNumberOfBlocks; i++){
@@ -209,22 +215,22 @@ public class TronGamePlayState extends GameState{
 			for(int index1=0;index1<randomHeight;index1++){
 				for(int index2 = 0;index2<randomWidth;index2++){
 					playField.getGroup(Resources.getString("block")).add(new BetterSprite(Resources.getImage("greenlazer"),(randomCol+index1)*PLAYER_IMAGE_WIDTH,(randomRow+index2)*PLAYER_IMAGE_WIDTH));
-				//	blocksGroup.add(new Sprite(Resources.getImage("lazer_green"),(randomCol+index1)*PLAYER_IMAGE_WIDTH,(randomRow+index2)*PLAYER_IMAGE_WIDTH));
+					//	blocksGroup.add(new Sprite(Resources.getImage("lazer_green"),(randomCol+index1)*PLAYER_IMAGE_WIDTH,(randomRow+index2)*PLAYER_IMAGE_WIDTH));
 					levelBlocks[randomRow+index1][randomCol+index2] = true;
 				}		
 			}
 		}	
-		
-		
+
+
 	}
-	
+
 	/**
 	 * create random bonuses for the level
 	 */
 	public void createRandomBonus(){
 		bonusList=new LinkedList<SpeedUpBonus>();
- 
-		for (int i = 0; i < 10; i++){		
+
+		for (int i = 0; i < 20; i++){		
 			int randomX = (int)Math.ceil(Math.random()* GRID_WIDTH);
 			int randomY = (int)Math.ceil(Math.random()* GRID_HEIGHT);    	
 			bonusList.add(new SpeedUpBonus(Resources.getImage("yellowbonus"), randomX,randomY,PLAYER_IMAGE_WIDTH));	
@@ -243,7 +249,7 @@ public class TronGamePlayState extends GameState{
 		firstPlayerControl.addInput(KeyEvent.VK_UP, "up", PLAYER_CLASS);
 		firstPlayerControl.addInput(KeyEvent.VK_LEFT, "left", PLAYER_CLASS);
 	}
-	
+
 	public void initSecondPlayerControls(TronPlayer player){
 		secondPlayerControl = new KeyboardControl(player, game);
 		if(Mode.isSingle()){
@@ -260,7 +266,7 @@ public class TronGamePlayState extends GameState{
 			ai.setPlayer(secondPlayer);
 		}
 	}
-	
+
 	/**
 	 * fills in the grid spaces behind the players with collidable blocks. 
 	 */
@@ -277,55 +283,69 @@ public class TronGamePlayState extends GameState{
 					blocksTaken[i][j]=true;
 				}
 			}			
-		}		
+		}
+		
+			
+				playField.getGroup(Resources.getString("block")).add(new BetterSprite(Resources.getImage("greenlazer"),i*PLAYER_IMAGE_WIDTH,j*PLAYER_IMAGE_WIDTH));
+				
+			
 	}
 	/**
 	 * initialize the blocks  
 	 */
 	public void initializeBlocks(){
-		  //  firstPlayerBlocks=new BetterSprite[GRID_WIDTH+2][GRID_HEIGHT+2];
+		//  firstPlayerBlocks=new BetterSprite[GRID_WIDTH+2][GRID_HEIGHT+2];
 		firstPlayerBlocks=new BetterSprite[GRID_WIDTH][GRID_HEIGHT];
-			for(int i=0;i<firstPlayer.grid.getTotalRow();i++){
-				for(int j=0;j<firstPlayer.grid.getTotalColumn();j++){
-					firstPlayerBlocks[i][j]=new BetterSprite(Resources.getImage("redlazer"),-50,-50);
-					//blocksGroup.add(tronPlayerBlocksList.get(count)[i][j]);
-				}
-			}	
-			secondPlayerBlocks=new BetterSprite[GRID_WIDTH][GRID_HEIGHT];
-			for(int i=0;i<secondPlayer.grid.getTotalRow();i++){
-				for(int j=0;j<secondPlayer.grid.getTotalColumn();j++){
-					secondPlayerBlocks[i][j]=new BetterSprite(Resources.getImage("bluelazer"),-50,-50);
-					//blocksGroup.add(tronPlayerBlocksList.get(count)[i][j]);
-				}
-			}	
-			blocksTaken = new boolean[GRID_WIDTH][GRID_HEIGHT];
-			for(int i=0;i<GRID_WIDTH;i++){
-				Arrays.fill(blocksTaken[i],false);
+		for(int i=0;i<firstPlayer.grid.getTotalRow();i++){
+			for(int j=0;j<firstPlayer.grid.getTotalColumn();j++){
+				firstPlayerBlocks[i][j]=new BetterSprite(Resources.getImage("redlazer"),-50,-50);
+				//blocksGroup.add(tronPlayerBlocksList.get(count)[i][j]);
 			}
-			
-			totalRow = firstPlayer.grid.getTotalRow();
-			totalCol = firstPlayer.grid.getTotalColumn();
-			levelBlocks = new boolean[totalRow][totalCol];
-			for(int i=0;i<totalRow;i++){
-				Arrays.fill(levelBlocks[i],false);
+		}	
+		secondPlayerBlocks=new BetterSprite[GRID_WIDTH][GRID_HEIGHT];
+		for(int i=0;i<secondPlayer.grid.getTotalRow();i++){
+			for(int j=0;j<secondPlayer.grid.getTotalColumn();j++){
+				secondPlayerBlocks[i][j]=new BetterSprite(Resources.getImage("bluelazer"),-50,-50);
+				//blocksGroup.add(tronPlayerBlocksList.get(count)[i][j]);
 			}
-			
-			
+		}	
+		blocksTaken = new boolean[GRID_WIDTH][GRID_HEIGHT];
+		for(int i=0;i<GRID_WIDTH;i++){
+			Arrays.fill(blocksTaken[i],false);
+		}
+
+		totalRow = firstPlayer.grid.getTotalRow();
+		totalCol = firstPlayer.grid.getTotalColumn();
+		levelBlocks = new boolean[totalRow][totalCol];
+		for(int i=0;i<totalRow;i++){
+			Arrays.fill(levelBlocks[i],false);
+		}
+
+
 	}
-	
 
 
-	
+
+
 	public void render(Graphics2D g) {
 		playField.render(g);
 	}
-	
+
 	public void update(long elapsedTime) {
 		updateBlocks();
-		
-		
+		if (timer.action(elapsedTime)){//&&currentLevel==1) {		
+			if(i>=totalRow){
+				if(j>=totalCol){
+					j=-1;
+				}
+				j++;
+				i=-1;
+			}
+			i++;		
+		}
+
 		if(Math.random()<0.04&&!bonusList.isEmpty()){
-		spawnBonus();
+			spawnBonus();
 		}
 		playField.update(elapsedTime);
 		eventPool.checkEvents();
@@ -338,11 +358,11 @@ public class TronGamePlayState extends GameState{
 
 	public void spawnBonus() {
 		if(prev==null||prev.isConsumed()){
-		SpeedUpBonus b=bonusList.poll();
-		prev=b;
-		b.setActive(true);
+			SpeedUpBonus b=bonusList.poll();
+			prev=b;
+			b.setActive(true);
 		}
 	}
-	
-	
+
+
 }
