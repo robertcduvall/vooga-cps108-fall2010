ba75259d5ba02921dab9fae4165145cce90bb1ca
uscfan1781 <uscfan1781@gmail.com>
2010-11-22 11:46:28 +0000

    Made enemies animated

--- src/vooga/games/mariogame/sprites/Enemy.java
+++ src/vooga/games/mariogame/sprites/Enemy.java
@@ -1,15 +1,16 @@
 package vooga.games.mariogame.sprites;
 
 
+import java.awt.image.BufferedImage;
+
 import vooga.engine.core.BetterSprite;
 
-
-import vooga.engine.core.BetterSprite;
+import com.golden.gamedev.object.AnimatedSprite;
 
 /**
  * @author David Herzka, Cameron McCallie, Andrew Brown
  * 
- *         This class represents the main enemy 'mario' sprite.
+ *         This class represents the main enemy sprite.
  * 
  */
 
@@ -19,28 +20,22 @@ public class Enemy extends BetterSprite {
 	private static final double mySpeed = .25;
 	
 	public Enemy(){
-		
+	}
+	
+	public void startAnimation(){
+		System.out.println("starting animation");
+		setAsRenderedSprite("Goomba");
+		((AnimatedSprite) getCurrentSprite()).setAnimate(true);
+		((AnimatedSprite) getCurrentSprite()).setLoopAnim(true);
 	}
 	
 	
-	/**
-	 * Constructs an enemy sprite out of the parameters listed below:
-	 * 
-	 * @param name
-	 * @param stateName
-	 *            - ex. alive, dead, etc...
-	 * @param left
-	 *            - represents the image used when the sprite turns/moves left
-	 * @param right
-	 *            - represents the image used when the sprite turns/moves right
-	 */
-	/*
-//	public Enemy(String name, String stateName, BufferedImage[] left,
-//			BufferedImage[] right) {
-//		super(name, stateName, left, right);
-//		setHorizontalSpeed(mySpeed * Math.signum(Math.random() - .5));
-//	}
-	*/
+	@Override
+	public void addAnimatedImages(String label, BufferedImage[] images){
+		super.addAnimatedImages(label, images);
+		startAnimation();
+	}
+	
 	/**
 	 * Sends the sprite in the opposite direction with opposite velocity. The
 	 * method is named 'bounce' because it occurs after a collision with the
