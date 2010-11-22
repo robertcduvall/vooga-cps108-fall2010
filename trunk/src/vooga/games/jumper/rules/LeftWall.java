382c21d227631256c0970e578fb23833e02b69f0
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 19:37:28 +0000

    LeftWall and RightWall were almost Identical so I made them extend BlockThroughWall.  They only contain two fields and should be deleted though.

--- trunk/src/vooga/games/jumper/rules/LeftWall.java
+++ trunk/src/vooga/games/jumper/rules/LeftWall.java
@@ -8,58 +8,11 @@ import vooga.engine.resource.Resources;
 
 /**
  * Allow DoodleSprite to move through the left wall
- * 
- * @author Cody
+ * @author Cody, Devon, Brian
  */
-public class LeftWall implements Rule {
-
-	private final int LEFT_WALL_POSITION = 0;
-	private final int RIGHT_WALL_POSITION = Resources.getInt("gameWidth");
+public class LeftWall extends BlockThroughWall {
 	
-	/** set type SpriteGroup to be enforced by this rule.
-	 * @param any number of type SpriteGroup
-	 */
-
-	@Override
-	public void enforce(SpriteGroup... groups) {
-		for (SpriteGroup group: groups) {
-			reposition(group);
-		}
-	}
+	private final int THIS_WALL_POSITION = 0;
+	private final int OTHER_WALL_POSITION = Resources.getInt("gameWidth");
 	
-	/**
-	 * Sets the sprites' x value to right wall
-	 * 
-	 * @param type SpriteGroup
-	 * 
-	 */
-
-	public void reposition(SpriteGroup groups) {
-		for(Sprite sprite: groups.getSprites()){
-			if (sprite!=null)
-				sprite.setX(RIGHT_WALL_POSITION);
-		}
-		
-	}
-	
-	/**
-	 * Check to see if the rule is satisfied.
-	 * 
-	 * @param any number of type SpriteGroup
-	 * @return true if sprite from sprites hit the left wall
-	 */
-
-	@Override
-	public boolean isSatisfied(SpriteGroup... groups) {
-		for (SpriteGroup group : groups) {
-			for (Sprite s : group.getSprites()) {
-				if (s!=null && (s.getX() + s.getWidth()) < LEFT_WALL_POSITION) {
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	}
-
 }
