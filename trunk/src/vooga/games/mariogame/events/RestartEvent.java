1b96d98dcd1ef968f1ddb42e0308087972073dbe
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 18:45:57 +0000

    blah some changes

---
+++ trunk/src/vooga/games/mariogame/events/RestartEvent.java
@@ -0,0 +1,19 @@
+package vooga.games.mariogame.events;
+
+import vooga.engine.event.IEventHandler;
+
+public class RestartEvent implements IEventHandler {
+
+	@Override
+	public boolean isTriggered() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void actionPerformed() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
