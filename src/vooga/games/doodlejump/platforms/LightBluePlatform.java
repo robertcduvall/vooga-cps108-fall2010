e2aafc03e7f3f6887e5ce87941f51a4e9cc03785
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 16:15:45 +0000

    [doodlejump] Refactored platform package

--- src/vooga/games/doodlejump/platforms/LightBluePlatform.java
+++ src/vooga/games/doodlejump/platforms/LightBluePlatform.java
@@ -12,30 +12,36 @@ import vooga.engine.resource.Resources;
  * 
  */
 public class LightBluePlatform extends BetterSprite {
-	
+
 	private static final String LIGHT_BLUE_PLATFORM_STRING = "lightBluePlatform";
 	private static final String MIN_SCREEN_X_STRING = "minScreenX";
 	private static final String MAX_SCREEN_X_STRING = "maxScreenX";
+	private static final double HORIZONTAL_SPEED = -0.1;
+	private static final double MULTIPLY_HORIZONTAL_SPEED = -1;
 
-	public LightBluePlatform(){
+	public LightBluePlatform() {
 		this(Resources.getImage(LIGHT_BLUE_PLATFORM_STRING));
 	}
+
 	public LightBluePlatform(BufferedImage image) {
 		super(image);
 	}
 
 	@Override
-	public void firstRun(){
-		setHorizontalSpeed(-0.1);
+	public void firstRun() {
+		setHorizontalSpeed(HORIZONTAL_SPEED);
 	}
-	
+
 	@Override
 	public void update(long elapsedTime) {
 		super.update(elapsedTime);
 		checkInBounds();
 	}
+
 	private void checkInBounds() {
-		if (getX() < Resources.getDouble(MIN_SCREEN_X_STRING) || getX() > Resources.getDouble(MAX_SCREEN_X_STRING) - getWidth())
-			setHorizontalSpeed(getHorizontalSpeed() * -1);
+		if (getX() < Resources.getDouble(MIN_SCREEN_X_STRING)
+				|| getX() > Resources.getDouble(MAX_SCREEN_X_STRING)
+						- getWidth())
+			setHorizontalSpeed(getHorizontalSpeed() * MULTIPLY_HORIZONTAL_SPEED);
 	}
 }
