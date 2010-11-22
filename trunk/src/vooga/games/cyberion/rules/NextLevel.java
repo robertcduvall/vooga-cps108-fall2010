667bc15aecf7e216f596e65c6e4b7d0247539c1d
hehao87 <hehao87@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 10:00:55 +0000

    add setGameStateManager() in rules

--- trunk/src/vooga/games/cyberion/rules/NextLevel.java
+++ trunk/src/vooga/games/cyberion/rules/NextLevel.java
@@ -5,9 +5,21 @@ import com.golden.gamedev.object.SpriteGroup;
 
 import vooga.engine.level.Rule;
 import vooga.engine.overlay.Stat;
+import vooga.engine.state.GameStateManager;
 import vooga.engine.core.BetterSprite;
 
-public class NextLevel implements Rule{
+public class NextLevel implements Rule {
+
+	private GameStateManager gameStateManager;
+
+	//
+	// public Dead(GameStateManager gameStateManager) {
+	// this.gameStateManager = gameStateManager;
+	// }
+
+	public void setGameStateManager(GameStateManager gameStateManager) {
+		this.gameStateManager = gameStateManager;
+	}
 
 	@Override
 	public void enforce(SpriteGroup... groups) {
@@ -18,13 +30,11 @@ public class NextLevel implements Rule{
 	@Override
 	public boolean isSatisfied(SpriteGroup... groups) {
 		// TODO Auto-generated method stub
-		for (Sprite tempSprite : groups[0].getSprites()){
-			if (tempSprite!=null && tempSprite.isActive())
+		for (Sprite tempSprite : groups[0].getSprites()) {
+			if (tempSprite != null && tempSprite.isActive())
 				return false;
 		}
 		return true;
 	}
-	
-	
 
 }
