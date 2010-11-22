e4821c3ae23b888f888a74035a53a7960237c920
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 19:39:51 +0000

    add different level into the game play state. More levels will be added in later

--- src/vooga/games/tronupdate/events/SwitchLevelEvent.java
+++ src/vooga/games/tronupdate/events/SwitchLevelEvent.java
@@ -8,19 +8,25 @@ import vooga.engine.state.GameStateManager;
 import vooga.engine.event.IEventHandler;
 import vooga.engine.state.GameState;
 import vooga.examples.state.PlayState;
+import vooga.games.tronupdate.state.TronGamePlayState;
 
 public class SwitchLevelEvent implements IEventHandler{
 	private GameStateManager gm;
 	private Game game;
 	//private boolean trigger;
+	private static int level=0;
 	
 	public SwitchLevelEvent(Game game, GameStateManager gm){
 		this.game=game;
 		this.gm=gm;
 	}
+	
 	@Override
 	public void actionPerformed() {
 		GameState state = gm.getGameState(0);//playState
+		TronGamePlayState playstate = (TronGamePlayState)state;
+		playstate.setLevel(level%playstate.getTotalLevel());
+		level++;
 		state.removeEverything();
 		state.initialize();
 		
