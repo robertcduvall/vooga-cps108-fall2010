1b144886df032421fc79f7ab2d4d918621f43ac2
cody.ko <cody.ko@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 06:24:07 +0000

    Added an isOverlap method in infiniteblockevent. Not sure if it works yet.

--- src/vooga/games/jumper/events/InfiniteBlocksEvent.java
+++ src/vooga/games/jumper/events/InfiniteBlocksEvent.java
@@ -3,6 +3,8 @@ package vooga.games.jumper.events;
 import java.awt.Point;
 import java.util.Random;
 
+import com.golden.gamedev.object.Sprite;
+
 import vooga.engine.event.IEventHandler;
 import vooga.engine.resource.Resources;
 import vooga.games.jumper.sprites.NormalBlock;
@@ -14,8 +16,7 @@ import vooga.games.jumper.states.PlayGameState;
  * @author Cody
  */
 public class InfiniteBlocksEvent implements IEventHandler {
-	
-	
+
 	Random random = new Random();
 	int xMax = Resources.getInt("gameWidth");
 	int xMin = 0;
@@ -23,9 +24,9 @@ public class InfiniteBlocksEvent implements IEventHandler {
 	int yMin = 0;
 	double xVelocity = 0;
 	double yVelocity = -0.2;
+	private final int MINIMUM_SPACE = 20;
 
 	private PlayGameState playState;
-	
 
 	public InfiniteBlocksEvent(PlayGameState gamestate) {
 		playState = gamestate;
@@ -36,12 +37,28 @@ public class InfiniteBlocksEvent implements IEventHandler {
 		return true;
 	}
 
+	public boolean isOverlap(int x, int y) {
+		for (Sprite s : playState.getGroup("normalBlocks").getSprites()) {
+			if (s != null && x > s.getX() - MINIMUM_SPACE
+					&& x < s.getX() + s.getWidth() + MINIMUM_SPACE
+					&& y > s.getY() + s.getHeight() + MINIMUM_SPACE
+					&& y < s.getY() - MINIMUM_SPACE) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public void actionPerformed() {
 		int randomX = random.nextInt(xMax - xMin) + xMin;
 		int randomY = random.nextInt(yMax - yMin) + yMax;
 		Point initPoint = new Point(randomX, randomY);
-		NormalBlock addBlock = new NormalBlock(Resources.getImage("platformWhite"), initPoint, xVelocity, yVelocity);
-		playState.getGroup("normalBlocks").add(addBlock);
+		NormalBlock addBlock = new NormalBlock(
+				Resources.getImage("platformGreen"), initPoint, xVelocity,
+				yVelocity);
+		if (!isOverlap(initPoint.x, initPoint.y)) {
+			playState.getGroup("normalBlocks").add(addBlock);
+		}
 	}
 }
