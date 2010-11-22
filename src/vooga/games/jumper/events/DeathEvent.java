05c9078a70c6f8f8f624282a86ec29a6ed7a55cc
cody.ko <cody.ko@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 17:46:58 +0000

    added a deathEvent to stop the screen when the doodle hits roof. For some reason the text doesn't show up

---
+++ src/vooga/games/jumper/events/DeathEvent.java
@@ -0,0 +1,37 @@
+package vooga.games.jumper.events;
+
+import com.golden.gamedev.object.Sprite;
+
+import vooga.engine.core.Game;
+import vooga.engine.event.IEventHandler;
+import vooga.games.jumper.DropThis;
+import vooga.games.jumper.states.PlayGameState;
+
+public class DeathEvent implements IEventHandler {
+	
+	private PlayGameState playState;
+	private DropThis myGame;
+	
+	public DeathEvent(DropThis dropThis, PlayGameState gamestate) {
+		playState = gamestate;
+		myGame = dropThis;
+	}
+
+	@Override
+	public boolean isTriggered() {
+		
+		for (Sprite s : playState.getGroup("doodleSprite").getSprites()) {
+			if (s!=null && s.getY() < 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void actionPerformed() {
+		// TODO Auto-generated method stub
+		myGame.deathGame();
+	}
+
+}
