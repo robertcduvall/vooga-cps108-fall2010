19e296d21f350cf26138a2de49578e8e91f8a152
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 02:58:21 +0000

    [doodlejump] Added Play and Resume Buttons and StartMenu and Pause states, updated resources.xml

---
+++ src/vooga/games/doodlejump/buttons/PlayButton.java
@@ -0,0 +1,32 @@
+package vooga.games.doodlejump.buttons;
+
+import java.awt.image.BufferedImage;
+
+import vooga.widget.Button;
+import vooga.engine.core.Game;
+import vooga.engine.resource.Resources;
+import vooga.games.doodlejump.BlahThis;
+
+public class PlayButton extends Button {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final BufferedImage PLAY_BUTTON_IMAGE = Resources.getImage("playButton");
+	private static final double PLAY_BUTTON_X = Resources.getDouble("playButtonX");
+	private static final double PLAY_BUTTON_Y = Resources.getDouble("playButtonY");
+
+	private BlahThis myGame;
+	
+	public PlayButton(Game game) {
+		super(game, PLAY_BUTTON_IMAGE, PLAY_BUTTON_X, PLAY_BUTTON_Y);
+		myGame = (BlahThis) game;
+	}
+
+	@Override
+	public void actionPerformed() {
+		myGame.resumeGame();
+	}
+
+}
