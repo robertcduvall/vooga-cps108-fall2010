f15d5d2bf6c807a3b032a9603cb77affe1271b40
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-11-22 03:45:05 +0000

    Added a singleton sound player class to the util package to allow for playing a Sound anywhere without a direct reference to game.

---
+++ trunk/src/vooga/engine/util/SoundPlayer.java
@@ -0,0 +1,50 @@
+package vooga.engine.util;
+
+import vooga.engine.core.Game;
+
+/**
+ * Singleton class designed to allow the ability to play sounds and music from
+ * anywhere in the game. The game must be set first, after that calling
+ * SoundPlayer.playMusic() or SoundPlayer.playSound will be the equivalent of
+ * calling those methods in your game.
+ * 
+ * @author Daniel Koverman
+ * 
+ */
+public class SoundPlayer {
+
+	private static Game game;
+
+	/**
+	 * Sets the game for playing music. Do this first.
+	 * 
+	 * @param game
+	 *            the current game which you would like to play sounds in
+	 */
+	public static void setGame(Game game) {
+		SoundPlayer.game = game;
+	}
+
+	/**
+	 * Plays background music for your game. Make sure that the game is set
+	 * before calling this.
+	 * 
+	 * @param audioFilePath
+	 *            File path to the music file. Resources supplies the filepath.
+	 */
+	public void playMusic(String audioFilePath) {
+		game.playMusic(audioFilePath);
+	}
+
+	/**
+	 * Play a sound effect for your game. Make sure that the game is set before
+	 * calling this.
+	 * 
+	 * @param audioFilePath
+	 *            File path to the music file. Resources supplies the filepath.
+	 */
+	public void playSound(String audioFilePath) {
+		game.playSound(audioFilePath);
+	}
+
+}
