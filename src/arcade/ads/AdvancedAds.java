f7c0e09c5a8ac7c26a0280e2826090f0167c0cc7
hehao87 <hehao87@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-03 02:57:48 +0000

    ads framework

---
+++ src/arcade/ads/AdvancedAds.java
@@ -0,0 +1,43 @@
+package arcade.ads;
+
+/**
+ * AdvancedAd abstract class that will extend BasicAd. This class will contain
+ * more specialized variables and functions, including a category, ranking, and
+ * a display duration (if applicable), as well as methods to activate and
+ * deactivate the ad.
+ * 
+ * @author Hao He (hh89@duke.edu)
+ * @author Nick Straub (njs7@duke.edu)
+ * @author Scott Winkleman (saw26@duke.edu)
+ * @author Kate Yang (kly2@duke.edu)
+ * 
+ * @version 1.0
+ */
+
+public class AdvancedAds extends BasicAds {
+
+	@Override
+	public void onClick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onMouseOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
