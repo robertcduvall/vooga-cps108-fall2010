8adb8cd9d74b0beb90bf44fc187e7cb71029f98c
acue@mac.com <acue@mac.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-03 03:13:27 +0000

    Full single player tic tac toe game.  Multiplayer connects to server but thats it

--- trunk/src/vooga/engine/networking/client/states/PlayState.java
+++ trunk/src/vooga/engine/networking/client/states/PlayState.java
@@ -8,7 +8,11 @@ import com.golden.gamedev.object.SpriteGroup;
 import vooga.engine.core.BetterSprite;
 import vooga.engine.core.Game;
 import vooga.engine.core.PlayField;
+import vooga.engine.event.EventPool;
 import vooga.engine.factory.LevelManager;
+import vooga.engine.factory.LevelParser;
+import vooga.engine.networking.client.TicTacToeConnection;
+import vooga.engine.networking.client.events.GameWonEvent;
 import vooga.engine.resource.Resources;
 import vooga.engine.state.GameState;
 import vooga.engine.control.*;
@@ -17,26 +21,40 @@ public class PlayState extends GameState{
 	private Game game;
 	private PlayField field;
 	private LevelManager levelManager;
+	private EventPool eventPool;
+	private GameState gameWonState;
+	private TicTacToeConnection connection;
 	private boolean xTurn;
+	private boolean won;
 	
-	public PlayState(Game game, LevelManager levelManager){
+	public PlayState(Game game, LevelManager levelManager, TicTacToeConnection connection){
 		this.game = game;
 		this.levelManager = levelManager;
+		this.connection = connection;
 		xTurn = true;
 	}
 
 	@Override
 	public void initialize() {
 		field = levelManager.loadFirstLevel();
-		System.out.println(field.getBackground());
 		initControls();
 		this.addPlayField(field);
+		initLevel();
 	}
 
 	public void initControls(){
 		Control gameControl = new MouseControl(this, game);
 		gameControl.addInput(MouseEvent.BUTTON1, "addPiece", "vooga.engine.networking.client.states.PlayState");
 		field.addControl("game", gameControl);
+	}
+	
+	public void initLevel(){
+		LevelParser levelParser = new LevelParser();
+		PlayField gameWonField = levelParser.getPlayfield(Resources.getString("gameWonXml"), game);
+		gameWonState = new GameWonState(game, gameWonField);
+		game.getGameStateManager().addGameState(gameWonState);
+		eventPool = new EventPool();
+		eventPool.addEvent(new GameWonEvent(field, this));
 	}
 	
 	public void addPiece(){
@@ -46,8 +64,6 @@ public class PlayState extends GameState{
 		int pieceY = (mouseY / Resources.getInt("squareDimension")) * Resources.getInt("squareDimension");
 		pieceX = xTurn ? pieceX + Resources.getInt("xOffsetX") : pieceX + Resources.getInt("oOffsetX");
 		pieceY = xTurn ? pieceY + Resources.getInt("xOffsetY") : pieceY + Resources.getInt("oOffsetY");
-		System.out.println(mouseX + " " + pieceX);
-		System.out.println(mouseY + " " + pieceY);
 		SpriteGroup xGroup = field.getGroup("xGroup");
 		SpriteGroup oGroup = field.getGroup("oGroup");
 		boolean add = true;
@@ -76,5 +92,18 @@ public class PlayState extends GameState{
 			}
 		}
 		xTurn = !xTurn;
+	}
+	
+	public void setWon(boolean didWin){
+		won = didWin;
+	}
+	
+	@Override
+	public void update(long elapsedTime){
+		if(won){
+			game.getGameStateManager().switchTo(gameWonState);
+		}
+		super.update(elapsedTime);
+		eventPool.checkEvents();
 	}
 }
