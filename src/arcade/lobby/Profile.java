cce2bfd1043874cd3c114902d08c031b1f8fe7f7
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-04 19:25:09 +0000

    beginning lobby model

---
+++ src/arcade/lobby/Profile.java
@@ -0,0 +1,22 @@
+package arcade.lobby;
+
+import java.sql.Date;
+
+public class Profile {
+	private String myUserName;
+	private String myFirstName;
+	private String myLastName;
+	private Date myBirthday;
+	// TODO Add more fields (table columns)
+	
+	public Profile(String userName) {
+		myUserName = userName;
+	}
+	
+	public void setName(String first,String last) {
+		myFirstName = first;
+		myLastName = last;
+	}
+	
+
+}
