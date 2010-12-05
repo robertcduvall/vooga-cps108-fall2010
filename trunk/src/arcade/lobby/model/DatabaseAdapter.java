2a7a6489e77e4797500aeace5aa67807140b3766
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 22:42:40 +0000

    Updates to lobby

--- trunk/src/arcade/lobby/model/DatabaseAdapter.java
+++ trunk/src/arcade/lobby/model/DatabaseAdapter.java
@@ -5,9 +5,7 @@ import java.util.Map;
 public interface DatabaseAdapter {	
 	
 	public String[] getColumn(String tableName, String columnName);
-	
-	public String[] getColumn(String tableName, int columnIndex);
-	
+		
 	Map<String, String> getRow(String tableName, String pkName);
 
 	boolean insert(String tableName, Map<String, String> row);
--- trunk/src/arcade/lobby/model/MySqlTest.java
+++ trunk/src/arcade/lobby/model/MySqlTest.java
@@ -1,22 +1,21 @@
 package arcade.lobby.model;
 
 public class MySqlTest {
-	
+
 	public static void main(String[] args) {
 		String host = "voogaarcade.db.7093929.hostedresource.com";
 		String dbName = "voogaarcade";
 		String user = dbName;
 		String pass = "Vooga108";
-		DatabaseAdapter db = new MySqlAdapter(host, dbName, user, pass);
-		
-		printColumn(db,"User_Name");
-	}
-	
-	private static void printColumn(DatabaseAdapter db, String field) {
-		String[] userNames = db.getColumn("users", field);
-		for(String s : userNames) {
-			System.out.println(s);
+		ProfileSet ps = new ProfileSet(host, dbName, "Users", user, pass);
+		Profile prof = new Profile(String.valueOf(Math.random()));
+		prof.setName("Testy", "McTesterson");
+		ps.addProfile(prof);
+		for (Profile p : ps) {
+			System.out.println(String.format("Name: %s, Username: %s, Birthday: %s",
+					p.getFullName(),p.getUserName(),p.getBirthday()));
 		}
+
 	}
 
 }
