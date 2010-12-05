61be9ea3433827a1af8bd49c0bb13ccb2e70ff0e
sfeldsott <sfeldsott@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 18:49:35 +0000

    Added some get/set methods.

--- src/arcade/lobby/model/Profile.java
+++ src/arcade/lobby/model/Profile.java
@@ -6,6 +6,7 @@ public class Profile {
 	private String myUserName;
 	private String myFirstName;
 	private String myLastName;
+	private String myEmail;
 	private Date myBirthday;
 	// TODO Add more fields (table columns)
 	
@@ -16,6 +17,26 @@ public class Profile {
 	public void setName(String first,String last) {
 		myFirstName = first;
 		myLastName = last;
+	}
+	
+	public void setBirthday(Date date) {
+		myBirthday = date;
+	}
+	
+	public void setEmail(String email) {
+		myEmail = email;
+	}
+	
+	public String getName() {
+		return myFirstName + " "+myLastName;
+	}
+	
+	public String getBirthday() {
+		return myBirthday.toString();
+	}
+	
+	public String getEmail() {
+		return myEmail;
 	}
 	
 
