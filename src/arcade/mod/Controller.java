72ab2b48640196798adc4f400f99b2cd5e88db82
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 02:45:54 +0000

    Mod Evironment Starting creating a Model interface and XMLModel implementation.
    General:
    Removed two files from top directory (a random picture and config file)

--- src/arcade/mod/Controller.java
+++ src/arcade/mod/Controller.java
@@ -1,14 +1,29 @@
 package arcade.mod;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import arcade.mod.model.Model;
+import arcade.mod.model.XMLModel;
 
 public class Controller {
 
 	Model myModel;
 	
-	public Controller(){
+	public Controller() {
 		View modView = new View(this);
-		myModel = new Model();
+		File xmlFile = null; //TODO replace null with file
+		try {
+			myModel = new XMLModel(xmlFile);
+		} catch (Exception e) {
+			//TODO show dialogue in view
+			e.printStackTrace();
+		}
 	}
 	
 	public Collection<String> getCategories(){
---
+++ src/arcade/mod/model/Model.java
@@ -0,0 +1,9 @@
+package arcade.mod.model;
+
+import java.util.Collection;
+
+public interface Model {
+	
+	public Collection<String> getCategories();
+
+}
