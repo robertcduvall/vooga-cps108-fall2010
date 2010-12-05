f61ddde84ed9fe00790db1d660efa3beb3859be5
sfeldsott <sfeldsott@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 19:04:38 +0000

    HomePage created.

---
+++ src/arcade/lobby/view/MainFrame.java
@@ -0,0 +1,69 @@
+package arcade.lobby.view;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Rectangle;
+import java.awt.GridBagLayout;
+
+import arcade.lobby.view.HomePage;
+import arcade.lobby.model.Profile;
+import java.awt.Dimension;
+import java.sql.Date;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel jContentPane = null;
+	private JPanel jPanel = null;
+
+	/**
+	 * This is the default constructor
+	 */
+	public MainFrame() {
+		super();
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(467, 246);
+		this.setContentPane(getJContentPane());
+		this.setTitle("JFrame");
+	}
+
+	/**
+	 * This method initializes jContentPane
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new JPanel();
+			jContentPane.setLayout(null);
+			jContentPane.add(getJPanel(), null);
+		}
+		return jContentPane;
+	}
+
+	/**
+	 * This method initializes jPanel	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	private JPanel getJPanel() {
+		if (jPanel == null) {
+			Profile bob = new Profile("bob");
+			bob.setName("Bob", "Smith");
+			bob.setBirthday(new Date(89, 6, 10));
+			bob.setEmail("abc123@cs.duke.edu");
+			jPanel = new HomePage(bob);
+		}
+		return jPanel;
+	}
+
+}  //  @jve:decl-index=0:visual-constraint="10,10"
