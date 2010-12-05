a5a906ab6937fef143b7a770fb33791ff3be4b57
jimmymu123456789 <jimmymu123456789@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 02:18:48 +0000

    organizer interface

--- trunk/src/arcade/store/organizer/Organizer.java
+++ trunk/src/arcade/store/organizer/Organizer.java
@@ -1,5 +1,12 @@
 package arcade.store.organizer;
 
-public class Organizer {
+import java.util.List;
 
+import arcade.store.page.GamePage;
+
+public interface Organizer {
+
+	public List<GamePage> organize(List<GamePage> list);
+	
+	
 }
