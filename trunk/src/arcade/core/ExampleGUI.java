a7e1b7f021a197b447a86fd167b177708a827dae
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 23:38:29 +0000

    editing panels

--- trunk/src/arcade/core/ExampleGUI.java
+++ trunk/src/arcade/core/ExampleGUI.java
@@ -1,18 +1,28 @@
 package arcade.core;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Toolkit;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
 
 public class ExampleGUI extends JFrame{
 
@@ -25,11 +35,7 @@ public class ExampleGUI extends JFrame{
 
 	public ExampleGUI()
 	{
-		codeL = new JLabel("Enter the 9 digit code: ", SwingConstants.RIGHT);
-		displayL = new JLabel("Your total Howl Points: ", SwingConstants.RIGHT);
-
-		codeTF = new JTextField(10);
-		displayTF = new JTextField(10);
+		/*
 
 		JTabbedPane tabbedPane = new JTabbedPane();
 
@@ -75,6 +81,8 @@ public class ExampleGUI extends JFrame{
 		setSize(xSize,ySize);  
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		*/
+		makeProPanel();
 	}
 	
 	protected JComponent makeTextPanel(String text) {
@@ -85,7 +93,76 @@ public class ExampleGUI extends JFrame{
         panel.add(filler);
         return panel;
     }
+	
+	protected void makeProPanel() {
+		JPanel basic = new JPanel();
+	    basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+	    add(basic);
 
+	    JPanel topPanel = new JPanel(new BorderLayout(0, 0));
+	    topPanel.setMaximumSize(new Dimension(450, 20));
+	    JLabel hint = new JLabel("Rating Systems");
+	    hint.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
+	    topPanel.add(hint);
+
+	    ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
+	    Image scaled = icon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+	    icon = new ImageIcon(scaled);
+	    JLabel label = new JLabel(icon);
+	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	    topPanel.add(label, BorderLayout.EAST);
+	    
+	    //hint.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+	    JSeparator separator = new JSeparator();
+	    separator.setForeground(Color.gray);
+
+	    topPanel.add(separator, BorderLayout.SOUTH);
+
+	    basic.add(topPanel);
+
+	    JPanel textPanel = new JPanel(new BorderLayout());
+	    textPanel.setBorder(BorderFactory.createEmptyBorder(15, 25, 15, 25));
+	    JTextPane pane = new JTextPane();
+
+	    pane.setContentType("text/html");
+	    String text = "<p><b>Closing windows using the mouse wheel</b></p>" +
+	        "<p>Clicking with the mouse wheel on an editor tab closes the window. " +
+	        "This method works also with dockable windows or Log window tabs.</p>";
+	    pane.setText(text);
+	    pane.setEditable(false);
+	    textPanel.add(pane);
+
+	    basic.add(textPanel);
+
+	    JPanel boxPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 0));
+
+	    JCheckBox box = new JCheckBox("Show Tips at startup");
+	    box.setMnemonic(KeyEvent.VK_S);
+
+	    boxPanel.add(box);
+	    basic.add(boxPanel);
+
+	    JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+	    JButton ntip = new JButton("Next Tip");
+	    ntip.setMnemonic(KeyEvent.VK_N);
+	    JButton close = new JButton("Close");
+	    close.setMnemonic(KeyEvent.VK_C);
+
+	    bottom.add(ntip);
+	    bottom.add(close);
+	    basic.add(bottom);
+
+	    bottom.setMaximumSize(new Dimension(450, 0));
+
+	    setSize(new Dimension(450, 350));
+	    setResizable(false);
+	    setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	    setLocationRelativeTo(null);
+	    setVisible(true);
+    }
+	
 	private class RedeemButtonHandler implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
