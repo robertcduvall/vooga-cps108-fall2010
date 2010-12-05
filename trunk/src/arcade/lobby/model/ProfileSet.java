8e5912ab108c8abaed3a37981cdc3778b9c9bde4
dgherzka <dgherzka@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 19:24:29 +0000

    Updated DatabaseAdapter interface

--- trunk/src/arcade/lobby/model/ProfileSet.java
+++ trunk/src/arcade/lobby/model/ProfileSet.java
@@ -1,59 +1,48 @@
 package arcade.lobby.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Iterator;
+import java.util.Map;
 
 public class ProfileSet implements Iterable<Profile> {
 	public DatabaseAdapter myDbAdapter;
 	public String myTable;
-	
-	public ProfileSet(String host, String dbName, String tableName, String user, String pass) {
-		myDbAdapter = new DatabaseAdapter(host,dbName,user,pass);
+
+	public ProfileSet(String host, String dbName, String tableName,
+			String user, String pass) {
+		myDbAdapter = new MySqlAdapter(host, dbName, user, pass);
 	}
 
 	public int size() {
-		ResultSet col = myDbAdapter.getColumn(myTable,0);
-		try {
-			return col.getFetchSize();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return -1;
+		String[] col = myDbAdapter.getColumn(myTable, 0);
+		return col.length;
 	}
-	
+
 	public boolean addProfile(Profile profile) {
-		//TODO add column names
+		// TODO add column names
 		String[] columns = {};
-		//TODO add values in same order as columns
+		// TODO add values in same order as columns
 		String[] values = new String[columns.length];
-		return myDbAdapter.insert(columns,values);
+		return myDbAdapter.insert(columns, values);
 	}
-	
+
 	public Profile getProfile(String userName) {
-		ResultSet row = myDbAdapter.getRow(userName);
+		Map<String, String> row = myDbAdapter.getRow(userName);
 		Profile userProf = new Profile(userName);
-		try {
-			userProf.setName(row.getString("fName"),row.getString("lName"));
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		userProf.setName(row.get("First_Name"), row.get("Last_Name"));
 		return null;
 	}
-	
+
 	public Profile getProfile(int row) {
 		return null;
 	}
 
 	@Override
 	public Iterator<Profile> iterator() {
-		return new Iterator<Profile>(){
+		return new Iterator<Profile>() {
 
 			int mySize = size();
 			int myLoc = 0;
-			
+
 			@Override
 			public boolean hasNext() {
 				return myLoc < mySize;
@@ -70,9 +59,9 @@ public class ProfileSet implements Iterable<Profile> {
 			 * This method will not be implemented
 			 */
 			public void remove() {
-				throw(new UnsupportedOperationException());
+				throw (new UnsupportedOperationException());
 			}
-			
+
 		};
 	}
 
