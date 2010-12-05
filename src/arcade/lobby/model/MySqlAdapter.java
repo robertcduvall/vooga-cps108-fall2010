98afa58022b8c864093124bf6a91c52d102cdb49
uscfan1781 <uscfan1781@gmail.com>
2010-12-05 21:16:45 +0000

    Commiting jdbc connector and bug fix

--- src/arcade/lobby/model/MySqlAdapter.java
+++ src/arcade/lobby/model/MySqlAdapter.java
@@ -13,35 +13,22 @@ public class MySqlAdapter implements DatabaseAdapter {
 	private static Connection myDBConnection;
 
 	public MySqlAdapter(String host, String dbName, String user, String pass) {
-		connect(host, dbName, user, pass);
+		connect(host, user, pass);
 	}
 
-	private boolean connect(String host, String dbName, String user, String pass){
+	private boolean connect(String host, String user, String pass){
 		try {
 			final String driver="com.mysql.jdbc.Driver"; 
 			Class.forName(driver); //registration of the driver
-			String url = "jdbc:mysql://"+host+"/"+dbName;
 			myDBConnection = DriverManager.getConnection(host, user, pass);
 		} catch (Exception x) {
 			System.out.println("Connection failed");
 			System.out.println(x);
 			return false;
 		}
-		testInsert();
 		return true;
 	}
 
-	private void testInsert(){
-		try{
-			String sql = "INSERT INTO Users (User_Name,First_Name,Last_Name,Email) VALUES ('test1','test2','test3','test4')";
-			PreparedStatement ps = myDBConnection.prepareStatement(sql);
-			ps.executeUpdate();
-			ps.close();
-		}
-		catch(Throwable e){
-			System.out.println(e);
-		}
-	}
 
 	@Override
 	public String[] getColumn(String tableName, String columnName) {
@@ -76,7 +63,28 @@ public class MySqlAdapter implements DatabaseAdapter {
 	@Override
 	public boolean insert(String tableName, Map<String,String> row) {
 		// TODO Auto-generated method stub
-		return false;
+		String keys = "(";
+		String values = "(";
+		for(String s:row.keySet()){
+			keys += s+",";
+			values += "'"+row.get(s)+"',";
+		}
+		keys.substring(0, keys.length()-1);
+		values.substring(0, values.length()-1);
+		keys += ")";
+		values += ")";
+	
+		try{
+			String sql = "INSERT INTO "+tableName+" "+ keys +"VALUES "+values;
+			PreparedStatement ps = myDBConnection.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch(Throwable e){
+			System.out.println(e);
+			return false;
+		}
+		return true;
 	}
 
 }
