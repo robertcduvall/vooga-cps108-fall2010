6bb5bd573fa50ba793994b2f737f8e8a3266fdab
seebop <seebop@gmail.com>
2010-12-05 04:55:40 +0000

    Added a basic RatingPanel that I hope will be usable by other API. Still working on how to customize it.

---
+++ src/arcade/wall/gui/RatingPanel.java
@@ -0,0 +1,40 @@
+package arcade.wall.gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class RatingPanel extends JPanel implements ActionListener {
+
+	
+	public RatingPanel(int scale){
+		ButtonGroup group = new ButtonGroup();
+		JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(1, 0));
+        
+		for (int i = 1; i < scale+1; i ++){
+			JRadioButton thisButton = new JRadioButton(""+i);
+			thisButton.setActionCommand(""+i);
+			group.add(thisButton);
+			radioPanel.add(thisButton);
+		}
+       
+        setLayout(new BorderLayout());
+        add(radioPanel, BorderLayout.SOUTH);
+        setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+	
+	
+}
