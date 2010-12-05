443c39cd170035408be780ca0db247141aeb9a68
jimmymu123456789 <jimmymu123456789@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 02:06:24 +0000

    added organizer

--- trunk/src/arcade/store/page/GamePage.java
+++ trunk/src/arcade/store/page/GamePage.java
@@ -1,16 +1,18 @@
 package arcade.store.page;
 
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.util.*;
 
+import javax.swing.JFrame;
+
 import arcade.store.account.UserShopAccount;
 
-public class GamePage {
+public class GamePage extends StorePage{
 	
 	private String gameName;
 	
 	private BufferedImage coverImage;
-	
 	
 	private String description;
 	private double rating;
@@ -21,9 +23,9 @@ public class GamePage {
 	
 	private List<String> tags;
 	
-	public GamePage()
+	public GamePage(JFrame surface)
 	{
-		
+		super(surface);
 		
 	}
 	
@@ -38,10 +40,19 @@ public class GamePage {
 		//TODO: fill in here!
 	}
 	
-	private boolean hasPrivilege(UserShopAccount user)
-	{
-		//TODO : fill in here!
-		return false;
+
+
+	@Override
+	public void paint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void processOnEvent(InputEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
