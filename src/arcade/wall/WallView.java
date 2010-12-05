739807ca2cd823836292d56804d60a19fbc2da91
seebop <seebop@gmail.com>
2010-12-05 02:37:27 +0000

    Controller as constructor to View?

--- src/arcade/wall/WallView.java
+++ src/arcade/wall/WallView.java
@@ -1,5 +1,85 @@
-package arcade.wall;
+package arcade.wall.gui;
 
-public class WallView {
+import java.awt.*;
+import java.awt.event.*;
 
+import javax.swing.*;
+
+import arcade.wall.WallController;
+
+public class WallView extends JPanel {
+	WallController myController;
+	static JFrame frame;
+
+	JPanel entryPanel;
+	JPanel displayPanel;
+
+	JButton reviewButton;
+	JLabel commentsLabel;
+
+	static JTextField commentEntryField;
+	JComboBox gameChoices;
+	
+	public static final String myName = "gamer13";
+	public static final String[] choices = { "Grandius", "Zombieland", "Jumper", 
+			"Doodlejump", "Galaxy Invaders", "Cyberion", 
+			"Tron", "MarioClone" };
+
+	public WallView(WallController controller) {
+		myController = controller;
+		entryPanel = new JPanel();
+		displayPanel = new JPanel();
+
+		reviewButton = new JButton("Review");
+		reviewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				controller.addComment(choices[gameChoices.getSelectedIndex()],
+									  commentEntryField.getText(), myName);
+				commentsLabel.setText("<html>" + 
+									  commentsLabel.getText() +  
+									  "<br/>" + 
+									  commentEntryField.getText() + "  ---" + myName +
+									  "</html>");
+			}
+		});
+		commentEntryField = new JTextField(17);
+		gameChoices = new JComboBox();
+		gameChoices = new JComboBox(choices);
+		gameChoices.setSelectedIndex(0);
+		commentsLabel = new JLabel("Comments for " + choices[gameChoices.getSelectedIndex()] + ":");
+		gameChoices.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+			    if ("comboBoxChanged".equals(event.getActionCommand())) {
+			        commentsLabel.setText("Comments for " + choices[gameChoices.getSelectedIndex()] + ":");
+			    }
+			}
+		});
+		
+		entryPanel.add(gameChoices);
+		entryPanel.add(commentEntryField);
+		entryPanel.add(reviewButton);
+		//entryPanel.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		displayPanel.add(commentsLabel);
+
+		this.setLayout(new GridLayout(2,2,5,5));
+		//this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		this.add(entryPanel);
+		this.add(displayPanel);
+	}
+
+	public static JTextField getCommentEntryField() {
+		return commentEntryField;
+	}
+
+	public static void main(String s[]) {
+		frame = new JFrame("WallDemo");
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {System.exit(0);}
+		});
+
+		frame.getContentPane().add(new WallView(), BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
