80534099376f0acfd6afc0b42920fce9a898202f
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 18:36:27 +0000

    Mod Model now has a ReosourceNode interface for storing trees of Nodes and an XMLNode implementation based on the w3c.dom.Node. The node is capable of getting children, getting a list of attributes, getting/setting individual attributes, and getting a description.

--- src/arcade/mod/model/Model.java
+++ src/arcade/mod/model/Model.java
@@ -1,9 +1,12 @@
 package arcade.mod.model;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface Model {
 	
 	public Collection<String> getCategories();
+	
+	public List<ResourceNode> getResourcesFromCategory(String category);
 
 }
---
+++ src/arcade/mod/model/XMLNode.java
@@ -0,0 +1,67 @@
+package arcade.mod.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLNode implements ResourceNode{
+	
+	public static final String DESCRIPTION_TAG = "Description";
+	Node node;
+	
+	public XMLNode(Node node){
+		this.node = node;
+	}
+	
+	public List<ResourceNode> getChildren(){
+		return wrapNodeList(node.getChildNodes());
+	}
+	
+	public Collection<String> getAttributes(){
+		Collection<String> attributeNames = new ArrayList<String>();
+		NamedNodeMap attributeNodeList = node.getAttributes();
+		for(int i=0; i<attributeNodeList.getLength(); i++){
+			Node attribute  = attributeNodeList.item(i);
+			attributeNames.add(attribute.getLocalName());
+		}
+		return attributeNames;
+	}
+	
+	
+	private List<ResourceNode> wrapNodeList(NodeList nodeList){
+		List<ResourceNode> resourceNodes = new ArrayList<ResourceNode>();
+		for(int i=0; i<nodeList.getLength(); i++){
+			resourceNodes.add(new XMLNode(nodeList.item(i)));
+		}
+		
+		return resourceNodes;
+	}
+
+	@Override
+	public String getAttribute(String attributeName) {
+		NamedNodeMap attributeNodeList = node.getAttributes();
+		Node attribute = attributeNodeList.getNamedItem(attributeName);
+		return attribute.getNodeValue();
+	}
+
+	@Override
+	public void setAttribute(String attributeName, String value) {
+		NamedNodeMap attributeNodeList = node.getAttributes();
+		Node attribute = attributeNodeList.getNamedItem(attributeName);
+		attribute.setNodeValue(value);
+	}
+
+	@Override
+	public String getDescription() {
+		Node child = node.getFirstChild();
+		while(child!= null && child.getLocalName().equalsIgnoreCase(DESCRIPTION_TAG)){
+			child = child.getNextSibling();
+		}
+		return child.getTextContent();
+	}
+
+}
