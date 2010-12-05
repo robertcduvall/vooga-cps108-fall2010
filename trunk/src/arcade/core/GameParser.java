96c909c9320e706f62fe3b77da6d5255ebf2cc11
aachoi72 <aachoi72@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-05 21:58:29 +0000

    Updated GameParser, added GameView Panel

--- trunk/src/arcade/core/GameParser.java
+++ trunk/src/arcade/core/GameParser.java
@@ -1,5 +1,37 @@
 package arcade.core;
 
+import java.util.Map;
+import java.util.ResourceBundle;
+
+
+
 public class GameParser {
 
+	private ResourceBundle properties;
+	public void parseGame(String game, Map<String, String[]> gameMap)
+	{
+		try {
+			
+			String gamePropertiesFile = "src.vooga.games." + game + "resources.game.properties";
+			properties = ResourceBundle.getBundle(gamePropertiesFile);
+			gameMap.put("genre", new String[]{properties.getString("genre")});
+			parseTags(gameMap);
+			parseInstructions(gameMap);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parseTags(Map<String, String[]> gameMap)
+	{
+		String[] tags = properties.getString("tags").split(",");
+		gameMap.put("tags", tags);
+	}
+	
+	private void parseInstructions(Map<String, String[]> gameMap)
+	{
+		String[] instructions = properties.getString("instructions").split(",");
+		gameMap.put("instructions", instructions);
+	}
 }
