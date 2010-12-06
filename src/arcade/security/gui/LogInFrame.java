40894167c41844be23c05a492660efc0a935de3e
laz3638 <laz3638@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 06:58:04 +0000

    Added checkboxes for toggling privileges on and off to DeveloperConfigFrame

--- src/arcade/security/gui/LogInFrame.java
+++ src/arcade/security/gui/LogInFrame.java
@@ -173,7 +173,7 @@ public class LogInFrame extends JFrame {
 
 	public static void main(String[] args){
 		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
