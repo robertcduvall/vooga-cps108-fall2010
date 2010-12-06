5c4e4a006f3206546ff5dbf9fa3e9e7b466e6f0e
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 02:23:57 +0000

    Completed Example GUI

--- trunk/src/arcade/core/ExampleGUI.java
+++ trunk/src/arcade/core/ExampleGUI.java
@@ -1,26 +1,24 @@
 package arcade.core;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -34,9 +32,13 @@ public class ExampleGUI extends JFrame{
 
 	private StartButtonHandler sbHandler;
 	private ExitButtonHandler ebHandler;
+	
+	private Toolkit tk;
 
 	public ExampleGUI()
 	{
+		tk = Toolkit.getDefaultToolkit();
+		
 		JTabbedPane tabbedPane = new JTabbedPane();
 
 		JComponent panel1 = makeCustomPanel();
@@ -55,7 +57,6 @@ public class ExampleGUI extends JFrame{
         
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
 
-		Toolkit tk = Toolkit.getDefaultToolkit();  
 		int xSize = ((int) tk.getScreenSize().getWidth());  
 		int ySize = ((int) tk.getScreenSize().getHeight());  
 
@@ -74,89 +75,35 @@ public class ExampleGUI extends JFrame{
         return panel;
     }
 	
-	protected void makeProPanel() {
-		JPanel basic = new JPanel();
-	    basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
-	    add(basic);
-
-	    JPanel topPanel = new JPanel(new BorderLayout(0, 0));
-	    topPanel.setMaximumSize(new Dimension(450, 20));
-	    JLabel hint = new JLabel("Rating Systems");
-	    hint.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 0));
-	    topPanel.add(hint);
-
-	    ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
-	    Image scaled = icon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
-	    icon = new ImageIcon(scaled);
-	    JLabel label = new JLabel(icon);
-	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-	    topPanel.add(label, BorderLayout.EAST);
-	    
-	    //hint.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-	    JSeparator separator = new JSeparator();
-	    separator.setForeground(Color.gray);
-
-	    topPanel.add(separator, BorderLayout.SOUTH);
-
-	    basic.add(topPanel);
-
-	    JPanel textPanel = new JPanel(new BorderLayout());
-	    textPanel.setBorder(BorderFactory.createEmptyBorder(15, 25, 15, 25));
-	    JTextPane pane = new JTextPane();
-
-	    pane.setContentType("text/html");
-	    String text = "hello world";
-	    pane.setText(text);
-	    pane.setEditable(false);
-	    textPanel.add(pane);
-
-	    basic.add(textPanel);
-
-	    JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
-	    JButton ntip = new JButton("Next Tip");
-	    ntip.setMnemonic(KeyEvent.VK_N);
-	    JButton close = new JButton("Close");
-	    close.setMnemonic(KeyEvent.VK_C);
-
-	    bottom.add(ntip);
-	    bottom.add(close);
-	    basic.add(bottom);
-
-	    bottom.setMaximumSize(new Dimension(450, 0));
-
-	    setSize(new Dimension(450, 350));
-	    setResizable(false);
-	    setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-	    setLocationRelativeTo(null);
-	    setVisible(true);
-    }
-	
 	private JComponent makeCustomPanel() {
-		
-		JPanel topPanel = new JPanel(new FlowLayout());
+	    JSplitPane columnar = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+	    		makeLeftPanel(), makeCenterPanel());
+	    columnar.setOneTouchExpandable(true);
+	    columnar.setDividerLocation(.25);
 	    
-	    //hint.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	    JSplitPane mainPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+	    		columnar, makeRightPanel());
+	    mainPanel.setOneTouchExpandable(true);
+	    mainPanel.setDividerLocation(.75);
 	    
-	    topPanel.add(makeLeftPanel());
-	    topPanel.add(makeCenterPanel());
-	    topPanel.add(makeRightPanel());
-	    
-	    return topPanel;
+	    return mainPanel;
     }
 	private JComponent makeCenterPanel()
-	{
-		JPanel center = new JPanel();
-		center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+	{	
+		JPanel top = new JPanel();
+		int xSizeOfColumn =(int) ( tk.getScreenSize().getWidth()/2);  
+		int ySizeOfColumn = ((int) tk.getScreenSize().getHeight()/2);
+		
+		top.setPreferredSize(new Dimension(xSizeOfColumn, ySizeOfColumn));
+		top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+		
+		JPanel bottom = new JPanel();
+		
+		bottom.setPreferredSize(new Dimension(xSizeOfColumn, ySizeOfColumn));
+		bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
 		
 		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
 	    JLabel label = new JLabel(icon);
-	    
-	    JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
-	    separator.setForeground(Color.BLUE);
-	    JSeparator moreSeparation = new JSeparator(SwingConstants.HORIZONTAL);
-        moreSeparation.setForeground(Color.BLUE);
 	    
 	    
 	    JTextPane descriptionPane = new JTextPane();
@@ -170,52 +117,65 @@ public class ExampleGUI extends JFrame{
         descriptionPane.setEditable(false);
         
         startB = new JButton("Start");
+        startB.setPreferredSize(new Dimension(400, 50));
 		sbHandler = new StartButtonHandler();
 		startB.addActionListener(sbHandler);
         
 		label.setAlignmentX(CENTER_ALIGNMENT);
-		separator.setAlignmentX(CENTER_ALIGNMENT);
 		descriptionPane.setAlignmentX(CENTER_ALIGNMENT);
-		moreSeparation.setAlignmentX(CENTER_ALIGNMENT);
 		startB.setAlignmentX(CENTER_ALIGNMENT);
 		
-        center.add(label);
-        center.add(separator);
-        center.add(descriptionPane);
-        center.add(moreSeparation);
-        center.add(startB);
+		top.add(label);
+		bottom.add(descriptionPane);
+		bottom.add(new Box.Filler(new Dimension(0, 20),
+				new Dimension(0, 20),
+				new Dimension(0, 20)));
+		bottom.add(startB);
+		bottom.add(new Box.Filler(new Dimension(0, 20),
+				new Dimension(0, 20),
+				new Dimension(0, 20)));
         
 		/*exitB = new JButton("Exit");
 		ebHandler = new ExitButtonHandler();
 		exitB.addActionListener(ebHandler);*/
         
-        center.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        
+        JSplitPane gameInstruction = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+	    		top, bottom);
+	    gameInstruction.setOneTouchExpandable(true);
+	    gameInstruction.setDividerLocation(.75);
+	    gameInstruction.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    
-	    return center;
+	    return gameInstruction;
 	}
 	private JComponent makeLeftPanel()
 	{
 		JPanel left = new JPanel();
+		int xSizeOfColumn =(int) ( tk.getScreenSize().getWidth()/5);  
+		int ySizeOfColumn = ((int) tk.getScreenSize().getHeight()/2);
+		
+		left.setMinimumSize(new Dimension(xSizeOfColumn, ySizeOfColumn));
 		left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
 		
 		JLabel rateThis = new JLabel("Rate This Game");
-		left.add(rateThis);
 		
 		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
 	    Image scaled = icon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
 	    icon = new ImageIcon(scaled);
 	    JLabel label = new JLabel(icon);
-	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-	    left.add(label);
 	    
 	    JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
 	    separator.setForeground(Color.BLUE);
-	    left.add(separator);
 	    
 	    JLabel rateOthers = new JLabel("Rate These Other Games");
-		left.add(rateOthers);
 		
 		JLabel moreLabels = new JLabel(icon);
+	    
+	    
+	    left.add(rateThis);
+	    left.add(label);
+	    left.add(separator);
+	    left.add(rateOthers);
 	    left.add(moreLabels);
 	    
 	    left.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -225,13 +185,16 @@ public class ExampleGUI extends JFrame{
 	private JComponent makeRightPanel()
 	{
 		JPanel right = new JPanel();
+		int xSizeOfColumn =(int) ( tk.getScreenSize().getWidth()/5);  
+		int ySizeOfColumn = ((int) tk.getScreenSize().getHeight()/2);
+		
+		right.setMinimumSize(new Dimension(xSizeOfColumn, ySizeOfColumn));
 	    right.setLayout(new GridLayout(3, 0));
 	    
 	    ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
 	    Image scaled = icon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
 	    icon = new ImageIcon(scaled);
 	    JLabel label = new JLabel(icon);
-	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 	    
 	    JPanel playerAvatar = new JPanel();
 	    playerAvatar.setLayout(new BoxLayout(playerAvatar, BoxLayout.Y_AXIS));
