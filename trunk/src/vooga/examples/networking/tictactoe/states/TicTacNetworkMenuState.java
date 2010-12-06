6dbae5d3925710bd77fbb79ded3b46955b205617
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 18:45:02 +0000

    implemented networkmenu

---
+++ trunk/src/vooga/examples/networking/tictactoe/states/TicTacNetworkMenuState.java
@@ -0,0 +1,21 @@
+package vooga.examples.networking.tictactoe.states;
+
+import vooga.engine.core.Game;
+import vooga.engine.networking.client.states.NetworkMenuState;
+
+
+
+public class TicTacNetworkMenuState extends NetworkMenuState{
+	
+	public TicTacNetworkMenuState(Game g) {
+		super(g);
+		config = "vooga/examples/networking/tictactoe/resources/config.properties";
+
+	}
+	
+	public void initialize() {
+		setEnterAddress("vooga.examples.networking.tictactoe.TicTacToe");
+		super.initialize(342, 333);
+	}
+
+}
