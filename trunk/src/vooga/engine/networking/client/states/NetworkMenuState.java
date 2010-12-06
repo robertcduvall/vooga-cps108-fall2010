a79b59ac49cf0fe59dd5f514c171465c242eb1cf
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 18:45:46 +0000

    made networkMenuState more extendable

--- trunk/src/vooga/engine/networking/client/states/NetworkMenuState.java
+++ trunk/src/vooga/engine/networking/client/states/NetworkMenuState.java
@@ -22,18 +22,18 @@ import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
 import com.golden.gamedev.util.ImageUtil;
 
-public class NetworkMenuState extends GameState{
+public abstract class NetworkMenuState extends GameState{
 
 	private Game game;
 	private KeyboardControl keyboardControl;
 	private MouseControl mouseControl;
-	private String config = "vooga/examples/networking/tictactoe/resources/config.properties";
+	protected String config;
+	protected String enterAddress;
 	private BufferedImage DEFAULT_NETWORK_MENU_BACKGROUND = Resources.getImage("networkMenuBackground");
 	private PlayField menuPlayField = new PlayField();
 
 	public NetworkMenuState(Game g) {
 		game = g;
-		
 		keyboardControl = new KeyboardControl(game, game);
 		mouseControl = new MouseControl(game, game);
 	}
@@ -45,17 +45,22 @@ public class NetworkMenuState extends GameState{
 		buttons.add((Sprite) new NetworkMenuButton(Resources.getImage("networkMenuButtonPrivate"), 0 , 60));
 		buttons.add((Sprite) new NetworkMenuButton(Resources.getImage("networkMenuButtonRandom"), 0 , 90));
 		return buttons;
+		
+	}
+	
+	protected void setEnterAddress (String address) {
+		enterAddress = address;
 	}
 
-	public void initialize() {
+	public void initialize(int windowWidth, int windowHeight) {
 		//ResourceBundle rb = ResourceBundle.getBundle(config);
-		BufferedImage menuBackground = ImageUtil.resize(	DEFAULT_NETWORK_MENU_BACKGROUND, 342, 333);
+		BufferedImage menuBackground = ImageUtil.resize(	DEFAULT_NETWORK_MENU_BACKGROUND, windowWidth, windowHeight);
 												//Integer.parseInt(rb.getString("GAME_WIDTH")),
 												//Integer.parseInt(rb.getString("GAME_HEIGHT")));
 		Sprite menuBackGroundSprite = new Sprite(DEFAULT_NETWORK_MENU_BACKGROUND, 0, 0);
 		//menuPlayField.addGroup(createButtons());
 		menuPlayField.add(menuBackGroundSprite);
-		getKeyboardControl().addInput(KeyEvent.VK_ENTER, "playGame", "vooga.examples.networking.tictactoe.TicTacToe");
+		getKeyboardControl().addInput(KeyEvent.VK_ENTER, "startWaitState", enterAddress);
 		menuPlayField.addControl("keyboard", keyboardControl);
 		menuPlayField.addControl("mouse", mouseControl);
 		this.addPlayField(menuPlayField);
