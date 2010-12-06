767c0e0e4d01652087024cb7fe95f4c3c4cf64c8
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 04:14:12 +0000

    comments appear on new lines in JTextArea

--- trunk/src/arcade/wall/WallModel.java
+++ trunk/src/arcade/wall/WallModel.java
@@ -26,6 +26,20 @@ public class WallModel {
 		return comments;
 	}
 
+//	public WallModel() {
+//		DBHandler.open("src/arcade/wall/WallDB.sqlite");
+//		DBHandler.createTable("GameReviews", "Game", "Comments", "Rating", "Username");
+//		DBHandler.setTable("GameReviews");
+//		
+//	}
+	
+//	public void addComment(String game, String comment, String user){
+//		DBHandler.open("src/arcade/wall/WallDB.sqlite");
+//		DBHandler.setTable("GameReviews");
+//		DBHandler.addRow(new Entry<String>("Game", game),
+//				 		 new Entry<String>("Comments", comment),
+//				 		 new Entry<String>("Username", user));
+//	}
 	private Map<String,ArrayList<String>> getComments() {
 		List<String> comments = DBHandler.getColumn(commentsColumn,new String());
 		List<String> games = DBHandler.getColumn(gamesColumn,new String());
