cd6cd5a5d6c30410ce2098deb0c3d5def5bdf2a2
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 04:57:51 +0000

    presenter javadocked

--- trunk/src/arcade/mod/controller/Presenter.java
+++ trunk/src/arcade/mod/controller/Presenter.java
@@ -2,13 +2,27 @@ package arcade.mod.controller;
 
 import java.util.Collection;
 
+/**
+ * Presenter is an interface which dictates the basic logic we need for a Presenter in the MVP design structure
+ * 
+ */
 public interface Presenter {
 	
+	/**
+	 * Save the current XML file in the Model
+	 */
 	public void save();
 	
+	/**
+	 * Retrieve the categories of resource type (i.e.-images, sounds, etc.)
+	 * @return Collection of Strings that represent all available categories in the current XML file
+	 */
 	//TODO remove necessity to return Collection
 	public Collection<String> getCategories();
 	
+	/**
+	 * Do something when once a category is selected
+	 */
 	public void newCategorySelected();
 
 }
