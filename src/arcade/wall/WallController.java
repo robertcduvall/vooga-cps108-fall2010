10c70114ba936afec7bca69ed9622a1c2a499e88
bhawana686 <bhawana686@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 02:48:41 +0000

    Modified WallModel.

--- src/arcade/wall/WallController.java
+++ src/arcade/wall/WallController.java
@@ -1,14 +1,14 @@
 package arcade.wall;
 
+import java.util.List;
+
 public class WallController {
 	WallModel model = new WallModel();
 	WallView view = new WallView("Gamer13", this);
 	
-	public String addComment(String game, String comment, String user){
-		model.addComment(game, comment, user);
-		//TODO - modify so that it returns all the comments for this game from the database
-		//in View, display all these comments
-		return comment;		
+	public List<String> addComment(String game, String comment, String user){
+		List<String> comments = model.addComment(game, comment, user);
+		return comments;		
 	}
 
 }
--- src/arcade/wall/WallModel.java
+++ src/arcade/wall/WallModel.java
@@ -1,15 +1,42 @@
 package arcade.wall;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import vooga.engine.resource.DBHandler;
 import vooga.engine.resource.Entry;
 
 public class WallModel {
-
-	public void addComment(String game, String comment, String user){
-		DBHandler.open("src/arcade/wall/WallDB.sqlite");
-		DBHandler.setTable("GameReviews");
-		DBHandler.addRow(new Entry<String>("Game", game),
-				 		 new Entry<String>("Comments", comment),
-				 		 new Entry<String>("Username", user));
+	private final static String database = "src/arcade/wall/WallDB.sqlite";
+	private final static String table = "GameReviews";
+	private static final String gamesColumn = "Game";
+	private static final String commentsColumn = "Comments";
+	private static final String usernameColumn = "Username";
+	
+	public List<String> addComment(String game, String comment, String user){
+		DBHandler.open(database);
+		DBHandler.setTable(table);
+		DBHandler.addRow(new Entry<String>(gamesColumn, game),
+				 		 new Entry<String>(commentsColumn, comment),
+				 		 new Entry<String>(usernameColumn, user));
+		
+		List<String> comments = getComments().get(game);
+		return comments;
 	}
+
+	private Map<String,ArrayList<String>> getComments() {
+		List<String> comments = DBHandler.getColumn(commentsColumn,new String());
+		List<String> games = DBHandler.getColumn(gamesColumn,new String());
+		Map<String,ArrayList<String>> gameComments = new HashMap<String,ArrayList<String>>();
+		for(int i=0;i<comments.size();i++){
+			if(!gameComments.containsKey(games.get(i))){
+				gameComments.put(games.get(i),new ArrayList<String>());
+			}
+			gameComments.get(games.get(i)).add(comments.get(i));
+		}
+		return gameComments;
+	}
+	
 }
