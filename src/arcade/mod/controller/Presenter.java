ea3efc628daae64aab3990ad2ba3e982bf00b589
danielkoverman <danielkoverman@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 04:59:59 +0000

    Mod Javadoc'd Presenter and Viewer interfaces

--- src/arcade/mod/controller/Presenter.java
+++ src/arcade/mod/controller/Presenter.java
@@ -3,25 +3,43 @@ package arcade.mod.controller;
 import java.util.Collection;
 
 /**
- * Presenter is an interface which dictates the basic logic we need for a Presenter in the MVP design structure
+ * Presenter interfaces acts as the presenter
+ * in the model-view-presenter GUI paradigm. Designed 
+ * to handle events which occur in the view. This 
+ * interface provides the means for the view to 
+ * inform the presenter of new events. Ideally, they 
+ * should all have no parameters and no return value.
  * 
+ * Implementations of this interface are responsible for 
+ * most of the logic for editing and reading a model which 
+ * is not specific to a concrete model implementation.
+ * 
+ * @author Daniel Koverman
+ *
  */
 public interface Presenter {
 	
 	/**
-	 * Save the current XML file in the Model
+	 * Inform the presenter that the user is 
+	 * attempting to save.
 	 */
 	public void save();
 	
-	/**
-	 * Retrieve the categories of resource type (i.e.-images, sounds, etc.)
-	 * @return Collection of Strings that represent all available categories in the current XML file
-	 */
+
 	//TODO remove necessity to return Collection
+	/**
+	 * Ask the presenter for the available resource 
+	 * categories when the viewer starts or a new 
+	 * file is selected
+	 * (i.e.-images, sounds, etc.)
+	 */
 	public Collection<String> getCategories();
 	
+	
 	/**
-	 * Do something when once a category is selected
+	 * Inform the presenter that the user is 
+	 * attempting to view a new category of 
+	 * resources
 	 */
 	public void newCategorySelected();
 
