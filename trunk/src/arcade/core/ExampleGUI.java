032b5d413e00193b548a564dac0cd3d5eba4d61c
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 00:53:49 +0000

    layout finished

--- trunk/src/arcade/core/ExampleGUI.java
+++ trunk/src/arcade/core/ExampleGUI.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -20,6 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
@@ -28,51 +30,30 @@ public class ExampleGUI extends JFrame{
 
 	private JLabel codeL, displayL;
 	private JTextField codeTF, displayTF;
-	private JButton redeemB, exitB;
+	private JButton startB, exitB;
 
-	private RedeemButtonHandler cbHandler;
+	private StartButtonHandler sbHandler;
 	private ExitButtonHandler ebHandler;
 
 	public ExampleGUI()
 	{
-		/*
-
 		JTabbedPane tabbedPane = new JTabbedPane();
 
-		JComponent panel1 = makeTextPanel("Panel #1");
-		tabbedPane.addTab("Tab 1", null, panel1,
-		"Does nothing");
+		JComponent panel1 = makeCustomPanel();
+		tabbedPane.addTab("Arcade", null, panel1,
+		"This is the Arcade tab");
 
-		JComponent panel2 = makeTextPanel("Panel #2");
-		tabbedPane.addTab("Tab 2", null, panel2,
-		"Does twice as much nothing");
+		JComponent panel2 = makeTextPanel("Lobby");
+		tabbedPane.addTab("Lobby", null, panel2,
+		"This is the Lobby tab");
 
-		JComponent panel3 = makeTextPanel("Panel #3");
-		tabbedPane.addTab("Tab 3", null, panel3,
-		"Still does nothing");
-
-		JComponent panel4 = makeTextPanel("Panel #4");
-		tabbedPane.addTab("Tab 4", null, panel4,
-		"Does nothing at all");
-		
-		JComponent panel5 = makeTextPanel("Panel #5");
-		tabbedPane.addTab("Tab 5", null, panel5,
-		"Does nothing at all");
-
-		JComponent panel6 = makeTextPanel("Panel #6");
-		tabbedPane.addTab("Tab 6", null, panel6,
-		"Does nothing at all");
+		JComponent panel3 = makeTextPanel("Store");
+		tabbedPane.addTab("Store", null, panel3,
+		"This is the Store tab");
 		
         add(tabbedPane);
         
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-
-		redeemB = new JButton("Redeem");
-		cbHandler = new RedeemButtonHandler();
-		redeemB.addActionListener(cbHandler);
-		exitB = new JButton("Exit");
-		ebHandler = new ExitButtonHandler();
-		exitB.addActionListener(ebHandler);
 
 		Toolkit tk = Toolkit.getDefaultToolkit();  
 		int xSize = ((int) tk.getScreenSize().getWidth());  
@@ -81,8 +62,7 @@ public class ExampleGUI extends JFrame{
 		setSize(xSize,ySize);  
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		*/
-		makeCustomPanel();
+
 	}
 	
 	protected JComponent makeTextPanel(String text) {
@@ -153,7 +133,7 @@ public class ExampleGUI extends JFrame{
 	    setVisible(true);
     }
 	
-	protected void makeCustomPanel() {
+	private JComponent makeCustomPanel() {
 		
 		JPanel topPanel = new JPanel(new FlowLayout());
 	    
@@ -162,21 +142,54 @@ public class ExampleGUI extends JFrame{
 	    topPanel.add(makeLeftPanel());
 	    topPanel.add(makeCenterPanel());
 	    topPanel.add(makeRightPanel());
-	    add(topPanel);
 	    
-	    setSize(new Dimension(450, 350));
-	    setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-	    setLocationRelativeTo(null);
-	    setVisible(true);
+	    return topPanel;
     }
 	private JComponent makeCenterPanel()
 	{
 		JPanel center = new JPanel();
+		center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
 		
 		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
 	    JLabel label = new JLabel(icon);
-	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-	    center.add(label);
+	    
+	    JSeparator separator = new JSeparator(SwingConstants.HORIZONTAL);
+	    separator.setForeground(Color.BLUE);
+	    JSeparator moreSeparation = new JSeparator(SwingConstants.HORIZONTAL);
+        moreSeparation.setForeground(Color.BLUE);
+	    
+	    
+	    JTextPane descriptionPane = new JTextPane();
+	    descriptionPane.setContentType("text/html");
+        String description = "<p><b>Description:</b></p>" +
+            "<p>This is where instructions for the game would go. <br>" +
+            "For example:<br> Left Arrow = move left. <br>" +
+            "Right Arrow = move right <br>" +
+            "Spacebar = fire.</p>";
+        descriptionPane.setText(description);
+        descriptionPane.setEditable(false);
+        
+        startB = new JButton("Start");
+		sbHandler = new StartButtonHandler();
+		startB.addActionListener(sbHandler);
+        
+		label.setAlignmentX(CENTER_ALIGNMENT);
+		separator.setAlignmentX(CENTER_ALIGNMENT);
+		descriptionPane.setAlignmentX(CENTER_ALIGNMENT);
+		moreSeparation.setAlignmentX(CENTER_ALIGNMENT);
+		startB.setAlignmentX(CENTER_ALIGNMENT);
+		
+        center.add(label);
+        center.add(separator);
+        center.add(descriptionPane);
+        center.add(moreSeparation);
+        center.add(startB);
+        
+		/*exitB = new JButton("Exit");
+		ebHandler = new ExitButtonHandler();
+		exitB.addActionListener(ebHandler);*/
+        
+        center.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    
 	    return center;
 	}
@@ -205,6 +218,8 @@ public class ExampleGUI extends JFrame{
 		JLabel moreLabels = new JLabel(icon);
 	    left.add(moreLabels);
 	    
+	    left.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	    
 	    return left;
 	}
 	private JComponent makeRightPanel()
@@ -212,11 +227,19 @@ public class ExampleGUI extends JFrame{
 		JPanel right = new JPanel();
 	    right.setLayout(new GridLayout(3, 0));
 	    
+	    ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
+	    Image scaled = icon.getImage().getScaledInstance(25, 25, java.awt.Image.SCALE_SMOOTH);
+	    icon = new ImageIcon(scaled);
+	    JLabel label = new JLabel(icon);
+	    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+	    
 	    JPanel playerAvatar = new JPanel();
 	    playerAvatar.setLayout(new BoxLayout(playerAvatar, BoxLayout.Y_AXIS));
 	    playerAvatar.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    JLabel playerName = new JLabel("Player Avatar");
 	    playerAvatar.add(playerName);
+	    playerAvatar.add(label);
+	    
 	    
 	    JPanel lobby = new JPanel();
 	    lobby.setLayout(new BoxLayout(lobby, BoxLayout.Y_AXIS));
@@ -224,32 +247,33 @@ public class ExampleGUI extends JFrame{
 	    JLabel lobbyFriends = new JLabel("You have 0 friends");
 	    lobby.add(lobbyFriends);
 	    
+	    JLabel moreLabels = new JLabel(icon);
+	    lobby.add(moreLabels);
+	    
 	    JPanel ads = new JPanel();
 	    ads.setLayout(new BoxLayout(ads, BoxLayout.Y_AXIS));;
 	    ads.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    JLabel randomAd = new JLabel("Buy Coke.");
 	    ads.add(randomAd);
 	    
+	    JLabel evenMoreLabels = new JLabel(icon);
+	    ads.add(evenMoreLabels);
+	    
 	    right.add(playerAvatar);
 	    right.add(lobby);
 	    right.add(ads);
 	    
+	    right.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	    
 	    return right;
 	}
 	
-	private class RedeemButtonHandler implements ActionListener
+	private class StartButtonHandler implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
 		{
-			String code;
-
-			code = codeTF.getText(); 
-			if (code.length() !=9)displayTF.setText("invalid code");
-			else{
-				System.out.println("hah");
-				codeTF.setText("You've already redeemed this code. Try another.");
-			}
-			displayTF.setText(" points.");
+			//TODO: start game
+			System.out.println("Started Game!");
 		}
 	}
 
