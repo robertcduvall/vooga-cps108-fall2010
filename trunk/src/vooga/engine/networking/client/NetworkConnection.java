1b17508998b9da5daacef16b37bc68ea83971828
devonstownsend <devonstownsend@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 08:22:09 +0000

    added network menu

--- trunk/src/vooga/engine/networking/client/NetworkConnection.java
+++ trunk/src/vooga/engine/networking/client/NetworkConnection.java
@@ -1,5 +1,62 @@
 package vooga.engine.networking.client;
 
-public class NetworkConnection {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import vooga.engine.networking.GameSocket;
+import vooga.engine.resource.Resources;
+
+public abstract class NetworkConnection extends GameSocket {
+	
+	public NetworkConnection() throws UnknownHostException, IOException{
+		super(new Socket("localhost", Resources.getInt("portNum")));
+	}
+	
+
+	public int getData() {
+		if (!isConnected()) 
+			throw new NullPointerException("Attempted to read closed socket!");
+
+		try {
+			String receivedData = receive();
+			System.out.println("Received: " + receivedData);
+			if (receivedData == null)
+				return Resources.getInt("gameOver");
+			receivedData = receivedData.trim();
+			try {
+				return new Integer(receivedData).intValue();
+			}
+			catch (NumberFormatException e) {
+				return statusStringToInt(receivedData);
+			}
+		}
+		catch (IOException e) {
+			System.out.println("I/O Error: " + e);
+			System.exit(1);
+			return 0;
+		}
+	}
+
+	private int statusStringToInt(String s) {
+		return Resources.getInt(s.trim());
+	}
+
+	public void sendData(int col) {
+		send(Integer.toString(col));
+	}
+
+
+	public void sendData(String toSendToServer) {
+		send(toSendToServer);
+	}
+
+	public void sendIQUIT() {
+		send("IQUIT");
+	}
+	
+	public void sendGAMEOVER(){
+		send("GAMEOVER");
+	}
 }
+
---
+++ trunk/src/vooga/engine/networking/client/states/NetworkMenuButton.java
@@ -0,0 +1,21 @@
+package vooga.engine.networking.client.states;
+
+import java.awt.image.BufferedImage;
+
+import vooga.widget.Button;
+
+public class NetworkMenuButton extends Button {
+	
+	public NetworkMenuButton (BufferedImage img, double x, double y) {
+		super(img, x, y);
+		
+	}
+
+	@Override
+	public void actionPerformed() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
