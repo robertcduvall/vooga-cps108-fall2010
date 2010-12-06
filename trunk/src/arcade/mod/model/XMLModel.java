ddb8cc522ad8c29d99d50db70d34f205b358032d
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-06 05:43:58 +0000

    javadoc and deleted some unused imports

--- trunk/src/arcade/mod/model/XMLModel.java
+++ trunk/src/arcade/mod/model/XMLModel.java
@@ -37,17 +37,34 @@ public class XMLModel implements Model{
 	
 	Document document;
 	
+	/**
+	 * Creates a new instance of XMLModel from a filePath
+	 * @param pathName of the file from which to create the new XMLModel
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public XMLModel(String pathName) throws ParserConfigurationException, SAXException, IOException{
 		XMLDocumentCreator xmlCreator = new XMLFileParser(pathName);
         document = xmlCreator.getDocument();
 	}
 	
+	/**
+	 * Creates a new instance of XMLModel from a given file
+	 * @param file from which to create the new XMLModel
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
 	public XMLModel(File file) throws ParserConfigurationException, SAXException, IOException{
 		XMLDocumentCreator xmlCreator = new XMLFileParser(file);
         document = xmlCreator.getDocument();        
 	}
 
-
+	/**
+	 * Returns a collection of strings which are all available categories in the XML file
+	 * @return a collection of strings which are all available categories in the XML file
+	 */
 	@Override
 	public Collection<String> getCategories() {
 		Collection<String> categories = new HashSet<String>();
@@ -62,6 +79,11 @@ public class XMLModel implements Model{
 		return categories;
 	}
 
+	/**
+	 * Return the ResourceNodes that exist as children of a category
+	 * @param string category which is the category from which to draw children
+	 * @return List<ResourceNode> children within a specified category
+	 */
 	@Override
 	public List<ResourceNode> getResourcesFromCategory(String category) {
 		List<ResourceNode> nodes = new ArrayList<ResourceNode>();
@@ -74,6 +96,8 @@ public class XMLModel implements Model{
 	}
 
 	/**
+	 * Write resources to a specified file
+	 * @param File on which to write resources
 	 * @author anonymous [http://www.exampledepot.com/egs/javax.xml.transform/WriteDom.html]
 	 */
 	@Override
