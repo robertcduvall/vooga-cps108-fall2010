704a653bb0630a4391e22070368b8f9f163f90e1
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-07 02:57:09 +0000

    add DAO,DAOFactory and DAOImpl pattern and add data abstraction

---
+++ trunk/src/arcade/security/dao/UserDAOImpl.java
@@ -0,0 +1,35 @@
+package arcade.security.dao;
+
+public class UserDAOImpl implements UserDAO{
+
+	@Override
+	public void delete(String primaryKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByKey(String primaryKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
