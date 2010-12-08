5f1fac9f9894f8103353dab087102ff661d982ac
seebop@gmail.com <seebop@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-08 07:54:15 +0000

    Huzzah! People can now make (relatively) customizable image rating panels and radio button rating panels.

---
+++ trunk/src/arcade/wall/view/ratings/RadioPanel.java
@@ -0,0 +1,34 @@
+package arcade.wall.view.ratings;
+
+import javax.swing.AbstractButton;
+import javax.swing.JRadioButton;
+
+/**
+ * Creates a Rating Panel that is made up of a number of Radio Buttons
+ * as provided by the user. In order to change the Panel layout, please refer
+ * to the methods in the ButtonPanel superclass.
+ * 
+ * @author Cameron McCallie
+ *
+ */
+
+public class RadioPanel extends ButtonPanel {
+
+	private JRadioButton[] myRadioButtons;
+	
+	public RadioPanel(int scale) {
+		super(scale);
+		myRadioButtons = new JRadioButton[scale];
+		for (int i = 1; i < scale+1; i ++){
+			JRadioButton thisButton = new JRadioButton(""+i);
+			thisButton.setActionCommand(""+i);
+			thisButton.addActionListener(this);
+			myButtons.add(thisButton);
+			myRadioButtons[i-1] = thisButton;
+			this.add(thisButton);	
+		}
+		JRadioButton buttonTwo = (JRadioButton) addComment(myRadioButtons[2], " =)");
+	}
+	
+
+}
