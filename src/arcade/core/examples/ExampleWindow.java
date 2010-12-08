7d55b6891171a65264d1826454f46da0e2cfd0fe
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-08 01:25:29 +0000

    [store] Pop Up Ads are annoying, so I added the Troll Face

--- src/arcade/core/examples/ExampleWindow.java
+++ src/arcade/core/examples/ExampleWindow.java
@@ -18,7 +18,8 @@ public class ExampleWindow extends Window {
 	@Override
 	protected void createContents() {
 		JPanel panel = new JPanel();
-		JLabel a=new JLabel("ads");
+		JLabel a=new JLabel("Moar Creddits?");
+		JLabel image =new JLabel(new ImageIcon("src/arcade/store/gui/resources/TrollFace.png"));
 		JButton b=new JButton("close");
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -29,6 +30,7 @@ public class ExampleWindow extends Window {
 		a.setAlignmentX(CENTER_ALIGNMENT);
 		b.setAlignmentX(CENTER_ALIGNMENT);
 		panel.add(a);
+		panel.add(image);
 		panel.add(b);
 		panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
 		add(panel);
