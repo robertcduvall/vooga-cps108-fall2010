bc045ecade3f0e63ad563727d5be6d0980d4866c
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-08 02:14:35 +0000

    wall updates - added dialog box to confirm that a user wants to change their rating for a particular game

--- trunk/src/arcade/wall/model/Comment.java
+++ trunk/src/arcade/wall/model/Comment.java
@@ -1,17 +1,10 @@
 package arcade.wall.model;
 
-
-
 public class Comment {
 	private String myGameName;
 	private String myUserName;
 	private String myCommentString;
 	private String myRating;
-	// TODO Add more fields (table columns)
-	
-//	public Comment(String userName) {
-//		this(userName, "");
-//	}
 	
 	public Comment(String gameName, String userName, String commentString, String rating) {
 		myGameName = gameName;
@@ -19,24 +12,6 @@ public class Comment {
 		myCommentString = commentString;
 		myRating = rating;
 	}
-	
-//	public void setName(String first,String last) {
-//		myFirstName = first;
-//		myLastName = last;
-//	}
-//	
-//	public void setBirthday(String date) {
-//		myBirthday = date;
-//	}
-//	
-//	public void setEmail(String email) {
-//		myEmail = email;
-//	}
-//	
-//	public void setAvatar(String avatarURL) {
-//		myAvatarURL = avatarURL;
-//	}
-//	
 	
 	public String getGameName() {
 		return myGameName;
@@ -53,33 +28,12 @@ public class Comment {
 	public String getRating() {
 		return myRating;
 	}
-//	
-//	public String getFullName() {
-//		return myFirstName + " "+myLastName;
-//	}
-//	
-//	public String getFirstName() {
-//		return myFirstName;
-//	}
-//	
-//	public String getLastName() {
-//		return myLastName;
-//	}
-//	
-//	public String getBirthday() {
-//		return myBirthday;
-//	}
-//	
-//	public String getEmail() {
-//		return myEmail;
-//	}
-//	
-//	public String getAvatar() {
-//		return myAvatarURL;
-//	}
-//	
 
 	public void setCommentString(String string) {
 		this.myCommentString = string;
+	}
+
+	public void setRating(String selectedValue) {
+		this.myRating = selectedValue;
 	}
 }
