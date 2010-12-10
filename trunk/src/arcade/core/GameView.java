6501709b5c0a113ad0b54b436b9b6d7007cd1a56
suyang1 <suyang1@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-10 06:19:48 +0000

    Modified Search

--- trunk/src/arcade/core/GameView.java
+++ trunk/src/arcade/core/GameView.java
@@ -105,28 +105,4 @@ public class GameView extends JPanel {
 		});
 		return back;
 	}
-	// private static void createAndShowGUI() {
-	//
-	// //Create and set up the window.
-	// JFrame frame = new JFrame("Game Viewer");
-	// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//
-	// //Create and set up the content pane.
-	// GameView newContentPane = new GameView("zombieland");
-	// newContentPane.setOpaque(true); //content panes must be opaque
-	// frame.setContentPane(newContentPane);
-	//
-	// //Display the window.
-	// frame.pack();
-	// frame.setVisible(true);
-	// }
-	//
-	// public static void main(String[] args) {
-	// javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	// public void run() {
-	// createAndShowGUI();
-	// }
-	// });
-	// }
-
 }
