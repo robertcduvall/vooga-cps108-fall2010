2aaeea70789bd76daca84b980a95801905fabe5f
jtkline1205 <jtkline1205@gmail.com>
2010-12-11 18:02:50 +0000

    wall updates

---
+++ src/arcade/wall/model/Review.java
@@ -0,0 +1,10 @@
+package arcade.wall.model;
+
+/**
+ * Represents a user's review of a game. A user can only submit a single review about each game.
+ * @author John
+ *
+ */
+public class Review {
+
+}
---
+++ src/arcade/wall/view/WallWidget.java
@@ -0,0 +1,11 @@
+package arcade.wall.view;
+
+/**
+ * Utility that allows users to comment on and rate items (games) while they are running 
+ * (playing) them in the Arcade.
+ * @author John, Bhawana, Cam
+ *
+ */
+public class WallWidget {
+
+}
