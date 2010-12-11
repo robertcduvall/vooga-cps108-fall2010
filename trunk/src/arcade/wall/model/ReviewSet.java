50523e851f8b9ff9c8eb616dd8e10729c7174263
jtkline1205@gmail.com <jtkline1205@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-11 18:02:50 +0000

    wall updates

---
+++ trunk/src/arcade/wall/model/ReviewSet.java
@@ -0,0 +1,8 @@
+package arcade.wall.model;
+/**
+ * A ReviewSet contains all the VOOGA Reviews made by all users. It is linked to our online database.
+ * @author John, David Herzka
+ */
+public class ReviewSet {
+
+}
