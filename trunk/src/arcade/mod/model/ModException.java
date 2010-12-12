65afb33379705f31f204bbaba5ef896f9d751106
brian.s.simel <brian.s.simel@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-12 17:13:01 +0000

    unimportant data-drivenness abstraction

--- trunk/src/arcade/mod/model/ModException.java
+++ trunk/src/arcade/mod/model/ModException.java
@@ -1,11 +1,15 @@
 package arcade.mod.model;
 
 public class ModException extends RuntimeException {
+	
+	private static final String FILE_ERROR_MESSAGE = "please choose another file";
+	private static final String INPUT_ERROR_MESSAGE = "unrecognized input";
+
 	public static ModException BAD_INPUT = new ModException(
-			"unrecognized input");
+			INPUT_ERROR_MESSAGE);
 
 	public static ModException BAD_FILE = new ModException(
-			"please choose another file");
+			FILE_ERROR_MESSAGE);
 
 	public ModException(String message) {
 		super(message);
