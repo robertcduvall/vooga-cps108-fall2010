29bcb64bb84a20eb9b98abbc48fc9fd64362ad01
cody.ko <cody.ko@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-14 15:22:11 +0000

    Added main to Blah in Zombies. Wasn't working for me otherwise.

--- trunk/src/vooga/games/zombies/Blah.java
+++ trunk/src/vooga/games/zombies/Blah.java
@@ -106,7 +106,7 @@ public class Blah extends Game implements Constants {
 	 * 
 	 * @param args
 	 */
-	public static void run() {
+	public static void main(String[] args) {
 		launch(new Blah());
 	}
 
