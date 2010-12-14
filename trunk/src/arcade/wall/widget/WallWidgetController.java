40a994a7ce62195e373a1bc1105e45ff1f12641f
bhawana686 <bhawana686@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-14 00:32:42 +0000

    Created WallWidget.

---
+++ trunk/src/arcade/wall/widget/WallWidgetController.java
@@ -0,0 +1,39 @@
+package arcade.wall.widget;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import arcade.wall.controllers.WallTabController;
+import arcade.wall.models.Comment;
+import arcade.wall.models.WallTabModel;
+
+public class WallWidgetController {
+	WallTabController controller;
+	WallTabModel model; 
+	WallWidgetView view;
+
+	public WallWidgetController() {
+		model = new WallTabModel();
+		view = new WallWidgetView(this);	
+		view.addReviewButtonListener(new ReviewButtonListener());
+	}
+
+	public WallWidgetView getView() {
+		return view;
+	}
+	
+	class ReviewButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	String selectedGameName = view.getSelectedGame();
+    		Comment submittedComment = new Comment(""+model.getNewCommentID(), selectedGameName, "2", 
+    											   view.getEntryText(), view.getSelectedRating());
+    		model.addComment(submittedComment);
+    		JOptionPane.showMessageDialog(view.getPanel(),
+    				"Your comments were successfully added.\n","",
+    				JOptionPane.PLAIN_MESSAGE );
+        }
+	}
+	
+}
