1241a6dfca9f55657a6baf05d022270d5f4314e7
seebop@gmail.com <seebop@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-15 21:15:02 +0000

    Working on a table for messages

--- trunk/src/arcade/wall/views/ratings/ComboPanel.java
+++ trunk/src/arcade/wall/views/ratings/ComboPanel.java
@@ -1,5 +1,7 @@
 package arcade.wall.views.ratings;
 
+import javax.swing.JRadioButton;
+
 /**
  * 
  * @author Cameron McCallie
@@ -7,9 +9,26 @@ package arcade.wall.views.ratings;
  */
 @SuppressWarnings("serial")
 public class ComboPanel extends AbstractButtonPanel{
-
+	
+	private String[] myRatings;
+	
 	public ComboPanel(int scale) {
 		super(scale);
+		myRatings = new String[scale];
+		for (int i = 1; i < scale+1; i ++){
+			myRatings[i-1] = ""+i;
+		}
+		
 	}
 
+	/**
+	 * Adds a comment after the rating.
+	 * Ex: 4 - Awesome!
+	 * 
+	 * Note that the user would have to specify the "-".
+	 */
+	public void addComment(int index, String comment){
+		String newLabel = myRatings[index - 1] + " " + comment;
+		myRatings[index - 1] = newLabel;
+	}
 }
