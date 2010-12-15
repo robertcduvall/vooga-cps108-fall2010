1106f6be3e2dd81adf0b756fb39b81db9f167f67
drew.sternesky <drew.sternesky@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-15 20:10:16 +0000

    updated privileges

--- trunk/src/arcade/store/privileges/PrivilegeManager.java
+++ trunk/src/arcade/store/privileges/PrivilegeManager.java
@@ -4,13 +4,19 @@ import java.util.*;
 import arcade.store.account.StoreUser;
 
 public class PrivilegeManager {
-	
-	private static ResourceBundle userTypes = ResourceBundle.getBundle("arcade.store.privileges.UserTypes");
-	
+
+	private static ResourceBundle userTypes = ResourceBundle.getBundle("arcade.store.privileges.usertypes");
+
 	public boolean getPermission(StoreUser user, String permissionType) {
 		String userType = user.getAccountType();
 		ResourceBundle bundle = ResourceBundle.getBundle(userTypes.getString(userType));
-		return bundle.getString(permissionType).equals("true") ? true : false;
+		try {
+			String booleanValue = bundle.getString(permissionType);
+			return booleanValue.equals("true") ? true : false;
+		}
+		catch (Exception e) {
+			return false;
+		}
 	}
-	
+
 }
