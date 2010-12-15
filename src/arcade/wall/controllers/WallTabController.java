65aacde5c41358ad5e5f38dc3d4b8d39f78dfb1b
seebop <seebop@gmail.com>
2010-12-15 19:01:13 +0000

    Added a dialogue box

--- src/arcade/wall/controllers/WallTabController.java
+++ src/arcade/wall/controllers/WallTabController.java
@@ -26,14 +26,14 @@ public class WallTabController {
 		myModel = new WallModel();
 		myView = new WallTabView(this);
 		refreshComments(WallTabView.myGameChoices[0]);
-		
+
 		//Add listeners to the view.
 		myView.addGameComboBoxListener(new GameComboBoxListener());
 		myView.addReviewButtonListener(new ReviewButtonListener());
 		myView.addSendMessageButtonListener(new SendMessageButtonListener());
 		myView.addComposeMessageButtonListener(new ComposeMessageButtonListener());
 	}
-	
+
 	/**
 	 * Refreshes the WallTab Comments Area to display the comments for the given game.
 	 */
@@ -48,7 +48,7 @@ public class WallTabController {
 	 */
 	public int showCommentDialog() {
 		Object[] options = {"Yes",
-				"No"};
+		"No"};
 		int n = JOptionPane.showOptionDialog(myView.getPanel(),
 				"Your new rating for this game is different from your last rating.\n"
 				+ "Do you still want to change your rating for this game?",
@@ -60,7 +60,7 @@ public class WallTabController {
 				options[1]);
 		return n;
 	}
-	
+
 	public WallTabView getView() {
 		return myView;
 	}
@@ -68,42 +68,50 @@ public class WallTabController {
 	public double getRating(String gameName) {
 		return myModel.getAverageRating(gameName);
 	}
-	
+
 	class GameComboBoxListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if ("comboBoxChanged".equals(e.getActionCommand())) {
-            	String selectedGame = myView.getSelectedGame();
-                myView.setGameHeaderLabel(selectedGame);
-                refreshComments(selectedGame);
-                myView.setEntryText("");
-    	    }
-        }
-    }
-	
+		public void actionPerformed(ActionEvent e) {
+			if ("comboBoxChanged".equals(e.getActionCommand())) {
+				String selectedGame = myView.getSelectedGame();
+				myView.setGameHeaderLabel(selectedGame);
+				refreshComments(selectedGame);
+				myView.setEntryText("");
+			}
+		}
+	}
+
 	class ReviewButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-        	String selectedGameName = myView.getSelectedGame();
-        	//TODO change to make use of ProfileSet.currentProfile
-    		Comment submittedComment = new Comment(""+myModel.getNewCommentID(), selectedGameName, "1", 
-    											   myView.getEntryText(), myView.getSelectedRating());
-    		//TODO is this the way we actually want to determine if a Comment is valid or not?
-    		if (myModel.commentIsConflicting(submittedComment)) {
-    			if (showCommentDialog() == JOptionPane.YES_OPTION) {
-    				myModel.updateCommentRatings(selectedGameName, "1", myView.getSelectedRating());
-    				myModel.addComment(submittedComment);
-    			}
-    		} else {
-    			myModel.addComment(submittedComment);
-    		}
-    		myView.setGameHeaderLabel(selectedGameName);
-    		refreshComments(selectedGameName);
-    		myView.updateTopRatedGamesLabel();
-    		myView.setEntryText("");
-        }
-    }
+		public void actionPerformed(ActionEvent e) {
+			String selectedGameName = myView.getSelectedGame();
+			//TODO change to make use of ProfileSet.currentProfile
+			Comment submittedComment = new Comment(""+myModel.getNewCommentID(), selectedGameName, "1", 
+					myView.getEntryText(), myView.getSelectedRating());
+			//TODO is this the way we actually want to determine if a Comment is valid or not?
+			if (myModel.commentIsConflicting(submittedComment)) {
+				if (showCommentDialog() == JOptionPane.YES_OPTION) {
+					myModel.updateCommentRatings(selectedGameName, "1", myView.getSelectedRating());
+					myModel.addComment(submittedComment);
+				}
+			} else {
+				myModel.addComment(submittedComment);
+			}
+			myView.setGameHeaderLabel(selectedGameName);
+			refreshComments(selectedGameName);
+			myView.updateTopRatedGamesLabel();
+			myView.setEntryText("");
+		}
+	}
 
 	class SendMessageButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
+			Object[] options = {"Yes","No", "Cancel"};
+			int n = JOptionPane.showOptionDialog(myView.getComposeMessageFrame(),"Are you sure you want to send this message?",
+					"Confirm",
+					JOptionPane.YES_NO_CANCEL_OPTION,
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					options,
+					options[2]);
 			Message message = new Message(""+myModel.getNewMessageID(), "1", 
 					myView.getReceiverText(), myView.getMessageContentText());
 			myModel.addMessage(message);
@@ -111,13 +119,13 @@ public class WallTabController {
 			myView.setMessageContentText("");
 		}
 	}
-	
+
 	class ComposeMessageButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			myView.getComposeMessageFrame().setVisible(true);
 		}
 	}
-	
+
 	public List<String> getGameRankList() {
 		return myModel.getGameRankList();
 	}
