c02cf8a7314e2b8b47ad6a64696515abce0d90a4
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 18:38:21 +0000

    [store] commented PrivilegeManager

--- src/arcade/store/privileges/PrivilegeManager.java
+++ src/arcade/store/privileges/PrivilegeManager.java
@@ -3,18 +3,26 @@ package arcade.store.privileges;
 import java.util.*;
 import arcade.store.account.StoreUser;
 
+/**
+ * PrivilegeManager checks the type of the current user and allows specific
+ * permissions based on the type.
+ * 
+ * @author Drew Sternesky, Jimmy Mu, Marcus Molchany
+ * 
+ */
 public class PrivilegeManager {
 
-	private static ResourceBundle userTypes = ResourceBundle.getBundle("arcade.store.privileges.usertypes");
+	private static ResourceBundle userTypes = ResourceBundle
+			.getBundle("arcade.store.privileges.usertypes");
 
 	public boolean getPermission(StoreUser user, String permissionType) {
 		String userType = user.getAccountType();
-		ResourceBundle bundle = ResourceBundle.getBundle(userTypes.getString(userType));
+		ResourceBundle bundle = ResourceBundle.getBundle(userTypes
+				.getString(userType));
 		try {
 			String booleanValue = bundle.getString(permissionType);
 			return booleanValue.equals("true") ? true : false;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
