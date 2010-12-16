1366c0f3f13d093344c928b1e18255fdef212b98
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 18:51:34 +0000

    [store] Modified AdminUser and DeveloperUser

--- src/arcade/store/account/AdminUser.java
+++ src/arcade/store/account/AdminUser.java
@@ -1,8 +1,24 @@
 package arcade.store.account;
 
-public class AdminUser extends StoreUser{
+/**
+ * AdminUser extends StoreUser and describes what an administrator can do in the
+ * store.
+ * 
+ * @author Drew Sternesky, Jimmy Mu, Marcus Molchany
+ * 
+ */
+public class AdminUser extends StoreUser {
 
-	private static final String TYPE = "3";
+	private static final String ADMIN_TYPE = "3";
+	private static final int ADMIN_CREDDITS = 1000000;
 	
-	
+	@Override
+	public String getAccountType() {
+		return ADMIN_TYPE; 
+	}
+
+	@Override
+	public void setCreddits(double newCreddits) {
+		setCreddits(newCreddits + ADMIN_CREDDITS);
+	}
 }
