9ab5a2e16393d57d12eb767a6f8579fe11e97998
laz3638 <laz3638@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 17:04:04 +0000

    moar javadoc

--- src/arcade/security/util/LogInHandler.java
+++ src/arcade/security/util/LogInHandler.java
@@ -7,38 +7,82 @@ import arcade.security.util.userserviceutil.PrivilegeMap;
 import arcade.security.util.userserviceutil.CurrentUser;
 import arcade.security.util.userserviceutil.UserServiceFactory;
 
+/**
+ * A model object for the login panel.
+ * 
+ * @author Meng Li, Jiaqi Yan, Nick Hawthorne
+ * 
+ */
 public class LogInHandler {
 	public static DataHandler dataHandler = DataHandler.getInstance("User");
-	
-	public static boolean successfulLogin(String username, String password){
+
+	/**
+	 * Checks a username/password combination to see if it exists in the
+	 * database, and logs that user in if it does.
+	 * 
+	 * @param username
+	 *            The username to check
+	 * @param password
+	 *            The password to check
+	 * @return true if the login is sucessful.
+	 */
+	public static boolean successfulLogin(String username, String password) {
 		int userId = getUserId(username);
-		if(userId < 1) return false;
-		if(isPasswordValid(userId, password)){
-			//Arcade.refreshRight(); //from arcade group
+		if (userId < 1)
+			return false;
+		if (isPasswordValid(userId, password)) {
+			// Arcade.refreshRight(); //from arcade group
 			ProfileSet.setUser(userId);
 			ProfileSet.getCurrentProfile().setUserName(username);
 			return true;
 		}
 		return false;
 	}
-	
-	public static void setCurrentUser(int userId){
+
+	/**
+	 * Sets the current user by userID.
+	 * 
+	 * @param userId
+	 *            The user to set as current
+	 */
+	public static void setCurrentUser(int userId) {
 		PrivilegeMap.loadMap();
 		CurrentUser user = UserServiceFactory.getCurrentUser();
 		user.setAsLogined(userId);
 	}
-	
-	
-	
-	public static int getUserId(String username){
+
+	/**
+	 * Gets the userId for a specific user.
+	 * 
+	 * @param username
+	 *            The user in question
+	 * @return The user's userID
+	 */
+	public static int getUserId(String username) {
 		return dataHandler.getUserId(username);
 	}
-	
-	public static boolean isAdmin(String username){
+
+	/**
+	 * Checks if a user is an administrator.
+	 * 
+	 * @param username
+	 *            The user to check
+	 * @return true if the user is an administrator
+	 */
+	public static boolean isAdmin(String username) {
 		return dataHandler.isAdmin(username).equals("Administrator");
 	}
-	
-	public static boolean isPasswordValid(int userId, String password){
+
+	/**
+	 * Checks a password to see if it corresponds to a userID.
+	 * 
+	 * @param userId
+	 *            The userID to check
+	 * @param password
+	 *            The password to check
+	 * @return true if they match
+	 */
+	public static boolean isPasswordValid(int userId, String password) {
 		String validPassword = dataHandler.getPassword(userId);
 		PasswordHasher hasher = new PasswordHasher();
 		String hashedPassword = hasher.encrypt(String.valueOf(password));
