bedc9923eb69ff938d6bc55ad831f56932f4b0e4
laz3638 <laz3638@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 18:26:58 +0000

    javadox

--- src/arcade/security/gui/AdminBox.java
+++ src/arcade/security/gui/AdminBox.java
@@ -5,31 +5,62 @@ import javax.swing.JComboBox;
 import arcade.security.util.AdminHandler;
 import java.awt.event.*;
 
-public class AdminBox extends JComboBox{
+/**
+ * AdminBox is an extension of JComboBox, which is displayed only on the
+ * administrator panel.
+ * 
+ * @author Jiaqi Yan, Meng Li, Nick Hawthorne
+ * 
+ */
+public class AdminBox extends JComboBox {
 	private String username;
-	public AdminBox(String name,String[] userTypes){
+
+	/**
+	 * Constructor for an AdminBox
+	 * 
+	 * @param name
+	 *            The username desired
+	 * @param userTypes
+	 *            An array of userTypes as options
+	 */
+	public AdminBox(String name, String[] userTypes) {
 		super(userTypes);
 		username = name;
 		addItemListener(new AdminBoxListener());
 	}
-	
-	public String getName(){
+
+	/**
+	 * Return the username associated with this AdminBox
+	 * 
+	 * @return The username for this box
+	 */
+	public String getName() {
 		return username;
 	}
-	
-	public void changeDisplayContent(int index){
+
+	/**
+	 * Changes the item displayed in the box.
+	 * 
+	 * @param index
+	 *            The index of the new item to display
+	 */
+	public void changeDisplayContent(int index) {
 		setSelectedIndex(index);
 		updateUI();
 	}
-	
-	public class AdminBoxListener implements ItemListener{
+
+	/**
+	 * 
+	 * Listener for the AdminBox.
+	 * 
+	 */
+	public class AdminBoxListener implements ItemListener {
 
 		@Override
 		public void itemStateChanged(ItemEvent e) {
-			AdminHandler.changeUserType(username, getSelectedIndex());		
+			AdminHandler.changeUserType(username, getSelectedIndex());
 		}
-		
+
 	}
-		
-	
+
 }
