3233a97ec29e6c7e8ccf9154fab63078f6fee252
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 04:58:24 +0000

    added high score update and working on ads

--- trunk/src/arcade/core/examples/Ads.java
+++ trunk/src/arcade/core/examples/Ads.java
@@ -1,25 +1,140 @@
 package arcade.core.examples;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import arcade.ads.adsmanager.AdsManager;
 import arcade.core.Panel;
 
 public class Ads extends Panel {
+	
+	private AdsManager myManager;
+	
 	public Ads() {
-		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+//		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
+//		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+//
+//		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//		JLabel randomAd = new JLabel("Buy Coke.");
+//		add(randomAd);
+//
+//		JLabel evenMoreLabels = new JLabel(icon);
+//		add(evenMoreLabels);
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		
 
-		setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		JLabel randomAd = new JLabel("Buy Coke.");
-		add(randomAd);
+		
+		setLayout(new BorderLayout());
 
-		JLabel evenMoreLabels = new JLabel(icon);
-		add(evenMoreLabels);
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new FlowLayout());
+		makeButtons(panel1);
+		// this.add(panel1);
+
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new FlowLayout());
+		addListener(panel2);
+
+		myManager = new AdsManager(new JPanel());
+		myManager.setActiveAds(new File("src/arcade/ads/resources/ads.txt"));
+		myManager.setRenderedAds();
+		//myManager.setRenderedAds("action");
+		
+		add(panel1, BorderLayout.PAGE_START);
+		add(panel2, BorderLayout.CENTER);
+
+
+		//myManager.setPanel(panel2);
+		myManager.runAdsManager();
+		// myManager.update();
+		// myManager.render();
+	}
+
+	/**
+	 * Create the JButtons to add to the panel
+	 * 
+	 */
+	private void makeButtons(JPanel panel) {
+		panel.setLayout(new FlowLayout());
+
+		JButton prev = new JButton("Previous Ad");
+		panel.add(prev);
+		prev.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myManager.getRenderedAdsGroup().prevAds();
+			}
+		});
+
+		JButton rotate = new JButton("Rotate Ad");
+		panel.add(rotate);
+		rotate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myManager.rotate();
+			}
+		});
+
+		JButton stoprotate = new JButton("Stop Rotate Ad");
+		panel.add(stoprotate);
+		stoprotate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myManager.stoprotate();
+			}
+		});
+
+		JButton next = new JButton("Next Ad");
+		panel.add(next);
+		next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myManager.getRenderedAdsGroup().nextAds();
+			}
+		});
+	}
+
+	/**
+	 * add MouseListener to Image panel
+	 */
+	private void addListener(JPanel panel) {
+		panel.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// myManager.getCurrentAd().onLeftClick();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 }
