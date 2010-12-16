4a1933dc501ffce12ccc94b8c88721632070e61b
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 07:27:29 +0000

    added other ratings

--- trunk/src/arcade/core/examples/OtherRatings.java
+++ trunk/src/arcade/core/examples/OtherRatings.java
@@ -1,14 +1,22 @@
 package arcade.core.examples;
 
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import arcade.core.Panel;
+import arcade.lobby.model.ProfileSet;
+import arcade.store.StoreModel;
+import arcade.wall.models.WallModel;
+import arcade.wall.views.walltab.WallTabPanel;
 
 
 public class OtherRatings extends Panel {
+	private WallModel model;
+	private String[] gameList;
+	
 	public OtherRatings(){
+		/*
 		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
 		JLabel rateOthers = new JLabel("Rate These Other Games");
 		JLabel moreLabels = new JLabel(icon);
@@ -16,5 +24,28 @@ public class OtherRatings extends Panel {
 		add(rateOthers);
 		add(moreLabels);
 		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		*/
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		gameList = StoreModel.getUserOwnedGamesAsStrings((ProfileSet.getCurrentProfile().getUserId()));
+		model = new WallModel();
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		for(String game: gameList)
+		{
+			add(getGameBlockInformation(game));
+		}
+	}
+	
+	public JLabel getGameBlockInformation(String selectedGame) {
+		JLabel gameInformation = new JLabel();
+		gameInformation.setText("<html>" + 
+				"<font color=blue>" + selectedGame + "</font> || Average Rating: " +
+				+ WallTabPanel.myController.getRating(selectedGame) + "</html>");
+		return gameInformation;
 	}
 }
