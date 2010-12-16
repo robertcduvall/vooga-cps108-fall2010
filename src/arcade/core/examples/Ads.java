fb5749518d7840109eb0152652c2b21ebd4294ab
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 06:54:41 +0000

    ads are not working consulting with ads group added lobby and rating

--- src/arcade/core/examples/Ads.java
+++ src/arcade/core/examples/Ads.java
@@ -34,107 +34,18 @@ public class Ads extends Panel {
 	
 	private void initialize()
 	{
-		
-
-		
-		setLayout(new BorderLayout());
-
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(new FlowLayout());
-		makeButtons(panel1);
-		// this.add(panel1);
-
 		JPanel panel2 = new JPanel();
-		panel2.setLayout(new FlowLayout());
-		addListener(panel2);
 
 		myManager = new AdsManager(panel2);
 		myManager.setActiveAds(new File("src/arcade/ads/resources/ads.txt"));
 		myManager.setRenderedAds();
 		//myManager.setRenderedAds("action");
-		
-		add(panel1, BorderLayout.PAGE_START);
-		add(panel2, BorderLayout.CENTER);
+		add(panel2);
 
 
 		//myManager.setPanel(panel2);
 		myManager.runAdsManager();
 		// myManager.update();
 		// myManager.render();
-	}
-
-	/**
-	 * Create the JButtons to add to the panel
-	 * 
-	 */
-	private void makeButtons(JPanel panel) {
-		panel.setLayout(new FlowLayout());
-
-		JButton prev = new JButton("Previous Ad");
-		panel.add(prev);
-		prev.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				myManager.getRenderedAdsGroup().prevAds();
-			}
-		});
-
-		JButton rotate = new JButton("Rotate Ad");
-		panel.add(rotate);
-		rotate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				myManager.rotate();
-			}
-		});
-
-		JButton stoprotate = new JButton("Stop Rotate Ad");
-		panel.add(stoprotate);
-		stoprotate.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				myManager.stoprotate();
-			}
-		});
-
-		JButton next = new JButton("Next Ad");
-		panel.add(next);
-		next.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				myManager.getRenderedAdsGroup().nextAds();
-			}
-		});
-	}
-
-	/**
-	 * add MouseListener to Image panel
-	 */
-	private void addListener(JPanel panel) {
-		panel.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				// myManager.getCurrentAd().onLeftClick();
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-
-			}
-		});
 	}
 }
