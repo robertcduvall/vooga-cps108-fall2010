62b77f3a7c7b2863f9a9466172572c9bb821044d
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 04:50:28 +0000

    CurrentUser has been added and profile has been added to CurrentUser

--- trunk/src/arcade/security/control/AdminPanelControl.java
+++ trunk/src/arcade/security/control/AdminPanelControl.java
@@ -11,13 +11,12 @@ import arcade.lobby.model.ProfileSet;
 import arcade.security.model.AdminProcess;
 import arcade.security.model.IModel;
 import arcade.security.model.LoginProcess;
-import arcade.security.util.userserviceutil.User;
-import arcade.security.util.userserviceutil.UserService;
+import arcade.security.util.userserviceutil.CurrentUser;
+
 import arcade.security.util.userserviceutil.UserServiceFactory;
 import arcade.security.view.AdminPanel;
 import arcade.security.view.IView;
 import arcade.security.view.LogInPanel;
-
 /**
  * Controller for the Administrator Panel. Used in conjunction with
  * security.view.AdminPanel and security.model.AdminProcess.
@@ -25,13 +24,11 @@ import arcade.security.view.LogInPanel;
  * @author Meng Li, Jiaqi Yan, Nick Hawthorne
  * 
  */
-
 public class AdminPanelControl implements IControl {
 
-	private final static Logger log = Logger.getLogger(AdminPanelControl.class);
+	private final static Logger log=Logger.getLogger(AdminPanelControl.class);
 	private AdminProcess model;
-	private AdminPanel view;
-
+	private AdminPanel view; 
 	/**
 	 * Constructor for the admin panel controller. Takes an IView object and an
 	 * IModel object
@@ -41,29 +38,26 @@ public class AdminPanelControl implements IControl {
 	 * @param model
 	 *            the corresponding model object
 	 */
-	public AdminPanelControl(IView view, IModel model) {
-		this.model = (AdminProcess) model;
-		this.view = (AdminPanel) view;
-		this.view.addLogoutButtonListener(new ActionListener() {
+	public AdminPanelControl(IView view, IModel model){
+		this.model = (AdminProcess)model;
+		this.view = (AdminPanel)view;
+		this.view.addLogoutButtonListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				UserService userService = UserServiceFactory.getUserService();
-				User user = userService.getCurrentUser();
-				log.info("Before log out, Current User role: "
-								+ user.getRole());
-				user.setUserAs("default");
-				log.info("After log out, Current User role: " + user.getRole());
+				CurrentUser user = UserServiceFactory.getCurrentUser();
+				log.info("Before log out, Current User role: "+user.isLoggedIn());
+				user.setLoggedOut();
+				log.info("After log out, Current User role: "+user.isLoggedIn());
 				ProfileSet.setCurrentProfile(null);
 				switchToLogInPage();
 			}
 
 		});
 	}
-
-	/**
+/**
 	 * Switches the current panel to the login panel.
 	 */
-	private void switchToLogInPage() {
+	public void switchToLogInPage(){
 
 		view.removeAll();
 		view.updateUI();
