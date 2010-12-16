4151f4aaf67ff924c3521d8b8b8d5aa475dc656e
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 08:22:09 +0000

    reverted ads to original since they have not integrated

--- src/arcade/core/examples/Ads.java
+++ src/arcade/core/examples/Ads.java
@@ -1,15 +1,12 @@
 package arcade.core.examples;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.Color;
 import java.io.File;
 
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import arcade.ads.adsmanager.AdsManager;
@@ -20,32 +17,38 @@ public class Ads extends Panel {
 	private AdsManager myManager;
 	
 	public Ads() {
-//		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
-//		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-//
-//		setBorder(BorderFactory.createLineBorder(Color.BLACK));
-//		JLabel randomAd = new JLabel("Buy Coke.");
-//		add(randomAd);
-//
-//		JLabel evenMoreLabels = new JLabel(icon);
+
 //		add(evenMoreLabels);
 		initialize();
 	}
 	
+	/**
+	 * Since ads couldn't integrate into arcade, we placed a 
+	 * placeholder here so when they do finish they can add the code back in.
+	 * 
+	 * The code that is commented out is the lines of code they told us to use 
+	 * which did not work even after their efforts.
+	 *
+	 */
 	private void initialize()
 	{
-		JPanel panel2 = new JPanel();
+		/*JPanel panel2 = new JPanel();
 
 		myManager = new AdsManager(panel2);
 		myManager.setActiveAds(new File("src/arcade/ads/resources/ads.txt"));
 		myManager.setRenderedAds();
-		//myManager.setRenderedAds("action");
 		add(panel2);
 
+		myManager.runAdsManager();*/
+		
+		ImageIcon icon = new ImageIcon("src/arcade/core/RatingStar.gif");
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		//myManager.setPanel(panel2);
-		myManager.runAdsManager();
-		// myManager.update();
-		// myManager.render();
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JLabel randomAd = new JLabel("Buy Coke.");
+		add(randomAd);
+
+		JLabel evenMoreLabels = new JLabel(icon);
+		add(evenMoreLabels);
 	}
 }
