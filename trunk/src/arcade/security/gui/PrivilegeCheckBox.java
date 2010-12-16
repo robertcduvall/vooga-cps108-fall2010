ce1b054c909cd323c73ec0cc0cd8896db3687dc6
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 17:17:55 +0000

    add another constructor into this class

--- trunk/src/arcade/security/gui/PrivilegeCheckBox.java
+++ trunk/src/arcade/security/gui/PrivilegeCheckBox.java
@@ -6,15 +6,24 @@ import arcade.security.util.UserPrivilegesPanelHandler;
 
 import java.awt.event.*;
 
+
 public class PrivilegeCheckBox extends JCheckBox{
+	
+	private static final long serialVersionUID = 1L;
 	private boolean checked;
 	private String privilege;
+	
 	public PrivilegeCheckBox(String privilege,boolean value){
+		this(privilege, value, null);
+		addItemListener(new PrivilegeCheckBoxListener());
+	}
+	
+	public PrivilegeCheckBox(String privilege,boolean value, ItemListener listener){
 		super(privilege,value);
 		this.privilege = privilege;
 		checked = value;
 		setSelected(value);
-		addItemListener(new PrivilegeCheckBoxListener());
+		addItemListener(listener);
 	}
 	
 	public void setValue(boolean value){
@@ -27,7 +36,7 @@ public class PrivilegeCheckBox extends JCheckBox{
 		return privilege;
 	}
 	
-	public class PrivilegeCheckBoxListener implements ItemListener{
+	private class PrivilegeCheckBoxListener implements ItemListener{
 
 		@Override
 		public void itemStateChanged(ItemEvent arg0) {
