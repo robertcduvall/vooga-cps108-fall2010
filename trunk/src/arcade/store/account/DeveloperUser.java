f27b075a6bc0170fff3394c1ce70a96c26d3e01c
marcusmolchany <marcusmolchany@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 18:51:34 +0000

    [store] Modified AdminUser and DeveloperUser

--- trunk/src/arcade/store/account/DeveloperUser.java
+++ trunk/src/arcade/store/account/DeveloperUser.java
@@ -1,8 +1,26 @@
 package arcade.store.account;
 
-public class DeveloperUser extends StoreUser{
+import java.util.List;
 
-	private static final String TYPE = "2";
+/**
+ * DeveloperUser extends StoreUser and describes what a developer can do in the
+ * store.
+ * 
+ * @author Drew Sternesky, Jimmy Mu, Marcus Molchany
+ * 
+ */
+public class DeveloperUser extends StoreUser {
 	
+	private static final String DEVELOPER_TYPE = "2";
 	
+	@Override
+	public String getAccountType() {
+		return DEVELOPER_TYPE; 
+	}
+	
+	@Override
+	public void setCreddits(double newCreddits) {
+		setCreddits(0);
+	}
+
 }
