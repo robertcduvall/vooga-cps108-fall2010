71fc29a982ec35dd80dbd0da66008562f34506e1
ivannivy <ivannivy@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 04:54:46 +0000

    CurrentUser has been added and profile has been added to CurrentUser

--- trunk/src/arcade/security/view/AdminPanel.java
+++ trunk/src/arcade/security/view/AdminPanel.java
@@ -11,8 +11,6 @@ import arcade.security.exceptions.UserConfigurationNotFoundException;
 import arcade.security.gui.SecurityButton;
 import arcade.security.resourcesbundle.LabelResources;
 import arcade.security.resourcesbundle.StaticFileResources;
-import arcade.security.util.userserviceutil.User;
-import arcade.security.util.userserviceutil.UserService;
 import arcade.security.util.userserviceutil.UserServiceFactory;
 
 import net.miginfocom.swing.MigLayout;
@@ -30,68 +28,66 @@ import java.util.ResourceBundle;
  * @author Meng Li, Jiaqi Yan, Nick Hawthorne
  * 
  */
-public class AdminPanel extends JPanel implements IView {
-	private final static Logger log = Logger.getLogger(AdminPanel.class);
+public class AdminPanel extends JPanel implements IView{
+	private final static Logger log=Logger.getLogger(AdminPanel.class);
 	private static final long serialVersionUID = 1L;
 	private static final int SECURITY_PANEL_WIDTH = 900;
 	private static final int SECURITY_PANEL_HEIGHT = 300;
 	private static final String DELIM = ";";
-	private JLabel adminUserName;
+	private JLabel adminUserName; 
 	private JButton LogoutButton;
-	// private AdminPanelControl controller;
+	//private AdminPanelControl controller;
 	private JScrollPane adminScollPane;
 	private JPanel userRolePane;
 	private static String listOfPanels;
 	private ResourceBundle resources = ResourceBundle
-			.getBundle("arcade.security.resources.securitypanels.panels");
+	.getBundle("arcade.security.resources.securitypanels.panels");
 
 	/**
 	 * Constructor for the Administrator Panel. Currently under construction.
 	 */
-	public AdminPanel() {
-		setName("Admin page");
+	public AdminPanel(){
+		setName("Admin page"); 
 		setLayout(new MigLayout("wrap 2"));
 		adminUserName = new JLabel("Admin page. Currently under construction");
 		add(adminUserName);
 		LogoutButton = new SecurityButton(LabelResources.getLabel("Logout"));
-		add(LogoutButton, "gapleft 585");
+		add(LogoutButton,"gapleft 585");
 
 		adminScollPane = new JScrollPane();
-		adminScollPane.setViewportView(getUserRolePanel());
+		adminScollPane.setViewportView(getUserRolePanel());		
 		addUserSecurityPanel();
-		add(adminScollPane, "span 2");
+		add(adminScollPane,"span 2");
 		setVisible(true);
 	}
-
 	/**
 	 * Returns the current user role panel
 	 * 
 	 */
-	private JPanel getUserRolePanel() {
+	public JPanel getUserRolePanel() {
 		if (userRolePane == null) {
 			userRolePane = new JPanel(new GridLayout(0, 1));
-			userRolePane.setPreferredSize(new Dimension(SECURITY_PANEL_WIDTH,
-					SECURITY_PANEL_HEIGHT));
+			userRolePane.setPreferredSize(new Dimension(SECURITY_PANEL_WIDTH,SECURITY_PANEL_HEIGHT));
 
 		}
 		return userRolePane;
-	}
 
+	}
 	/**
 	 * Adds a user security panel to the list of panels
 	 */
-	private void addUserSecurityPanel() {
-		listOfPanels = resources.getString("listofpanels");
+	public void addUserSecurityPanel(){  
+		listOfPanels=resources.getString("listofpanels");		
 		loadPanels(listOfPanels);
-	}
 
+	}
 	/**
 	 * Adds a listener for the logout button
 	 * 
 	 * @param listener
 	 *            the listener to be added
 	 */
-	public void addLogoutButtonListener(ActionListener listener) {
+	public void addLogoutButtonListener(ActionListener listener){
 		LogoutButton.addActionListener(listener);
 	}
 
@@ -103,28 +99,31 @@ public class AdminPanel extends JPanel implements IView {
 	 */
 	private void loadPanels(String listOfPanels) {
 		String[] panel = listOfPanels.split(DELIM);
-		for (String p : panel) {
-			JPanel temp = createSecurityPane(p);
-			getUserRolePanel().add(temp);
-		}
+		for(String p: panel){
+			JPanel temp=createSecurityPane(p);
+			getUserRolePanel().add(temp);					
+		}				
 	}
-
 	/**
 	 * Creates and returns a security panel
 	 * 
 	 */
-	private JPanel createSecurityPane(String classPath) {
-		try {
-			Object securityPanel = Class.forName(classPath).newInstance();
-			return (JPanel) securityPanel;
-		} catch (ClassNotFoundException e1) {
+	private JPanel createSecurityPane(String classPath)
+	{			
+		try{
+			Object securityPanel=Class.forName(classPath).newInstance();  		    	 	
+			return (JPanel)securityPanel;      
+		}
+		catch(ClassNotFoundException e1){
 			e1.printStackTrace();
-		} catch (InstantiationException e2) {
+		}
+		catch(InstantiationException e2){
 			e2.printStackTrace();
-		} catch (IllegalAccessException e3) {
+		}
+		catch(IllegalAccessException e3){
 			e3.printStackTrace();
 		}
-		return null;
+		return null;					    
 	}
 
 }
