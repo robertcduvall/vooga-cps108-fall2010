5b408f91a1eba9299b38a50606480833261e76e2
seebop@gmail.com <seebop@gmail.com@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 03:20:28 +0000

    WallWidget updates

--- trunk/src/arcade/wall/controllers/WallWidgetController.java
+++ trunk/src/arcade/wall/controllers/WallWidgetController.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
 import arcade.lobby.model.ProfileSet;
 import arcade.wall.models.WallModel;
 import arcade.wall.models.data.comment.Comment;
+import arcade.wall.models.data.review.Review;
 import arcade.wall.views.wallwidget.WallWidgetView;
 
 /**
@@ -17,19 +18,19 @@ import arcade.wall.views.wallwidget.WallWidgetView;
  * @author Bhawana Singh
  */
 public class WallWidgetController {
-	WallTabController controller;
-	WallModel model; 
-	WallWidgetView view;
+	WallTabController myController;
+	WallModel myModel; 
+	WallWidgetView myView;
 	public String myTitle;
 
 	public WallWidgetController() {
-		model = new WallModel();
-		view = new WallWidgetView(this);	
-		view.addReviewButtonListener(new ReviewButtonListener());
+		myModel = new WallModel();
+		myView = new WallWidgetView(this);	
+		myView.addReviewButtonListener(new SubmitReviewButtonListener());
 	}
 
 	public WallWidgetView getView() {
-		return view;
+		return myView;
 	}
 	
 	public void setTitle(String title){
@@ -40,16 +41,38 @@ public class WallWidgetController {
 		return myTitle;
 	}
 	
-	//not being used
-	class ReviewButtonListener implements ActionListener {
+	/**
+	 * Displays a dialog box asking the user if they want to overwrite their previous rating given to a game.
+	 * @return 
+	 * 		The user's selection
+	 */
+	public int showCommentDialog() {
+		Object[] options = {"Yes",
+		"No"};
+		int n = JOptionPane.showOptionDialog(myView.getPanel(),
+				"Your new rating for this game is different from your last rating.\n"
+				+ "Do you still want to change your rating for this game?",
+				"Conflicting Ratings",
+				JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null,
+				options,
+				options[1]);
+		return n;
+	}
+	
+	class SubmitReviewButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-        	String selectedGameName = view.getSelectedGame();
-    		Comment submittedComment = new Comment(""+model.getNewCommentID(), selectedGameName, ""+ProfileSet.getCurrentProfile().getUserId(), 
-    											   view.getEntryText());
-    		model.addComment(submittedComment);
-    		JOptionPane.showMessageDialog(view.getPanel(),
-    				"Your comments were successfully added.\n","",
-    				JOptionPane.PLAIN_MESSAGE );
+        	String selectedGameName = myTitle;
+			Review submittedReview = new Review(""+myModel.getNewReviewID(), ""+ProfileSet.getCurrentProfile().getUserId(), selectedGameName, 
+					   myView.getEntryText(), myView.getSelectedRating());
+    		if (myModel.reviewIsConflicting(submittedReview)) {
+    			if (showCommentDialog() == JOptionPane.YES_OPTION) {
+    				myModel.addReview(submittedReview);
+    			}
+    		} else {
+    			myModel.addReview(submittedReview);
+    		}
         }
 	}
 	
--- trunk/src/arcade/wall/retired/ExampleWallWidget.java
+++ trunk/src/arcade/wall/retired/ExampleWallWidget.java
@@ -1,4 +1,4 @@
-package arcade.wall.views.wallwidget;
+package arcade.wall.retired;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
