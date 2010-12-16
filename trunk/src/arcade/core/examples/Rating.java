18dda9eb81a19444349ed6c4ddd12d5e3be83ed6
sailing.thru.gravity <sailing.thru.gravity@000728a5-ed1f-30b5-9e82-9fe7a4cf436c>
2010-12-16 07:43:21 +0000

    added a select index for the combo box in feedbackpanel uses that in rating added rating to arcade

--- trunk/src/arcade/core/examples/Rating.java
+++ trunk/src/arcade/core/examples/Rating.java
@@ -1,12 +1,14 @@
 package arcade.core.examples;
 
+import arcade.core.GameView;
 import arcade.core.Panel;
-import arcade.lobby.model.Profile;
 import arcade.wall.controllers.WallTabController;
+import arcade.wall.views.walltab.FeedbackPanel;
+import arcade.wall.views.walltab.WallTabPanel;
 
 public class Rating extends Panel {
 	private WallTabController myController;
-	Profile myProfile;
+	private FeedbackPanel ratingPanel;
 	
 	public Rating() {
 		/*
@@ -26,7 +28,30 @@ public class Rating extends Panel {
 	private void initialize()
 	{
 		myController = new WallTabController();
-		add(myController.getView().getFeedbackPanel());
+		ratingPanel = myController.getView().getFeedbackPanel();
+		int indexOfGame = getIndex();
+		
+		if(indexOfGame != -1)
+		{
+			ratingPanel.setSelectedIndex(indexOfGame);
+		}
+		
+		add(ratingPanel);
+	}
+	
+	private int getIndex()
+	{
+		String[] gameArray = WallTabPanel.myGameChoices;
+		String currentGame = GameView.getGame();
+		
+		for(int i = 0; i < gameArray.length; i++)
+		{
+			if(gameArray[i].equals(currentGame))
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 
 }
